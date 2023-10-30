2c2b0bb420fb23519aef5f32b2d007246467989e
Isa Robertini <robertiniisa@gmail.com>
2023-10-30 19:50:40 +0100

    saved

--- src/NavigationComponents/Navigation.jsx
+++ src/NavigationComponents/Navigation.jsx
@@ -1,10 +1,9 @@
-import Logo from '../assets/Logo.svg';
-import { NavigationMenu } from './NavigationMenu';
+import { NavigationMenu } from './NavigationManu';
 
 export const Navigation = () => {
     return (
         <div className="flex mx-16 bg-white items-center justify-between">
-            <img src={Logo} alt="Logo" />
+            <img src="https://cdn.glitch.global/be758d61-a981-49f2-a7a6-72216e478708/Logo.svg?v=1698689188784" alt="Logo" />
             <NavigationMenu />
         </div>
     );
