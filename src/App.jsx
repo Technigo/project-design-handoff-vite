f26527affedc0a1843879845fd9180b446936d42
veronica astrom <v.aastrom@gmail.com>
2023-10-31 09:45:06 +0100

    added basic structure and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
 };
