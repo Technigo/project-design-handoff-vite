419bc913554aa35cad3ad8488c273e48fe5c8afd
LauraLyckholm <laura.lyckholm@gmail.com>
2023-10-31 15:26:30 +0100

    Tried adding language changer in footer, looks ugly but works

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { Logo } from "../Logo";
 import styled from "styled-components";
+import { useTranslation } from "react-i18next";
 
 const FooterContainer = styled.footer`
     display: flex;
@@ -10,9 +11,28 @@ const FooterContainer = styled.footer`
 `
 
 export const Footer = () => {
+    const { t, i18n } = useTranslation();
+
+    // Function to change the language
+    const changeLanguageFunction = (lng) => {
+        i18n.changeLanguage(lng);
+    }
+
+    const englishFunction = () => {
+        changeLanguageFunction("en");
+    }
+
+    const swedishFunction = () => {
+        changeLanguageFunction("se");
+    }
+
     return (
         <FooterContainer>
             <Logo text={"Align Yoga Studio"} />
+            <h2>{t("welcome")}</h2>
+            <h1>{t("translatePage.heading")}</h1>
+            <button onClick={englishFunction}>English</button>
+            <button onClick={swedishFunction}>Swedish</button>
         </FooterContainer>
     )
 }
--- src/components/NavigationLinks.jsx
+++ src/components/NavigationLinks.jsx
@@ -1,23 +1,56 @@
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
+// import { slide as Menu } from "react-burger-menu";
+import "./navigation-links.css";
 
-const NavBar = styled.nav`
-    display: flex;
-    ul {
-        display: flex;
-        list-style: none;
-        li {
-            padding-right: 1rem;
-            a {
-                text-decoration: none;
-            }
-        }
-    }
-`
+// var styles = {
+//     bmBurgerButton: {
+//         position: 'fixed',
+//         width: '36px',
+//         height: '30px',
+//         left: '36px',
+//         top: '36px'
+//     },
+//     bmBurgerBars: {
+//         background: '#373a47'
+//     },
+//     bmBurgerBarsHover: {
+//         background: '#a90000'
+//     },
+//     bmCrossButton: {
+//         height: '24px',
+//         width: '24px'
+//     },
+//     bmCross: {
+//         background: '#bdc3c7'
+//     },
+//     bmMenuWrap: {
+//         position: 'fixed',
+//         height: '100%'
+//     },
+//     bmMenu: {
+//         background: '#373a47',
+//         padding: '2.5em 1.5em 0',
+//         fontSize: '1.15em'
+//     },
+//     bmMorphShape: {
+//         fill: '#373a47'
+//     },
+//     bmItemList: {
+//         color: '#b8b7ad',
+//         padding: '0.8em'
+//     },
+//     bmItem: {
+//         display: 'inline-block'
+//     },
+//     bmOverlay: {
+//         background: 'rgba(0, 0, 0, 0.3)'
+//     }
+// }
 
 export const NavigationLinks = () => {
     return (
-        <NavBar>
+
+        <nav>
             <ul>
                 <li><NavLink to={"/at-home"}>At home</NavLink></li>
                 <li><NavLink to={"/studio"}>Studio</NavLink></li>
@@ -27,6 +60,11 @@ export const NavigationLinks = () => {
                 <li><NavLink to={"#"}>Search</NavLink></li>
                 <li><NavLink to={"/sign-up"}>Sign up</NavLink></li>
             </ul>
-        </NavBar>
+        </nav>
+
     )
 }
+{/* <Menu styles={styles} right className="bm-menu">*/ }
+{/* <NavBar> */ }
+{/* </NavBar> */ }
+{/* </Menu>*/ }
