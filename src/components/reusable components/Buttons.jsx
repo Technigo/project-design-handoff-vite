74fd8f7046ca9ec6c41ef63171dccbd3de41bc64
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-31 22:48:06 +0100

    cleaning up default import

--- src/components/reusable components/Buttons.jsx
+++ src/components/reusable components/Buttons.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const buttons = () => {
     return (
         <div>buttons</div>
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const NewsPage = () => {
     return (
         <div>NewsPage</div>
