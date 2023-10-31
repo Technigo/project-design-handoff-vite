b7b8b0fe6c1c588985aa73fab01b88a10919b932
veronica astrom <v.aastrom@gmail.com>
2023-10-31 10:15:16 +0100

    added react router dom structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+
 
 export const App = () => {
   return (
-    <div>
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header>
+          <Link to="/">Home</Link>
+          <Link to="/about">About Us</Link>
+        </Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer>
+          <Link to="/contact">Contact Us</Link>
+        </Footer>
+      </div>
+    </BrowserRouter>
   );
 };
