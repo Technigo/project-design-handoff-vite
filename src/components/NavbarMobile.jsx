aca41fed1a660a6a166ae1dac3b853e18c7b144c
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2023-10-31 17:06:50 +0100

    burger menu mobile

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -13,21 +13,20 @@ const NavbarMobile = () => {
         <div className="navbar">
             <img src="/menu.png" alt="menu" style={{ width: '24px', height: '24px', cursor: 'pointer' }} onClick={handleMenuButtonClick} />
             <Menu isOpen={menuOpen} onStateChange={({ isOpen }) => setMenuOpen(isOpen)}>
-                <a id="home" className="menu-item" href="/">
-                    Home
-                </a>
-                <a id="videos" className="menu-item" href="/videos">
-                    Videos
-                </a>
-                <a id="workout" className="menu-item" href="/workout">
-                    Workout Plan
-                </a>
-                <a id="tracker" className="menu-item" href="/tracker">
-                    Tracker
-                </a>
-                <a id="favorites" className="menu-item" href="/favorites">
-                    Favorites
-                </a>
+                <div className="burger-items">
+                    <a id="home" className="menu-item" href="/">
+                        Log In
+                    </a>
+                    <a id="videos" className="menu-item" href="/videos">
+                        About Us
+                    </a>
+                    <a id="workout" className="menu-item" href="/workout">
+                        Membership Plans
+                    </a>
+                    <a id="tracker" className="menu-item" href="/tracker">
+                        Contact Us
+                    </a>
+                </div>
             </Menu>
 
             <img src="/logo.png" alt="logo" />
