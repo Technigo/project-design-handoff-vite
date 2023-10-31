546872bdfd62759803702b9b48dc580f7a6cd6e6
Idah Collin <idah_collin@hotmail.com>
2023-10-31 12:26:25 +0100

    Added content for info1 and css

---
+++ src/components/MainCont/Info1.jsx
@@ -0,0 +1,11 @@
+import "./info1.css"
+
+export const Info1 = () => {
+    return <div className="info1">
+        <h3>THIS IS OUR CLAN</h3>
+        <p>At Galactiv Fitness, we're on a mission to create hereoes. With a warm welcome to all, including newcomers to fitness, we're here to help you unleash your potential. Our inspiration comes from the timeless spirit of the Star Wars universe. Let us be your guiding force on the path to becoming the hero of your own story. Join us on this epic adventure.</p>
+        <button>Read more</button>
+        <h3>OUR CONCEPTS</h3>
+        <p>Our unique concept blends fitness and fandom. We offer Jedi training, where you start as a Padawan and train with lightsabers, cardio, and wellness. For those drawn to the dark side, our Sith training is intense,focusing on sparring and self-defence. For those who seek the strength of the Mandalorians, our training emphasises on muscle mass and CrossFit. Whether you're a Jedi, Sith, or Mandalorian at heart, our gym caters to your inner hero's journey.</p>
+    </div>
+}
--- src/components/MainCont/MainCont.jsx
+++ src/components/MainCont/MainCont.jsx
@@ -1,14 +1,15 @@
 //Mounted in App.jsx
 
 import { Hero } from "./Hero.jsx"
+import { Info1 } from "./Info1.jsx"
 import { Form } from "./Form.jsx"
 
 
 export const MainCont = () => {
     return (
         <div>
-            <p>This is the MainCont.jsx, more things might be mounted in me! Right now Hero and Form is mounted in me</p>
             <Hero />
+            <Info1 />
             <Form />
         </div>
     )
