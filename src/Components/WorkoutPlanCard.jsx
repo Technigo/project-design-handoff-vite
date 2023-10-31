00c51b410a75f636935917175290b4603e56066a
Hang-Nguyen-Vu <nguyenvuhang@gmail.com>
2023-10-31 23:53:40 +0100

    add icons to relevant cards

--- src/Components/WorkoutPlanCard.jsx
+++ src/Components/WorkoutPlanCard.jsx
@@ -3,6 +3,26 @@
 export const WorkoutPlanCard = ({ plan }) => {
   return (
     <>
+      <button type="button" className="heart">
+        <svg 
+          width="24" 
+          height="24" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="heart">
+            <path 
+              id="Icon" 
+              d="M4.31802 6.31802C2.56066 8.07538 2.56066 10.9246 4.31802 12.682L12.0001 20.364L19.682 12.682C21.4393 10.9246 21.4393 8.07538 19.682 6.31802C17.9246 4.56066 15.0754 4.56066 13.318 6.31802L12.0001 7.63609L10.682 6.31802C8.92462 4.56066 6.07538 4.56066 4.31802 6.31802Z"
+              stroke="#545F71" 
+              strokeWidth="2" 
+              strokeLinecap="round" 
+              strokeLinejoin="round"
+            />
+          </g>
+        </svg>
+      </button>
       <img src={plan.image} alt={plan.title} />
       <p>{plan.title}</p>
     </>
--- src/Sections/WorkoutPlans.jsx
+++ src/Sections/WorkoutPlans.jsx
@@ -9,6 +9,23 @@ export const WorkoutPlans = () => {
   return (
     <section className="workout-plans">
       <Headings sectionId={sectionId} />
+      <svg 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="lock-closed">
+          <path 
+            id="Icon" 
+            d="M12 15V17M6 21H18C19.1046 21 20 20.1046 20 19V13C20 11.8954 19.1046 11 18 11H6C4.89543 11 4 11.8954 4 13V19C4 20.1046 4.89543 21 6 21ZM16 11V7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V11H16Z" 
+            stroke="#545F71" 
+            strokeWidth="2" 
+            strokeLinecap="round"
+          />
+        </g>
+      </svg>
       <Subheadings sectionId={sectionId} />
       <div className="plan-wrapper">
         {data.plans.map((plan, index) => {
