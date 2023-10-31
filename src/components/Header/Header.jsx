8a075c3573769380149ab717c35063d0a8b37434
BeckieMorton <beckam11@gmail.com>
2023-10-31 11:22:12 +0100

    file structure and nav bar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavBar } from "../NavBar/NavBar";
+
+export const Header = () => {
+  return (
+    <>
+      <NavBar />
+      <div>Header</div>;
+    </>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
-import { HeroSection } from "../components/HeroSection";
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+import { HeroSection } from "../components/HeroSection/HeroSection";
 
 export const Home = () => {
   return (
