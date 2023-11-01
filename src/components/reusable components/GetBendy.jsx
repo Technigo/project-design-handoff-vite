0d2e84db077daece38a241d11cc130114f127efa
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-01 22:11:42 +0100

    added reusable components GetBendy + GetStrong

---
+++ src/components/reusable components/GetBendy.jsx
@@ -0,0 +1,7 @@
+export const GetBendy = () => {
+    return (
+        <div>
+            <img className="w-screen" src="images/bendy.jpg" alt="woman in leather doing a vertical split with the help of a pole " />
+        </div>
+    )
+}
---
+++ src/components/reusable components/GetStrong.jsx
@@ -0,0 +1,7 @@
+export const GetStrong = () => {
+    return (
+        <>
+            <img className="w-screen -scale-x-100" src="images/polePoel.png" alt="a man with a hoodie hanging from a pole horizontally with the help of his hands" />
+        </>
+    )
+}
