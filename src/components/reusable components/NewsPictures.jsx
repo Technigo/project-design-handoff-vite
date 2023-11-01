b4e74dbf9a91040dabc0190782bc64b8a4bd1d9c
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-01 14:17:35 +0100

    created NewsPictures component

---
+++ src/components/reusable components/NewsPictures.jsx
@@ -0,0 +1,10 @@
+
+export const NewsPictures = () => {
+    return (
+        <div className="flex flex-row justify-center gap-4 mx-1 h-auto mb-20">
+            <img src="images/Rectangle20.png" alt="woman doing a split upside down on the head" />
+            <img src="images/Rectangle21.png" alt="sportwoman doing a split hanging on a pole upside down" />
+            <img src="images/Rectangle22.png" alt="tatooed woman posing hanging upside down on a pole " />
+        </div>
+    )
+}
