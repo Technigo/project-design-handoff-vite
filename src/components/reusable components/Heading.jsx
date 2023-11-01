38b41224a9cf00458453c4123206c3b40c4dfaf7
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-01 13:39:27 +0100

    created HeadingBottom component

--- src/components/reusable components/Heading.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Heading = () => {
-    return (
-        <div>Heading</div>
-    )
-}
---
+++ src/components/reusable components/HeadingBottom.jsx
@@ -0,0 +1,10 @@
+export const HeadingBottom = ({ bottomPageHeading }) => {
+
+    return (
+        <>
+
+            <p className="mx-auto flex flex-row justify-center text-xl font-raleway font-normal tracking-wide text-white">{bottomPageHeading}</p>
+
+        </>
+    )
+}
