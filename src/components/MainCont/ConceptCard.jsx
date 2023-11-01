00d3aa45d10f629917f8e66d70541d0b5b03fb53
Idah Collin <idah_collin@hotmail.com>
2023-11-01 20:12:11 +0100

    More styling and side-scroll carousel

--- src/components/MainCont/ConceptCard.jsx
+++ src/components/MainCont/ConceptCard.jsx
@@ -3,15 +3,14 @@
 import "./conceptcard.css"
 
 export const ConceptCard = () => {
-    return <div className="conceptcard">
+    return <div className="conceptcard-scroll">
         <img src="./conceptcard1.png" alt="jedi" />
         <h3>BECOME A JEDI</h3>
+        <img src="./conceptcard2.png" alt="image" />
+        <h3>JOIN THE DARK SIDE</h3>
+        <img src="./conceptcard3.png" alt="image" />
+        <h3>THE WAY OF MANDALORE</h3>
     </div>
 }
 
 
-//images to be added to side-scroll:
-//<img src="./conceptcard2.png" alt="image" />
-//<h3>JOIN THE DARK SIDE</h3>
-//<img src="./conceptcard3.png" alt="image" />
-//<h3>THE WAY OF MANDALORE</h3>
