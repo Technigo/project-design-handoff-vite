09bdc38f16c8ad906b41db1a8252a5084ab2a017
Emmy Jansson <emmybengtsson@live.se>
2023-11-01 19:37:50 +0100

    Added the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes, Link, Outlet } from "react-router-dom";
 import { Navbar } from "./Components.jsx/Navbar";
 import { About } from "./Components.jsx/About";
+import { Header } from "./Components.jsx/Header";
 
 export const App = () => {
   return (
@@ -19,6 +20,7 @@ export const App = () => {
             <Route path="/about" element={<About />} />
           </Routes>
         </BrowserRouter>
+        <Header />
       </div>
     </>
   );
