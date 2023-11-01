15eb60b249b8dd9f407a0cecc27abc6430f99e4a
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-01 14:18:06 +0100

    created ClassesPictures component

---
+++ src/components/reusable components/ClassesPictures.jsx
@@ -0,0 +1,9 @@
+export const ClassesPictures = () => {
+    return (
+        <div className="mx-auto flex flex-row justify-center items-center mb-10">
+            <img src="images/DoublesMoving.png" alt="2 male acrobats on a pole" />
+            <img src="images/exoticPole.png" alt="woman in high heels doing a split holding a pole" />
+            <img src="images/pregnantPole.png" alt="" />
+        </div>
+    )
+}
