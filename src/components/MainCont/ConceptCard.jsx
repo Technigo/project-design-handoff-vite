c859e78a0f86948091cf140f7424ab5444a36c1d
Idah Collin <idah_collin@hotmail.com>
2023-11-01 15:13:50 +0100

    Styling on landing page

--- src/components/MainCont/ConceptCard.jsx
+++ src/components/MainCont/ConceptCard.jsx
@@ -5,5 +5,13 @@ import "./conceptcard.css"
 export const ConceptCard = () => {
     return <div className="conceptcard">
         <img src="./conceptcard1.png" alt="jedi" />
+        <h3>BECOME A JEDI</h3>
     </div>
 }
+
+
+//images to be added to side-scroll:
+//<img src="./conceptcard2.png" alt="image" />
+//<h3>JOIN THE DARK SIDE</h3>
+//<img src="./conceptcard3.png" alt="image" />
+//<h3>THE WAY OF MANDALORE</h3>
