7ed72f7baea4636bade8f018a4133c2c728accbf
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-01 22:19:00 +0100

    mounted/restructured/created cmpnents-LandingPage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,20 @@
-import { Background } from '../components/reusable components/background'
+import { Footer } from '../components/Footer'
+import { MiddleSectionGet } from '../components/MiddleSectionGet'
+import { ArticleSection } from '../components/ArticleSection';
+import { AboveTheFold } from '../components/AboveTheFold';
+import { Header } from '../components/Header';
+import { ExploreFurther } from '../components/ExploreFurther';
 
 export const LandingPage = () => {
+
     return (
-        <>
-            <Background />
-            <div className='text-sm font-bold text-rose'>
-                LandingPage</div>
-        </>
-
-
+        <div className="min-h-screen relative bg-gradient-to-b from-blue-grey to-grey">
+            <Header />
+            <AboveTheFold />
+            <ArticleSection />
+            <MiddleSectionGet />
+            <ExploreFurther />
+            <Footer />
+        </div>
     )
 }
