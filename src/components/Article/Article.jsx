df8d994861451e7ded2fe2080e48c99d20c4c7ec
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-11-01 09:06:57 +0100

    styled artical card for phone

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,18 +1,28 @@
 import "./Article.css"
 import articleData from "../../articleData.json"
 import { ArticleCard } from "./ArticleCard";
+import styled from 'styled-components';
+
+const ArticleWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 10px;
+  max-width: 100%;
+  padding: 38px 15px;
+`
+
 
 let articleArray = articleData.articles;
 console.log(articleArray)
 
 export const Article = () => {
   return (
-    <div className="article-wrapper">
+    <ArticleWrapper className="article-wrapper">
       {articleArray.map((article, index) =>
         <ArticleCard key={index} articles={ article} />
       )}
         
-    </div>
+    </ArticleWrapper>
   
     
   )
--- src/components/Article/ArticleCard.jsx
+++ src/components/Article/ArticleCard.jsx
@@ -1,17 +1,51 @@
 import { Description } from "../../reusableComponents/Description"
 import { Heading } from "../../reusableComponents/Heading"
+import styled from 'styled-components'
 
+const CardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  max-width: 100%;
+  padding: 8px;
+  border-radius: 8px;
+  background-color:#E2DFDF ;
+`
+const TextWrapper = styled.div`
+display:grid;
+grid-template-columns: 1fr;
+margin-left: 10px;
+`
+const ArticleImage = styled.img`
+max-height: fit-content;
+max-width: 100%;
+`
+const ArticleDescription = styled(Description)`
+font-family: Arsenal;
+font-size: 14px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+`
+const ArticleHeading = styled(Heading)`
+font-family: var(--heading-font-family);
+font-size: 25px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+`
 
 export const ArticleCard = (props) => {
   let imageArticle = props.articles.image;
   let titleArticle = props.articles.name;
   let descriptionArticle = props.articles.description;
   return (
-    <div className="card-wrapper">
-      <img src={ imageArticle } alt="Article image " />
-      <Heading text={ titleArticle } />
-      <Description text={descriptionArticle} />
-    </div>
+    <CardWrapper className="card-wrapper">
+      <ArticleImage src={imageArticle} alt="Article image " />
+      <TextWrapper>
+      <ArticleHeading text={ titleArticle } />
+        < ArticleDescription text={descriptionArticle} />
+        </TextWrapper>
+    </CardWrapper>
   )
 }
 
