4e4d81b41459972f28035a4c4f122db048027421
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-11-01 23:19:07 +0100

    remove layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="/about" element={<About />} />
-                    {/* <Route path="*" element={<NoPage />} /> */}
+                    <Route path="*" element={<NoPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
