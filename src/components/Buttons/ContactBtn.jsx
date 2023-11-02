5890d84efd9f2badf641d6c11aff24c651cb0b8d
veronica astrom <v.aastrom@gmail.com>
2023-11-02 18:31:08 +0100

    updating the contact with the form element. no styling done

--- src/components/Buttons/ContactBtn.jsx
+++ src/components/Buttons/ContactBtn.jsx
@@ -2,12 +2,12 @@
 import { PText } from "../Typography/PText";
 import "./button_styling.css";
 
-export const ContactBtn = () => {
+export const ContactBtn = ({ onClick }) => {
     let text_send_btn ="Ask a question";
 
   return (
     <>
-    <button className="send-btn">
+    <button  onClick={onClick} className="send-btn">
                 <PText
                     className="text_btn brown-text"
                     text={text_send_btn}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import callIcon from "../assets/icons/call.svg"
 import chatIcon from "../assets/icons/chat.svg"
 import emailIcon from "../assets/icons/email.svg"
@@ -8,6 +9,12 @@ import { ContactBtn } from "../components/Buttons/contactBtn.jsx";
 import { ChatBtn } from "../components/Buttons/ChatBtn.jsx";
 
 export const Contact = () => {
+
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [isQuestionSubmitted, setIsQuestionSubmitted] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [question, setQuestion] = useState('');
 
   let headline_contact = "Welcome to contact us";
   const contact_info = [
@@ -35,34 +42,82 @@ export const Contact = () => {
       9PM - 5AM CET`,
     }
   ];
+  const handleAskQuestionClick = () => {
+    setIsFormVisible(true);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setIsFormVisible(false);
+    setIsQuestionSubmitted(true);
+  };
 
   return (
     <div className="contact-page-wrapper">
       <div className="contact-content">
         <H2_Headline h2_headline={headline_contact} />
-        <div className="form-content">
+        {isQuestionSubmitted ? (
+          <div>
+            <p>Thanks for your question! We’ll get back to you within 24 hours.</p>
+            <button onClick={() => setIsQuestionSubmitted(false)}>Ask a new question</button>
+          </div>
+        ) : isFormVisible ? (
+          <form onSubmit={handleFormSubmit}>
+            <div className="form-field">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="question">Question</label>
+              <textarea
+                id="question"
+                value={question}
+                onChange={(e) => setQuestion(e.target.value)}
+                rows="5"
+                required
+              />
+            </div>
+            <button type="submit">Send</button>
+          </form>
+        ) : (
           <div className="question contact-info-wrapper">
             <img src={contact_info[0].image} alt={contact_info[0].alt} />
             <div className="contact-info">
               <PText text={contact_info[0].text} />
-              <ContactBtn />
+              <ContactBtn onClick={handleAskQuestionClick} />
             </div>
           </div>
-          <div className="chat contact-info-wrapper">
-            <img src={contact_info[1].image} alt={contact_info[1].alt} />
-            <div className="contact-info">
-              <PText text={contact_info[1].text} />
-              <ChatBtn />
-            </div>
+        )}
+        <div className="chat contact-info-wrapper">
+          <img src={contact_info[1].image} alt={contact_info[1].alt} />
+          <div className="contact-info">
+            <PText text={contact_info[1].text} />
+            <ChatBtn />
           </div>
-          <div className="phone contact-info-wrapper">
-            <img src={contact_info[2].image} alt={contact_info[2].alt} />
-            <div className="contact-info">
-              <PText text={contact_info[2].text} />
-            </div>
+        </div>
+        <div className="phone contact-info-wrapper">
+          <img src={contact_info[2].image} alt={contact_info[2].alt} />
+          <div className="contact-info">
+            <PText text={contact_info[2].text} />
           </div>
         </div>
       </div>
     </div>
-  );
+  ); 
 }
