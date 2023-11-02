a3e4031e92f2f98d8b4f64be8471499e9b20683a
Emmy Jansson <emmybengtsson@live.se>
2023-11-02 20:20:45 +0100

    pictures

--- src/Components.jsx/Header.jsx
+++ src/Components.jsx/Header.jsx
@@ -5,34 +5,35 @@ export const Header = () => {
     <div className="header">
       <img
         className="bubbles"
-        src="./src/Images/Gym-bubble.png"
+        src="./public/Images/Gym-bubble.png"
         alt="Gym bubble"
       />
-      <img className="bubbles" src="./src/Images/Spa-1.png" alt="Spa bubble" />
       <img
         className="bubbles"
-        src="./src/Images/Co-working.png"
+        src="./public/Images/Spa-1.png"
+        alt="Spa bubble"
+      />
+      <img
+        className="bubbles"
+        src="./public/Images/Co-working.png"
         alt="Co work bubble"
       />
       <img
         className="bubbles"
-        src="./src/Images/After-hours-1.png"
+        src="./public/Images/After-hours-1.png"
         alt="After hours bubble"
       />
       ;
       <div className="header-text">
         <h1>An elevated gym experience.</h1>
         <h1>Everything in one place. </h1>
-        <p>
-          An elevated gym experience. Everything in one place. Up to 67 % of
-          traditional gym memberships go completely unused.{" "}
-        </p>
+        <p>Up to 67 % of traditional gym memberships go completely unused.</p>
         <p>
           Many gyms build their business model on the bet that most people will
           stay home. We don’t.
         </p>
       </div>
-      <img className="vector" src="./src/Images/Vector-2.png" alt="Line" />
+      <img className="vector" src="./public/Images/Vector-2.png" alt="Line" />
     </div>
   );
 };
--- src/Components.jsx/InfoPage.jsx
+++ src/Components.jsx/InfoPage.jsx
@@ -2,34 +2,71 @@ import "./InfoPage.css";
 
 export const InfoPage = () => {
   return (
-    <div className="breakfast">
-      <h2>Breakfast</h2>
-      <p>
-        Start your day with our breakfast buffet starting from 5:00 AM,
-        including a juice bar and a Protein Bar for post-workout energy.
-      </p>
-      <button className="read-more-button">Read more</button>
-      <img src="./src/Images/Breakfast-1.png" alt="Breakfast image" />
-      <img src="./src/Images/Breakfast-2.png" alt="Breakfast image" />
+    <div className="info-page">
+      <div className="grid-container">
+        <div className="grid-item">
+          <img src="./public/Images/Breakfast-1.png" alt="Breakfast image" />
+        </div>
+        <div className="grid-item">
+          <h2>Breakfast</h2>
+          <p>
+            Start your day with our breakfast buffet starting from 5:00 AM,
+            including a juice bar and a Protein Bar for post-workout energy.
+          </p>
+          <button className="read-more-button">Read more</button>
+        </div>
 
-      <div className="gym">
-        <img src="./src/Images/Gym-1.png" alt="Gym image" />
-        <img src="./src/Images/Gym-2.png" alt="Gym image" />
-        <h2>The Gym</h2>
-        <p>
-          Transform your fitness journey at our state-of-the-art gym, the newest
-          machines, yoga area and an open snack bar.
-        </p>
-        <button className="read-more-button">Read more</button>
-        <div className="co-working">
+        <div className="grid-item">
+          <img src="./public/Images/Gym-1.png" alt="Gym image" />
+        </div>
+        <div className="grid-item">
+          <h2>The Gym</h2>
+          <p>
+            Transform your fitness journey at our state-of-the-art gym, the
+            newest machines, yoga area, and an open snack bar.
+          </p>
+          <button className="read-more-button">Read more</button>
+        </div>
+
+        <div className="grid-item">
+          <img src="./public/Images/Co-working.png" alt="Co-working image" />
+        </div>
+        <div className="grid-item">
           <h2>Co-working</h2>
           <p>
             Elevate your productivity at our cutting-edge co-working space, an
-            open space combined with private meeting areas, open fireplaces and
+            open space combined with private meeting areas, open fireplaces, and
             much more.
           </p>
           <button className="read-more-button">Read more</button>
-          <img src="./src/Images/Co-working.png" alt="Gym image" />
+        </div>
+
+        <div className="grid-item">
+          <img src="./public/Images/Spa-3.png" alt="Spa image" />
+        </div>
+        <div className="grid-item">
+          <h2>Spa & Wellness</h2>
+          <p>
+            Discover tranquility at our spa and wellness area, unwind after a
+            good workout, a long workday, or whenever life calls for it.
+          </p>
+          <button className="read-more-button">Read more</button>
+        </div>
+
+        <div className="grid-item">
+          <img
+            src="./public/Images/After-hours-1.png"
+            alt="After hours image"
+          />
+        </div>
+        <div className="grid-item">
+          <h2>After Hours</h2>
+          <p>
+            Join us for after-hours in our cocktail bar with DJ or live
+            sessions. Open every Friday for everyone and Saturday’s for members
+            only.
+          </p>
+          <button className="read-more-button">Read more</button>
         </div>
       </div>
     </div>
