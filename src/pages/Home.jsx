98cf3783496176a31a8d12645a4b1c49e761145d
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-11-02 23:57:43 +0100

    changed to section element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,14 +23,14 @@ const Home = () => {
 
     return (
         <>
-            <div className="bg-cover px-4 py-16" style={{ backgroundImage: `url(${HeaderPhoto})` }}>
+            <section className="bg-cover px-4 py-16" style={{ backgroundImage: `url(${HeaderPhoto})` }}>
                 <h1 className="font-bold text-white-n text-2xl w-32 mb-4">Master the Pilates fundamentals</h1>
                 <h2 className="semi-bold text-white-n text-lg mb-4">Sculpt, sweat and flow it out on the mat with the help of 100+ online classes. Available whenever you want.</h2>
                 <button className="text-white-n bg-blue-n px-8 py-3 rounded-full hover:bg-beige-n hover:text-blue-n hover:underline" >JOIN THE MOVEMENT</button>
-            </div>
-            <div className="py-4 border-y-4 border-green-n overflow-hidden">
+            </section>
+            <section className="py-4 border-y-4 border-green-n overflow-hidden">
                 <p className={`text-md font-bold whitespace-nowrap ${styles.scroll}`}>{`CLASSES: ${[...classes, ...classes].join("  •  ")}`}</p>
-            </div>
+            </section>
             <section className="flex">
                 <img
                     className="w-1/3 object-contain"
@@ -45,9 +45,9 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <div className="py-4 border-y-4 border-green-n overflow-hidden">
+            <section className="py-4 border-y-4 border-green-n overflow-hidden">
                 <p className={`text-md font-bold whitespace-nowrap ${styles.scroll}`}>{`BENEFITS: ${[...benefits, ...benefits].join("  •  ")}`}</p>
-            </div>
+            </section>
         </>
     )
 }
