30fd41d1891a43c586f2bd4dd32c1a8025d1053c
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-11-03 05:10:33 +0700

    update components, styling and app.jsx
    update components, styling and app.jsx

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import aboutUsImage from '../assets/image/aboutUs.png';
-// import LogoSVG from '../assets/image/logotype.svg';
-// import '../styles/navBar.css';
-
-
-// export const NavBar = () => {
-//     return (
-//         <div>
-//             <div className="aboutus-image-wrapper">
-//                 <div className="navbar-image">
-//                     <div className="navbar-wrapper">
-//                         <img src={LogoSVG} alt="logo" />
-//                         <span className="navBar">ABOUTUS</span>
-//                         <span className="navBar_2">EXPLORE</span>
-//                         <span className="navBar_3">SCHEDULE</span>
-//                         <span className="navBar_4">PRICES</span>
-//                     </div>
-//                     <div className="aboutus-text">
-//                         <div className="aboutus-text-wrapper">
-//                             <h1 className="aboutus-text-head">Your Journey to inner balance begins here</h1>
-//                         </div>
-//                         <img className="aboutus-image" src={aboutUsImage} alt="about us" />
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
 import React from 'react';
 import aboutUsImage from '../assets/image/aboutUs.png';
 import LogoSVG from '../assets/image/logotype.svg';
@@ -40,10 +10,12 @@ export const NavBar = () => {
                 <div className="navbar-image">
                     <div className="navbar-wrapper">
                         <img src={LogoSVG} alt="logo" />
-                        <span className="navBar">ABOUTUS</span>
+                        <div className="navBar-list">
+                        <span className="navBar_1">ABOUT US</span>
                         <span className="navBar_2">EXPLORE</span>
                         <span className="navBar_3">SCHEDULE</span>
                         <span className="navBar_4">PRICES</span>
+                        </div>
                     </div>
                     <div className="aboutus-text">
                         <div className="aboutus-text-wrapper">
