18163efa74974a524e492b82b7985e25d28c763d
jennylarsen <jennymarikalarsen@gmail.com>
2023-11-02 16:54:34 +0100

    updated styling in love and kundalini yoga section

--- src/pages/Home/SectionsHome/KundaliniYoga.jsx
+++ src/pages/Home/SectionsHome/KundaliniYoga.jsx
@@ -4,23 +4,43 @@ import styled from "styled-components";
 
 const KundaliniYogaSection = styled.section`
 display: flex;
+padding-top: 40px;
 flex-direction: column;
 align-items: center;
 gap: 40px;
 background: var(--cobalt, #0021F5);
-color: white`
+color: white`;
 
-const kundaliniYogaNormalText = "Kundalini Yoga is all about transforming energy within yourself. When being a regular practitioner of Kundalini Yoga you have an efficient way of activating your life force and building up stamina to transform that amount of energy in your daily life."
+
+const kundaliniYogaNormalText = `Kundalini Yoga is all about
+transforming energy within yourself.
+When being a regular practitioner of Kundalini Yoga,
+you have an efficient way of activating your life force
+and building up stamina to transform
+that amount of energy in your daily life.`;
+
+const TellMeEverything = styled.div`
+color: #FFF;
+font-family: Metal Mania;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 30px; /* 187.5% */
+text-decoration-line: underline;
+`;
 
 export const KundaliniYoga = () => {
   return (
     <>
     <KundaliniYogaSection>
-    <SubHeading text={"KUNDALINI YOGA"}/>
+    <SubHeading text={'KUNDALINI YOGA'}/>
     <NormalText text={kundaliniYogaNormalText}/>
+    <TellMeEverything>
+    Tell me EVERYTHING
+    </TellMeEverything>
     </KundaliniYogaSection>
     </>
   );
 };
 
-//OBS GLÖM EJ TELL ME EVERYTHING
+
--- src/pages/Home/SectionsHome/Love.jsx
+++ src/pages/Home/SectionsHome/Love.jsx
@@ -4,7 +4,30 @@ import styled from "styled-components";
 
 const LoveSection = styled.section`
 background: var(--Marine, #141832);
-color: white`
+color: white;
+padding-top: 24px;
+padding-bottom: 52px;
+`;
+
+const LoveSubheading = styled.div`
+color: var(--fire);
+text-align: center;
+font-family: Metal Mania;
+font-size: 30px;
+font-style: normal;
+font-weight: 400;
+line-height: 30px; /* 100% */`
+
+const LoveName = styled.div`
+color: #FFF;
+font-family: JetBrains Mono;
+font-size: 16px;
+font-style: normal;
+font-weight: 800;
+line-height: 30px;
+text-align: center;
+`
+
 
 const LoveNormalText = "Having a daily Sadhana has totally changed my life. I am in love with my sacred morning practice."
 
@@ -12,8 +35,13 @@ export const Love = () => {
   return (
     <>
     <LoveSection>
-    <SubHeading text={"LOVE"}/>
+    <LoveSubheading>
+      LOVE
+    </LoveSubheading>
     <NormalText text={LoveNormalText}/>
+    <LoveName>
+      Maria
+    </LoveName>
     </LoveSection>
     </>
   );
