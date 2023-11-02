a3a5f6324b53209d951ab200774eef4d9db2be62
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-03 00:41:15 +0100

    replaced divs by br in Footer h5

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 export const Footer = () => {
     return (
         <div className="mx-auto mt-auto pb-16 flex flex-col items-center justify-center w-5/6 font-raleway text-white text-base font-medium space-y-5">
-            <img src="icons/horizontalRule.svg" alt="horizontal rule" />
+            <img src="icons/horizontalRule.svg" alt="horizontal rule" className="" />
             <h4>LETS CONNECT</h4>
             <div className="flex flex-row justify-center items-center gap-4 mx-auto">
 
@@ -26,18 +26,17 @@ export const Footer = () => {
 
             <img src="icons/horizontalRule.svg" alt="horizontal rule" className="" />
             <h4>STUDIO VASASTAN</h4>
-            <div className="mx-auto flex flex-col justify-center items-center font-sourcesans3 font-normal text-sm space-y-5">
-                <div className="adress1">
-                    <h5>Hagagatan 16</h5>
-                    <h5>113 48 Stockholm</h5>
-                </div>
+            <div className="mx-auto flex flex-col justify-center items-center font-sourcesans3 font-normal text-sm">
 
-                <div className="adress2">
-                    <h5>073-7624047</h5>
-                </div>
-                <div className="adress3">
-                    <h5>info@nordicpoleacademy.com</h5>
-                </div>
+                <h5>Hagagatan 16</h5>
+                <h5>113 48 Stockholm</h5>
+                <br />
+
+                <h5>073-7624047</h5>
+                <br />
+
+                <h5>info@nordicpoleacademy.com</h5>
+
             </div>
 
             <img src="icons/horizontalRule.svg" alt="horizontal rule" />
