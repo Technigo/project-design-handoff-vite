6cce3613e2f8313f5fc28e03ea7d8196b0c77d70
isaackamran <isaackamran@hotmail.com>
2023-11-02 10:19:57 +0100

    translation working

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,34 +1,40 @@
-// About us, Play Parkour, FAQ, Contact information och språk
 import styled from "styled-components";
 import data from "../assets/images.json";
-import { Translation } from "./Translation";
+import { useTranslation } from 'react-i18next';
 
 const FootBar = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-background: rgba(138, 201, 38, 0.60);
-width: 547px;
-height: 160px;
-padding: 0 20px;
-`
-const LogoImg = styled.img`
-width: 76px;
-height: 76px;
-border-radius: 120px;
-margin-left: 20px;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: rgba(138, 201, 38, 0.60);
+  width: 547px;
+  height: 160px;
+  padding: 0 20px;
+`;
 
+const LogoImg = styled.img`
+  width: 76px;
+  height: 76px;
+  border-radius: 120px;
+  margin-left: 20px;
+`;
 
 export const Footer = () => {
+  const { t, i18n } = useTranslation();
 
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
 
   return (
-<>
-<FootBar>
-<LogoImg src={data['logo']} alt="Logo" />
-<Translation/>
-</FootBar>
-</>
-  )
-}
+    <FootBar>
+      <LogoImg src={data['logo']} alt="Logo" />
+      <h1>{t("head6")}</h1>
+      <div>
+        <button onClick={() => changeLanguage("en")}>English</button>
+        <button onClick={() => changeLanguage("es")}>Spanish</button>
+        <button onClick={() => changeLanguage("sv")}>Swedish</button>
+      </div>
+    </FootBar>
+  );
+};
--- src/Component/Translation.jsx
+++ src/Component/Translation.jsx
@@ -1,19 +1,43 @@
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
 
 export const Translation = () => {
-  const { t, i18n } = useTranslation()
+  const { t, i18n } = useTranslation();
+
+  const textKeys = ["head1",
+  "paragraph1",
+  "head2",
+  "paragraph2",
+  "paragraph3",
+  "paragraph4",
+  "head3",
+"head4:",
+"paragraph5:",
+"head5:",
+"paragraph6:",
+"paragraph7:",
+"head6:",
+"paragraph8:",
+];
+  const languages = ["en", "es", "sv"];
 
   const changeLanguage = (language) => {
-    i18n.changeLanguage(language)
-  }
-
+    i18n.changeLanguage(language);
+  };
 
   return (
-  <>
-  <h1>{t("head1")}</h1>
-  <button onClick={() => changeLanguage("en")}>English</button>
-  <button onClick={() => changeLanguage("es")}>Spanish</button>
-  <button onClick={() => changeLanguage("sv")}>Swedish</button>
-  </>
-  )  
-}
+    <>
+      {textKeys.map((key, index) => (
+        <div key={index}>
+          <h1>{t(key)}</h1>
+        </div>
+      ))}
+      <div>
+        {languages.map((language, index) => (
+          <button key={index} onClick={() => changeLanguage(language)}>
+            {language}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
