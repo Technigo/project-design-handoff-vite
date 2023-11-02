eb449b86be692a4a6e6324233e869d745b1c31fc
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-11-02 22:38:08 +0100

    Added banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,34 @@
 import HeaderPhoto from "../assets/header-photo.png";
+import styles from "./Home.module.css";
 
 const Home = () => {
+    const classes = [
+        "CORE FLEX",
+        "PRE NATAL",
+        "POST NATAL",
+        "FOUNDATION",
+        "BREATH WORK",
+        "LOW, MEDIUM AND HIGH INTENSITY",
+        "CLASSES",
+    ];
+
+    const benefits = [
+        "CORE STRENGTH",
+        "INCREASED ENERGY",
+        "IMPROVED FLEXIBILITY AND MOBILITY",
+        "IMPROVED BALANCE",
+        "STRONG POSTURE",
+    ];
+
     return (
         <>
             <div className="bg-cover px-4 py-16" style={{ backgroundImage: `url(${HeaderPhoto})` }}>
                 <h1 className="font-bold text-white-n text-2xl w-32 mb-4">Master the Pilates fundamentals</h1>
-                <h2 className="semi-bold text-white-n text-lg drop-shadow-2xl mb-4">Sculpt, sweat and flow it out on the mat with the help of 100+ online classes. Available whenever you want.</h2>
+                <h2 className="semi-bold text-white-n text-lg mb-4">Sculpt, sweat and flow it out on the mat with the help of 100+ online classes. Available whenever you want.</h2>
                 <button className="text-white-n bg-blue-n px-8 py-3 rounded-full hover:bg-beige-n hover:text-blue-n hover:underline" >JOIN THE MOVEMENT</button>
+            </div>
+            <div className="py-4 border-y-4 border-green-n overflow-hidden">
+                <p className={`text-md font-bold whitespace-nowrap ${styles.scroll}`}>{`CLASSES: ${classes.join("  •  ")}`}</p>
             </div>
         </>
     )
