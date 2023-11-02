9dd9cec39cd808646fab207e947aea097cddca07
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-03 00:33:32 +0100

    installing flowbite for navbar solution-(unused)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     colors: {
@@ -21,6 +22,8 @@ export default {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
 
