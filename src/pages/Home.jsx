73441cb8c5d239333448d6c944b12af06b4796d8
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-11-02 21:50:37 +0100

    Started on header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
+import HeaderPhoto from "../assets/header-photo.png";
+
 const Home = () => {
     return (
-        <p>Home</p>
+        <>
+            <div className="bg-cover px-4 py-16" style={{ backgroundImage: `url(${HeaderPhoto})` }}>
+                <h1 className="font-bold text-white-n text-2xl w-32 mb-4">Master the Pilates fundamentals</h1>
+                <h2 className="semi-bold text-white-n text-lg">Sculpt, sweat and flow it out on the mat with the help of 100+ online classes. Available whenever you want.</h2>
+            </div>
+        </>
     )
 }
 
