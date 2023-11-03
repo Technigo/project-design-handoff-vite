b4dc9fe51d29a40b91dac4d95d84dfcc53185d4d
Minadarabi <minaadabirii@gmail.com>
2023-11-03 12:43:23 -0700

    fixed animation and footer size

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate} from "react-router-dom"
 
+
 export const Header = () => {
     const nav = useNavigate()
     const [ open, setOpen ] = useState(false)
@@ -11,6 +12,20 @@ export const Header = () => {
         <img onClick={()=>{nav("/")}} className="logo" src="/LOGO.svg" alt="logo" />
 
         </div>
+       <nav className="navbar">
+       <ul className='nav-list'>
+
+            <li><a href="">GET STARTED</a></li>
+            <li><a href="">MEMBERSHIP</a></li>
+            <li><a href="">INDIVIDUAL COACHING</a></li>
+            <li><a href="">NUTRITION</a></li>
+            <li><a href="">CORPORATE</a></li>
+            <li><a href="">COACHES</a></li>
+        </ul>
+        <button onClick={()=>{nav("/contact")}} className="trial-btn">BOOK A FREE TRIAL</button>
+
+
+       </nav>
         <div className="bar-container">
             <i className="fa-solid fa-bars bar-btn" onClick={() => setOpen( (open) => !open)} ></i>
         </div>
