d90e720a6f48cfef5f83087089e1b2f05cfa753d
majoh23 <139242342+majoh23@users.noreply.github.com>
2023-11-03 15:03:51 +0100

    Test

--- src/components/HamburgerIcon.jsx
+++ src/components/HamburgerIcon.jsx
@@ -1,7 +1,7 @@
 import { ReactSVG } from 'react-svg';
 
 const HamburgerIcon = (props) => (
-  <ReactSVG src="/src/assets/hamburger.svg" {...props} />
+  <ReactSVG src="./src/assets/hamburger.svg" {...props} />
 );
 
 export default HamburgerIcon;
