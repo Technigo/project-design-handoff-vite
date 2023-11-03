31ef00a2706a3900cd5f5f6275f48e056f8ead16
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-03 23:33:18 +0100

    translated ArticleSection comp +updated imports

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,10 +1,16 @@
-import { HeadingArticle } from "./reusable components/HeadingArticle"
-import { SubHeadingArticle } from "./reusable components/SubHeadingArticle"
-import { Buttons } from "./reusable components/buttons"
+import { useTranslation } from "react-i18next"
+import { HeadingArticle } from "./reusableComponents/HeadingArticle"
+import { SubHeadingArticle } from "./reusableComponents/SubHeadingArticle"
+import { Buttons } from "./reusableComponents/Buttons"
 
 export const ArticleSection = () => {
-    const heading = "Welcome to Nordic Pole Academy"
-    const subHeading = "Join our stockholm based pole community to unlock your potential no matter what your amibition!"
+
+    const { t } = useTranslation();
+    const article = t("article", { returnObjects: true })
+
+    const heading = t(article.welcome);
+    const subHeading = t(article.join);
+
     return (
         <div className="mx-4 flex flex-col justify-center items-center">
             <HeadingArticle articleHeading={heading} />
