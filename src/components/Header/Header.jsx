4ec5ef5609183957156262ce5dcf46fa14d0fd4b
olgalepisto <olga.lepisto@gmail.com>
2023-11-04 00:49:49 +0200

    defined existing CSS elements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,9 @@ export const Header = () => {
     return (
         <div className={styles.mainContainer}>
                 <img src="src/assets/chalkyLogo.svg" alt="Chalky logo" />
-                <img src="src/assets/heroImage.svg" alt="Chalky header image" />
             <div className={styles.infoContainer}>
-                <div className={styles.headingContainer}>
                     <h1>Reach new heights with us!</h1>
-                </div>
-                <div className={styles.textContainer}>
                     <p>Whether you're a curious first-timer or a seasoned climber seeking new challenges, our dedicated team of experts is here to guide you every grip of the way.</p>
-                </div>
                 <div className={styles.buttonContainer}>
                     <button className={styles.tryForFree}>TRY FOR FREE</button>
                     <button className={styles.aboutUs}>ABOUT US</button>
