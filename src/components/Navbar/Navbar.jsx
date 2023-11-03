a16b8a9b4ada879d537393a7aeab7bbb53ee4ead
Idah Collin <idah_collin@hotmail.com>
2023-11-03 20:35:28 +0100

    Header solved

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,16 +3,20 @@
 
 import { Link } from "react-router-dom"
 import { Logo } from "../Logo"
-import { BurgerMenu } from "../Burger/BurgerMenu"
+
 import "./navbar.css"
 
 export const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="menu-toggle"><Link to="/about"><BurgerMenu /></Link></div>
+            <div className="menu-toggle"><Link to="/about">≡</Link></div>
             <div className="navbar-logo"><Link to="/"><Logo /></Link></div>
             <div className="navbar-profile"><img src="/dvicon.png" alt="profile" /></div>
         </div>
     )
 }
+
+//If burger menu was to be used, left in learning purpose:
+//import { BurgerMenu } from "../Burger/BurgerMenu"
+//<BurgerMenu />
 
