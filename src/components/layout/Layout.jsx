f9c4acb76bb1aa3b436f9392d58250edab2c332b
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-11-04 21:45:04 +0100

    Completed 1st page

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -9,26 +9,37 @@ import Youtube from "../../assets/youtube.svg?react";
 
 const Layout = () => {
     return (
-        <div className="flex flex-col h-screen justify-between">
+        <>
             <div>
-                <header className="bg-green-n w-full">
+                <header className="bg-green-n w-full sticky top-0 z-10">
                     <div className="mx-auto p-4 flex justify-between items-center">
                         <Link to="/">
                             <Logo />
                         </Link>
-                        <Hamburger className="h-8 w-8 hover:cursor-pointer hover:text-blue-n" />
+                        <div className="hidden md:flex gap-4">
+                            <Link to="/" className="font-bold py-3 px-2 rounded-full hover:text-blue-n hover:underline underline-offset-2 transition-all ease-out duration-300 flex gap-1">
+                                <span>CLASSES</span>
+                                <ArrowDown />
+                            </Link>
+                            <Link to="/about" className="font-bold px-2 py-3 rounded-full hover:text-blue-n hover:underline underline-offset-2 transition-all ease-out duration-300">ABOUT US</Link>
+                            <Link to="/" className="font-bold text-white-n border-2 text-blue-n border-blue-n px-8 py-3 rounded-full  hover:text-white-n hover:underline underline-offset-2 transition-all ease-out duration-300">LOG IN</Link>
+                            <Link to="/" className="font-bold text-white-n bg-blue-n px-8 py-3 rounded-full hover:bg-beige-n hover:text-blue-n hover:underline underline-offset-2 transition-all ease-out duration-300">SIGN UP</Link>
+                        </div>
+                        <div className="md:hidden">
+                            <Hamburger className="h-8 w-8 hover:cursor-pointer hover:text-blue-n transition-all ease-out duration-300" />
+                        </div>
                     </div>
                 </header>
                 <main>
                     <Outlet />
                 </main>
             </div>
-            <footer className="flex justify-between bg-green-n w-full p-4 items-center font-semibold">
+            <footer className="flex justify-between bg-green-n w-full p-4 items-center font-semibold sticky">
                 <div>
                     <div className="flex gap-4 items-center mb-4">
-                        <Link className="hover:text-blue-n" to="/about">ABOUT US</Link>
-                        <p className="cursor-pointer hover:text-blue-n">FAQ</p>
-                        <p className="flex items-center cursor-pointer hover:text-blue-n">
+                        <Link className="hover:text-blue-n transition-all ease-out duration-300" to="/about">ABOUT US</Link>
+                        <p className="cursor-pointer hover:text-blue-n transition-all ease-out duration-300">FAQ</p>
+                        <p className="flex items-center cursor-pointer hover:text-blue-n transition-all ease-out duration-300">
                             <Planet />
                             <span className="mx-2">ENGLISH</span>
                             <ArrowDown />
@@ -37,9 +48,9 @@ const Layout = () => {
                     <div>
                         <p className="mb-2">JOIN OUR COMMUNITY</p>
                         <div className="flex gap-4">
-                            <Facebook className="cursor-pointer hover:text-blue-n" />
-                            <Instagram className="cursor-pointer hover:text-blue-n" />
-                            <Youtube className="cursor-pointer hover:text-blue-n" />
+                            <Facebook className="cursor-pointer hover:text-blue-n transition-all ease-out duration-300" />
+                            <Instagram className="cursor-pointer hover:text-blue-n transition-all ease-out duration-300" />
+                            <Youtube className="cursor-pointer hover:text-blue-n transition-all ease-out duration-300" />
                         </div>
                     </div>
                 </div>
@@ -47,7 +58,7 @@ const Layout = () => {
                     <Logo />
                 </Link>
             </footer>
-        </div>
+        </>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import HeaderPhoto from "../assets/header-photo.png";
-import iPhone from "../assets/iphone-16.png";
+import iPhone from "../assets/iphone.png";
 import Yoga from "../assets/yoga.png";
-import Appstore from "../assets/appstore.svg?react";
-import GooglePlay from "../assets/googleplay.svg?react";
+import AppstoreDark from "../assets/app-store-dark.svg?react";
+import AppstoreLight from "../assets/app-store-light.svg?react";
+import GooglePlayDark from "../assets/google-play-dark.svg?react";
+import GooglePlayLight from "../assets/google-play-light.svg?react";
 import RightArrow from "../assets/right-arrow.svg?react";
 import LeftArrow from "../assets/left-arrow.svg?react";
 import styles from "./Home.module.css";
-import { App } from "../App";
-
 
 const Home = () => {
     const classes = [
@@ -36,59 +36,69 @@ const Home = () => {
                     src={HeaderPhoto}
                 />
                 <div className="absolute inset-0 bg-gray-700 opacity-50 rounded-md" />
-                <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="p-4">
+                <div className="absolute inset-0 flex items-center px-4 lg:px-32">
+                    <div className="w-2/3 md:w-1/3">
                         <h1 className="font-bold text-white-n text-2xl w-32 mb-4">Master the Pilates fundamentals</h1>
-                        <h2 className="semi-bold text-white-n text-lg mb-4">Sculpt, sweat and flow it out on the mat with the help of 100+ online classes. Available whenever you want.</h2>
-                        <button className="font-bold text-white-n bg-blue-n px-8 py-3 rounded-full hover:bg-beige-n hover:text-blue-n hover:underline underline-offset-2">JOIN THE MOVEMENT</button>
+                        <h2 className="semi-bold text-white-n text-lg mb-6">Sculpt, sweat and flow it out on the mat with the help of 100+ online classes. Available whenever you want.</h2>
+                        <button className="font-bold text-white-n bg-blue-n px-8 py-3 rounded-full hover:bg-beige-n hover:text-blue-n hover:underline underline-offset-2 transition-all ease-out duration-300">JOIN THE MOVEMENT</button>
                     </div>
                 </div>
             </section>
             <section className="py-4 border-y-4 border-green-n overflow-hidden">
                 <p className={`text-md font-bold whitespace-nowrap ${styles.scroll}`}>{`CLASSES: ${[...classes, ...classes].join("  •  ")}`}</p>
             </section>
-            <section className="flex flex-col md:flex-row p-4">
+            <section className="flex flex-col md:flex-row px-4 py-8 gap-4">
                 <div className="flex mb-4 md:mb-0">
                     <img
-                        className="w-1/3 object-contain"
+                        className="w-1/2 object-contain"
                         src={iPhone}
                         alt="Photo of an iPhone"
                     />
                     <div className="flex items-center">
                         <div>
-                            <p className="text-lg -ms-16 mb-4">Different levels tailored to your needs</p>
-                            <p className="text-lg -ms-8 mb-4">Low impact sessions ranging from 15-45 minutes</p>
-                            <p className="text-lg ps-4">Certified instructors teaching you the fundamentals of Pilates</p>
+                            <p className="text-lg -ms-12 mb-4 text-xl">Different levels tailored to your needs</p>
+                            <p className="text-lg mb-4 text-xl">Low impact sessions ranging from 15-45 minutes</p>
+                            <p className="text-lg ps-12 text-xl">Certified instructors teaching you the fundamentals of Pilates</p>
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-between gap-4">
+                <div className="flex gap-4 items-center">
                     <h1 className="text-3xl">
                         Unlock <b>100+ classes</b> and start <b>moving on the move</b>
                     </h1>
                     <div className="flex flex-col justify-center">
-                        <Appstore className="mb-4 cursor-pointer hover:bg-beige-n hover:text-black" />
-                        <GooglePlay className="cursor-pointer hover:bg-beige-n hover:text-black" />
+                        <div className={`mb-4 cursor-pointer ${styles.downloads}`}>
+                            <AppstoreDark className={styles.dark} />
+                            <AppstoreLight className={styles.light} />
+                        </div>
+                        <div className={`mb-4 cursor-pointer ${styles.downloads}`}>
+                            <GooglePlayDark className={styles.dark} />
+                            <GooglePlayLight className={styles.light} />
+                        </div>
                     </div>
                 </div>
             </section>
             <section className="py-4 border-y-4 border-green-n overflow-hidden">
                 <p className={`text-md font-bold whitespace-nowrap ${styles.scroll}`}>{`BENEFITS: ${[...benefits, ...benefits].join("  •  ")}`}</p>
             </section>
-            <section className="px-4 py-12 flex items-center flex-col">
-                <img className="max-w-[317px] mb-8" src={Yoga} alt="Doing yoga" />
-                <h1 className="font-bold text-3xl mb-4">Testimonials</h1>
-                <div className="flex flex-row items-center gap-6 mb-4">
-                    <LeftArrow className="cursor-pointer h-16 w-16 hover:text-blue-n" />
-                    <p className="italic font-thin">
-                        I love the movements and seeing my body transform before my eyes! I didn’t realise how much my posture could improve until I tried the “Posture class”.
-                    </p>
-                    <RightArrow className="cursor-pointer h-16 w-16 hover:text-blue-n" />
+            <section className="px-8 py-12 flex items-center flex-col md:flex-row gap-8">
+                <img className="max-w-[317px] md:max-w-[337px] lg:max-w-[550px] mb-8" src={Yoga} alt="Doing yoga" />
+                <div className="flex items-center">
+                    <div className="flex flex-col items-center max-w-xl">
+                        <h1 className="font-bold text-3xl mb-4">Testimonials</h1>
+                        <div className="flex flex-row items-center gap-6 mb-8">
+                            <LeftArrow className="cursor-pointer h-16 w-16 hover:text-blue-n" />
+                            <p className="italic font-thin text-xl">
+                                I love the movements and seeing my body transform before my eyes! I didn’t realise how much my posture could improve until I tried the “Posture class”.
+                            </p>
+                            <RightArrow className="cursor-pointer h-16 w-16 hover:text-blue-n" />
+                        </div>
+                        <button className="font-bold text-white-n bg-blue-n px-8 py-3 rounded-full hover:bg-beige-n hover:text-blue-n hover:underline underline-offset-2 transition-all ease-out duration-300">JOIN THE MOVEMENT</button>
+                    </div>
                 </div>
-                <button className="font-bold text-white-n bg-blue-n px-8 py-3 rounded-full hover:bg-beige-n hover:text-blue-n hover:underline underline-offset-2">JOIN THE MOVEMENT</button>
             </section>
         </>
-    )
+    );
 }
 
 export default Home;
