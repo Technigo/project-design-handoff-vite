59937b12ce2a6738c2504d89bda52df6137a851f
Carolina Luna <kroluna@gmail.com>
2023-11-04 12:19:58 +0100

    styling prev and next button

--- src/components/CarouselMoments/CarouselMoments.jsx
+++ src/components/CarouselMoments/CarouselMoments.jsx
@@ -1,86 +1,7 @@
-// import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "./CarouselMoments.css";
-
-// import swipe1Image from "../../assets/images/swipe-1.png";
-// import swipe2Image from "../../assets/images/swipe-2.png";
-// import swipe3Image from "../../assets/images/swipe-3.png";
-// import swipe4Image from "../../assets/images/swipe-4.png";
-// import swipe5Image from "../../assets/images/swipe-5.png";
-// import swipe6Image from "../../assets/images/swipe-6.png";
-// import swipe7Image from "../../assets/images/swipe-7.png";
-// import swipe8Image from "../../assets/images/swipe-8.png";
-// import swipe9Image from "../../assets/images/swipe-9.png";
-// import swipe10Image from "../../assets/images/swipe-10.png";
-// import swipe11Image from "../../assets/images/swipe-11.png";
-
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-// import "swiper/css/scrollbar";
-// import "swiper/css/bundle";
-
-// export const CarouselMoments = () => {
-//   return (
-//     <Swiper
-//       className="swiper-wrapper"
-//       modules={[Navigation, Pagination, Scrollbar, A11y]}
-//       spaceBetween={60}
-//       slidesPerView={2}
-//       centeredSlides={true}
-//       navigation
-//       onSwiper={(swiper) => console.log(swiper)}
-//       onSlideChange={() => console.log("slide change")}>
-//       <SwiperSlide>
-//         <img src={swipe1Image} alt="women sitting in yoga class" />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img src={swipe2Image} alt="women holding hands" />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img src={swipe3Image} alt="woman doing yoga in sunset" />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img src={swipe4Image} alt="woman jumping in sunset" />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img
-//           src={swipe5Image}
-//           alt="woman in yoga position in a big empty rum"
-//         />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img src={swipe6Image} alt="woman sitting in yoga position" />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img src={swipe7Image} alt="hand i yoga position" />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img src={swipe8Image} alt="women in yoga position" />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img src={swipe9Image} alt="flowers" />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img
-//           src={swipe10Image}
-//           alt="woman sitting in yoga position in nature"
-//         />
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         <img
-//           src={swipe11Image}
-//           alt="woman sitting in yoga position in nature"
-//         />
-//       </SwiperSlide>
-//       ...
-//     </Swiper>
-//   );
-// };
-
 import { useState } from "react";
 import "./CarouselMoments.css"; // Assume we have a separate CSS file for styles
-
+import prevImage from "../../assets/prev-btn.svg";
+import nextImage from "../../assets/next-btn.svg";
 // Importing the images
 import swipe1Image from "../../assets/images/swipe-1.png";
 import swipe2Image from "../../assets/images/swipe-2.png";
@@ -108,50 +29,6 @@ const images = [
   swipe11Image,
 ];
 
-// export const CarouselMoments = () => {
-//   const [activeIndex, setActiveIndex] = useState(0);
-
-//   const updateIndex = (newIndex) => {
-//     if (newIndex < 0) {
-//       newIndex = images.length - 1;
-//     } else if (newIndex >= images.length) {
-//       newIndex = 0;
-//     }
-
-//     setActiveIndex(newIndex);
-//   };
-
-//   return (
-//     <div className="carousel">
-//       <div
-//         className="carousel-inner"
-//         style={{
-//           transform: `translateX(-${
-//             activeIndex * (100 + 15 / images.length)
-//           }%)`,
-//         }}>
-//         {images.map((image, index) => (
-//           <img
-//             key={index}
-//             src={image}
-//             alt={`Swipe ${index + 1}`}
-//             className="carousel-item"
-//           />
-//         ))}
-//       </div>
-//       <button
-//         onClick={() => updateIndex(activeIndex - 1)}
-//         className="carousel-control prev">
-//         &#10094;
-//       </button>
-//       <button
-//         onClick={() => updateIndex(activeIndex + 1)}
-//         className="carousel-control next">
-//         &#10095;
-//       </button>
-//     </div>
-//   );
-// };
 export const CarouselMoments = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -170,7 +47,7 @@ export const CarouselMoments = () => {
   const getTransform = () => {
     let offset = activeIndex * (slideWidth + slideMarginRight);
     if (activeIndex === images.length - 1) {
-      offset -= slideMarginRight; // Remove the margin for the last item
+      offset -= slideMarginRight;
     }
     return `translateX(-${offset}%)`;
   };
@@ -191,13 +68,13 @@ export const CarouselMoments = () => {
         onClick={() => updateIndex(activeIndex - 1)}
         className="carousel-control prev"
         disabled={activeIndex === 0}>
-        &#10094;
+        <img src={prevImage} alt="previuos button" className="prev-btn" />
       </button>
       <button
         onClick={() => updateIndex(activeIndex + 1)}
         className="carousel-control next"
         disabled={activeIndex === images.length - 1}>
-        &#10095;
+        <img src={nextImage} alt="next button" className="next-btn" />
       </button>
     </div>
   );
