d26cbf930708b0809ab8572536e9d36d6d8ff51f
Frida <139373483+FridaKristian@users.noreply.github.com>
2023-11-05 22:04:01 +0100

    Added a burger menu in the header with animation as the menu shows

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,16 +1,38 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import logo from "/ff-logo.svg"
-import hamburger from "/hamburger-menu.svg"
 import "./header.css"
 
 export const Header = () => {
+    const [ burgerVisible, setBurgerVisible ] = useState(false)
+    const [ burgerIcon, setBurgerIcon ] = useState(true)
+
+
+    const handleClick = () => {
+        setBurgerVisible(() => burgerVisible ? false : true)
+        setBurgerIcon(() => burgerIcon ? false : true)
+    }
+
     return (
         <>
-        <div className="grid-parent">
+        <div className="grid-parent grid-parent-header">
             <div className="header">
                 <Link to="/"><img className="header-logo" src={logo} alt="Family Fitness Logo" /></Link>
-                <img src={hamburger} alt="Menu" />
+                <div className={`burger-menu-icon ${burgerIcon ? "" : "close"}`} id="burger-menu" onClick={handleClick}><span></span></div>
             </div>
+            {burgerVisible && 
+                <div className={burgerVisible ? "overlay" : ""} id="menu">
+                    <div className="burger-links">
+                        <Link to="/" onClick={handleClick}>Find your center</Link>
+                        <Link to="/" onClick={handleClick}>Membership</Link>
+                        <Link to="/" onClick={handleClick}>MiniFit</Link>
+                        <Link to="/about" onClick={handleClick}>About us</Link>
+                    </div>
+                    <div className="log-out-link">
+                        <Link to="/" onClick={handleClick}>Log out</Link>
+                    </div>
+                </div>
+            }
         </div>
         </>
     )
