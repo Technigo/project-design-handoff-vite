9667343ca87cd72fa5d54d17e5f6aa62bb501dc0
veronica astrom <v.aastrom@gmail.com>
2023-11-05 12:08:59 +0100

    worked on responsiveness for contact page. Not done yet

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,6 +10,7 @@ import { ChatBtn } from "../components/Buttons/ChatBtn.jsx";
 import { SubmitBtn } from '../components/Buttons/SubmitBtn.jsx';
 import { NewQuestionBtn } from '../components/Buttons/NewQuestionBTN.jsx';
 import coffeImg from "../assets/images_mobile/Coffee_img.png";
+import contact_image from "../assets/images_mobile/contact_image2.png"
 import { useTranslation } from "react-i18next";
 
 export const Contact = () => {
@@ -58,99 +59,114 @@ export const Contact = () => {
   };
 
   return (
-    <div className="contact-page-wrapper">
+    <section className="contact-page-wrapper">
       <div className="contact-content">
         <H2_Headline h2_headline={headline_contact} />
-        {isQuestionSubmitted ? (
-          <div className="ask-new-question-wrapper">
-            <img src={coffeImg} alt="" />
-            <div className="contact-info-wrapper">
-              <img src={contact_info[0].image} alt={contact_info[0].alt} />
-              <div className="contact-info">
-                <PText text={t("contact.form_contact.confirmationText")} />
-                <NewQuestionBtn onClick={handleAskQuestionClick} />
+        <div className="contact-image-content-wrapper">
+          {isQuestionSubmitted ?
+            <img className="coffeeimg" src={coffeImg} alt="Coffee cup with the text Thanks" /> :
+            <img
+              src={contact_image}
+              alt="woman doing a tree pose."
+              className="contact-image"
+            />
+          }
+          <div className="contact-page-content-wrapper">
+            {isQuestionSubmitted ? (
+              <>
+                <div className="contact-info-wrapper question-asked-wrapper">
+                  <img src={contact_info[0].image} alt={contact_info[0].alt} />
+                  <div className="contact-info question-asked">
+                    <PText text={t("contact.form_contact.confirmationText")} />
+                    <NewQuestionBtn onClick={handleAskQuestionClick} />
+                  </div>
+                </div>
+              </>
+            ) : isFormVisible ? (
+              <div className="contact-page-form">
+                <div className="contact-info-wrapper">
+                  <img src={contact_info[0].image} alt={contact_info[0].alt} />
+                  <form onSubmit={handleFormSubmit} className="contact-info">
+                    <PText
+                      className="new_line"
+                      text={contact_info[0].text}
+                    />
+                    <div className="form-field">
+                      <label htmlFor="name">
+                        <PText text={t("contact.form_contact.label.0")} />
+                      </label>
+                      <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form-field">
+                      <label htmlFor="email">
+                        <PText text={t("contact.form_contact.label.1")}
+                        />
+                      </label>
+                      <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form-field">
+                      <label htmlFor="question">
+                        <PText text={t("contact.form_contact.label.2")} />
+                      </label>
+                      <textarea
+                        id="question"
+                        value={question}
+                        onChange={(e) => setQuestion(e.target.value)}
+                        rows="5"
+                        required
+                      />
+                    </div>
+                    <SubmitBtn />
+                  </form>
+                </div>
               </div>
-            </div>
-          </div>
-        ) : isFormVisible ? (
-          <div className="contact-info-wrapper">
-            <img src={contact_info[0].image} alt={contact_info[0].alt} />
-            <form onSubmit={handleFormSubmit} className="contact-info">
-              <PText
-                className="new_line"
-                text={contact_info[0].text}
-              />
-              <div className="form-field">
-                <label htmlFor="name">
-                  <PText text={t("contact.form_contact.label.0")} />
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="form-field">
-                <label htmlFor="email">
-                  <PText text={t("contact.form_contact.label.1")}
+            ) : (
+              <div className="ask-question-first contact-info-wrapper">
+                <img src={contact_info[0].image} alt={contact_info[0].alt} />
+                <div className="contact-info">
+                  <PText
+                    text={contact_info[0].text}
                   />
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
+                  <ContactBtn onClick={handleAskQuestionClick} />
+                </div>
               </div>
-              <div className="form-field">
-                <label htmlFor="question">
-                  <PText text={t("contact.form_contact.label.2")} />
-                </label>
-                <textarea
-                  id="question"
-                  value={question}
-                  onChange={(e) => setQuestion(e.target.value)}
-                  rows="5"
-                  required
-                />
+            )}
+            <div className="contact-content-wrapper">
+              <div className="contact-info-wrapper">
+                <img src={contact_info[1].image} alt={contact_info[1].alt} />
+                <div className="contact-info">
+                  <PText
+                    className="new_line"
+                    text={contact_info[1].text}
+                  />
+                  <ChatBtn />
+                </div>
               </div>
-              <SubmitBtn />
-            </form>
-          </div>
-        ) : (
-          <div className="question contact-info-wrapper">
-            <img src={contact_info[0].image} alt={contact_info[0].alt} />
-            <div className="contact-info">
-              <PText
-                text={contact_info[0].text}
-              />
-              <ContactBtn onClick={handleAskQuestionClick} />
+              <div className="contact-info-wrapper">
+                <img src={contact_info[2].image} alt={contact_info[2].alt} />
+                <div className="contact-info">
+                  <PText
+                    className="new_line"
+                    text={contact_info[2].text}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-        )}
-        <div className="chat contact-info-wrapper">
-          <img src={contact_info[1].image} alt={contact_info[1].alt} />
-          <div className="contact-info">
-            <PText
-              className="new_line"
-              text={contact_info[1].text}
-            />
-            <ChatBtn />
-          </div>
-        </div>
-        <div className="phone contact-info-wrapper">
-          <img src={contact_info[2].image} alt={contact_info[2].alt} />
-          <div className="contact-info">
-            <PText
-              className="new_line"
-              text={contact_info[2].text}
-            />
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
