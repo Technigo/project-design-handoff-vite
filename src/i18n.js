0c04c8d725c507833a2dcb11c9447d5a85e59cde
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-11-05 15:01:44 +0100

    troubleshooting deploy

--- src/i18n.js
+++ src/i18n.js
@@ -3,11 +3,11 @@ import { initReactI18next } from "react-i18next";
 
 //Separate JSON translation files
 
+import enTranslations from "./Translations/en.json";
+import noTranslations from "./Translations/no.json";
+
 // import seTranslations from "./Translations/se.json";
 // import daTranslations from "./Translations/da.json";
-
-import noTranslations from "./Translations/no.json";
-import enTranslations from "./Translations/en.json";
 
 // import ptTranslations from "./Translations/pt.json";
 // import mkTranslations from "./Translations/mk.json";
