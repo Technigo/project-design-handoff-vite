4538d90c29f85bf7e532c19cbc265ecafb43baa2
Julia Holm <juliaholm@MacBook-Pro-som-tillhor-Julia.local>
2023-11-06 01:39:38 +0100

    added swiper

--- src/pages/home/scenes/testimonials/Testimonials.jsx
+++ src/pages/home/scenes/testimonials/Testimonials.jsx
@@ -1,9 +1,35 @@
 import { SectionTitle } from "../../../../components/reusable/SectionTitle";
 import "./Testimonials.css";
 import { useTranslation } from "react-i18next";
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 
 export const Testimonials = () => {
   const { t } = useTranslation();
+
+  const swiperConfig = {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 24,
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: "#nav-right",
+      prevEl: "#nav-left",
+    },
+  };
+
+  // Update the swiper configuration for tablet view
+  if (window.innerWidth >= 600) {
+    swiperConfig.slidesPerView = 2;
+  }
+  // Update the swiper configuration for desktop view
+  if (window.innerWidth >= 1024) {
+    swiperConfig.slidesPerView = 3;
+    swiperConfig.centeredSlides = false;
+  }
+
+  new Swiper(".swiper", swiperConfig);
 
   return (
     <section id="testimonials">
@@ -12,24 +38,39 @@ export const Testimonials = () => {
           className="testimonial-title"
           title={t("homePage.testimonials.title")}
         />
-        <div className="testimonials-container">
-          <div className="single-testimonial">
-            <p>{t("homePage.testimonials.testimonial1.text")}</p>
-            <span className="customer-name">
-              {t("homePage.testimonials.testimonial1.customer")}
-            </span>
-          </div>
-          <div className="single-testimonial">
-            <p>{t("homePage.testimonials.testimonial2.text")}</p>
-            <span className="customer-name">
-              {t("homePage.testimonials.testimonial2.customer")}
-            </span>
-          </div>
-          <div className="single-testimonial">
-            <p>{t("homePage.testimonials.testimonial3.text")}</p>
-            <span className="customer-name">
-              {t("homePage.testimonials.testimonial3.customer")}
-            </span>
+        <div className="swiper-custom-nav">
+          <RiArrowLeftSLine id="nav-left" className="swiper-icon" />
+          <RiArrowRightSLine id="nav-right" className="swiper-icon" />
+        </div>
+        <div className="swiper">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">
+              <div className="single-testimonial">
+                <p>{t("homePage.testimonials.testimonial1.text")}</p>
+                <br />
+                <span className="customer-name">
+                  {t("homePage.testimonials.testimonial1.customer")}
+                </span>
+              </div>
+            </div>
+            <div className="swiper-slide">
+              <div className="single-testimonial">
+                <p>{t("homePage.testimonials.testimonial2.text")}</p>
+                <br />
+                <span className="customer-name">
+                  {t("homePage.testimonials.testimonial2.customer")}
+                </span>
+              </div>
+            </div>
+            <div className="swiper-slide">
+              <div className="single-testimonial">
+                <p>{t("homePage.testimonials.testimonial3.text")}</p>
+                <br />
+                <span className="customer-name">
+                  {t("homePage.testimonials.testimonial3.customer")}
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
