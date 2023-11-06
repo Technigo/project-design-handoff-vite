2648316573926e366d0b2230369913c7eddcf1ea
Julia Holm <juliaholm@MacBook-Pro-som-tillhor-Julia.local>
2023-11-06 02:19:35 +0100

    added more links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,38 @@
-import {
-  RiArrowDownSLine,
-  RiInstagramFill,
-  RiSpotifyFill,
-} from "react-icons/ri";
+import { RiInstagramFill, RiSpotifyFill } from "react-icons/ri";
 import "./Footer.css";
 import { useTranslation } from "react-i18next";
 import { LanguageSwitcher } from "../LanguageSwitcher";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   const { t } = useTranslation();
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
 
   return (
     <footer>
       <nav className="navbar-footer">
         <LanguageSwitcher />
         <ul>
-          <li>{t("footer.nav1")}</li>
+          <Link to="/" onClick={scrollToTop}>
+            <li>{t("footer.nav1")}</li>
+          </Link>
           <li>{t("footer.nav2")}</li>
           <li>{t("footer.nav3")}</li>
         </ul>
       </nav>
       <div className="social-icons">
-        <RiInstagramFill className="icon" />
-        <RiSpotifyFill className="icon" />
+        <a href="https://www.instagram.com/inkapetrarebecca/" target="_blank">
+          <RiInstagramFill className="icon" />
+        </a>
+        <a
+          href="https://open.spotify.com/user/eer0ds8nf29bby3op5iil4rxn?si=f4e01785b6b84794&nd=1"
+          target="_blank"
+        >
+          <RiSpotifyFill className="icon" />
+        </a>
       </div>
     </footer>
   );
