23c3cf394d615bae19577b6d32ce8cb5130181b1
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-17 10:38:49 +0100

    adding responsiveness to explorefurther section

--- src/components/ExploreFurther.jsx
+++ src/components/ExploreFurther.jsx
@@ -1,23 +1,21 @@
-import { useTranslation } from "react-i18next"
-import { ClassesPictures } from "./reusableComponents/ClassesPictures"
-import { HeadingBottom } from "./reusableComponents/HeadingBottom"
-import { NewsPictures } from "./reusableComponents/NewsPictures"
+import { useTranslation } from "react-i18next";
+import { ClassesPictures } from "./reusableComponents/ClassesPictures";
+import { HeadingBottom } from "./reusableComponents/HeadingBottom";
+import { NewsPictures } from "./reusableComponents/NewsPictures";
 
 export const ExploreFurther = () => {
+  const { t } = useTranslation();
+  const explore = t("explore", { returnObjects: true });
 
-    const { t } = useTranslation();
-    const explore = t("explore", { returnObjects: true });
+  const classesTitle = t(explore.discover);
+  const newsTitle = t(explore.signUp);
 
-    const classesTitle = t(explore.discover);
-    const newsTitle = t(explore.signUp);
-
-    return (
-        <>
-            <HeadingBottom bottomPageHeading={classesTitle.toUpperCase()} />
-            <ClassesPictures />
-            <HeadingBottom bottomPageHeading={newsTitle.toUpperCase()} />
-            <NewsPictures />
-        </>
-
-    )
-}
+  return (
+    <div className="w-full">
+      <HeadingBottom bottomPageHeading={classesTitle.toUpperCase()} />
+      <ClassesPictures />
+      <HeadingBottom bottomPageHeading={newsTitle.toUpperCase()} />
+      <NewsPictures />
+    </div>
+  );
+};
