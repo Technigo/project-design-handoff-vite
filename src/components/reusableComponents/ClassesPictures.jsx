51b9fcd1471a79ed9448b99c62f6cbc0a7a6b491
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-17 10:42:25 +0100

    added omitted alt on img for classesPictures

--- src/components/reusableComponents/ClassesPictures.jsx
+++ src/components/reusableComponents/ClassesPictures.jsx
@@ -1,9 +1,27 @@
 export const ClassesPictures = () => {
-    return (
-        <div className="mx-auto flex flex-row justify-center items-center mb-10">
-            <img src="images/DoublesMoving.png" alt="2 male acrobats on a pole" />
-            <img src="images/exoticPole.png" alt="woman in high heels doing a split holding a pole" />
-            <img src="images/pregnantPole.png" alt="" />
-        </div>
-    )
-}
+  return (
+    <div className="mx-auto flex flex-row justify-center items-center mb-10">
+      <div>
+        <img
+          src="images/DoublesMoving.png"
+          alt="2 male acrobats on a pole"
+          className="object-scale-down"
+        />
+      </div>
+      <div>
+        <img
+          src="images/exoticPole.png"
+          alt="woman in high heels doing a split holding a pole"
+          className="object-scale-down"
+        />
+      </div>
+      <div>
+        <img
+          src="images/pregnantPole.png"
+          alt="pregnant woman leaning on pole"
+          className="object-scale-down"
+        />
+      </div>
+    </div>
+  );
+};
