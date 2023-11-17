3ef406b5d23ec158fb7935ddad0eb5237e479ca7
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-17 10:45:31 +0100

    coherence in semicolon

--- src/components/MiddleSectionGet.jsx
+++ src/components/MiddleSectionGet.jsx
@@ -1,14 +1,13 @@
-import { GetBendy } from "./reusableComponents/GetBendy"
-import { GetStrong } from "./reusableComponents/GetStrong"
-import { CircleGetVertical } from "./reusableComponents/CircleGetVertical"
+import { GetBendy } from "./reusableComponents/GetBendy";
+import { GetStrong } from "./reusableComponents/GetStrong";
+import { CircleGetVertical } from "./reusableComponents/CircleGetVertical";
 
 export const MiddleSectionGet = () => {
-
-    return (
-        <div className="flex flex-col items-center">
-            <GetStrong />
-            <GetBendy />
-            <CircleGetVertical />
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center">
+      <GetStrong />
+      <GetBendy />
+      <CircleGetVertical />
+    </div>
+  );
+};
