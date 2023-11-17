4ffe33f0f8e71376a11ad63051435294f99b0c87
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-17 10:43:03 +0100

    responsiveness to NewsPictures section

--- src/components/reusableComponents/NewsPictures.jsx
+++ src/components/reusableComponents/NewsPictures.jsx
@@ -1,10 +1,27 @@
-
 export const NewsPictures = () => {
-    return (
-        <div className="flex flex-row justify-center gap-4 mx-1 h-auto mb-20">
-            <img src="images/Rectangle20.png" alt="woman doing a split upside down on the head" />
-            <img src="images/Rectangle21.png" alt="sportwoman doing a split hanging on a pole upside down" />
-            <img src="images/Rectangle22.png" alt="tatooed woman posing hanging upside down on a pole " />
-        </div>
-    )
-}
+  return (
+    <div className="w-full flex flex-row justify-center gap-4 mx-1 h-auto mb-20">
+      <div>
+        <img
+          src="images/Rectangle20.png"
+          alt="woman doing a split upside down on the head"
+          className="object-scale-down"
+        />
+      </div>
+      <div>
+        <img
+          src="images/Rectangle21.png"
+          alt="sportwoman doing a split hanging on a pole upside down"
+          className="object-scale-down"
+        />
+      </div>
+      <div>
+        <img
+          src="images/Rectangle22.png"
+          alt="tatooed woman posing hanging upside down on a pole "
+          className="object-scale-down"
+        />
+      </div>
+    </div>
+  );
+};
