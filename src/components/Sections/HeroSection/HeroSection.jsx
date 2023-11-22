09ab816686d2e8624ed26b09913b7adba1169f23
BeckieMorton <beckam11@gmail.com>
2023-11-22 20:02:41 +0100

    navbar styling

--- src/components/Sections/HeroSection/HeroSection.jsx
+++ src/components/Sections/HeroSection/HeroSection.jsx
@@ -4,19 +4,20 @@ import { Button } from "../../ui/Button/Button";
 export const HeroSection = () => {
   return (
     <>
-      <div className={styles.heroContainer}>
-        <div className={styles.heroBox}>
+      <div className="heroContainer">
+        <div className="heroBox">
           <h1>Where Serenity Meets Stretch</h1>
           <p>
             At our yoga studio, we're not your typical pretzel-twisting,
             'Om'-chanting bunch. We're all about infusing your yoga journey with
             a big dose of FUN, a sprinkle of QUIRKY, and a heap of ZEN.
           </p>
-          <p>
-            <Button /> <Button />
-          </p>
         </div>
-        <div className={styles.heroBox}>
+        <div className="heroBox">
+          <p>
+            <Button buttonName="Join today!" />
+            <Button buttonName="Read more" />
+          </p>
           <img src="/assets/hero-img.png" />
         </div>
       </div>
--- src/components/Sections/NavBar/NavBar.jsx
+++ src/components/Sections/NavBar/NavBar.jsx
@@ -5,27 +5,29 @@ export const NavBar = () => {
   return (
     <div className={styles.navbarContainer}>
       <nav className={styles.navBar}>
-        <li>
-          <Button buttonName="Flow Yoga" />
-        </li>
-        <li>
-          <a>Our Classes</a>
-        </li>
-        <li>
-          <a>Schedule</a>
-        </li>
-        <li>
-          <a>Events</a>
-        </li>
-        <li>
-          <a>Blog</a>
-        </li>
-        <li>
-          <Button buttonName="Join!" />
-        </li>
-        <li>
-          <Button buttonName="Log in" />
-        </li>
+        <ul>
+          <li>
+            <Button buttonName="Flow Yoga" />
+          </li>
+          <li>
+            <a>Our Classes</a>
+          </li>
+          <li>
+            <a>Schedule</a>
+          </li>
+          <li>
+            <a>Events</a>
+          </li>
+          <li>
+            <a>Blog</a>
+          </li>
+          <li>
+            <Button buttonName="Join!" />
+          </li>
+          <li>
+            <Button buttonName="Log in" />
+          </li>
+        </ul>
       </nav>
     </div>
   );
