34b1a449fa46d7de2af3b3004aed055795173df3
BeckieMorton <beckam11@gmail.com>
2023-11-29 17:24:18 +0100

    formatted grid for events

--- src/components/Sections/EventCards/EventCards.jsx
+++ src/components/Sections/EventCards/EventCards.jsx
@@ -1,5 +1,18 @@
 import styles from "./EventCards.module.css";
 
 export const EventCards = () => {
-  return <div>EventCards</div>;
+  return (
+    <>
+      <div className={styles.eventsContainer}>
+        <div className={styles.eventsCard}>events card 1</div>
+        <div className={styles.eventsCard}>events card 2</div>
+        <div className={styles.eventsCard}>events card 3</div>
+        <div className={styles.eventsCard}>events card 4</div>
+        <div className={styles.eventsCard}>events card 5</div>
+        <div className={styles.eventsCard}>events card 6</div>
+        <div className={styles.eventsCard}>events card 7</div>
+        <div className={styles.eventsCard}>events card 8</div>
+      </div>
+    </>
+  );
 };
