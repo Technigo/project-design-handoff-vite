db125c6ceceace42ad8b4ee6ef257b299dd183a2
Sandra Gustafsson <sandragustafsson@Mac-mini.local>
2023-12-07 13:06:13 +0100

    responsivenees about componnet

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,5 +1,7 @@
 import './AboutUs.css';
 import ImageAboutUs from '/images/image19.png';
+import AdditionalImage1 from '/images/Image22.png';
+import AdditionalImage2 from '/images/Image18.png';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
@@ -13,13 +15,16 @@ export const AboutUs = () => {
                 {/* Using the translation function t to get the translated strings */}
                 <h1>{t('AboutUs.heading')}</h1>
                 <p>{t('AboutUs.description')}</p>
-                <Link to="/about">
-                    <button className="yellowButton">{t('AboutUs.buttonText')}</button>
-                </Link>
+                
             </div>
             <div className="rightColumn">
                 <img src={ImageAboutUs} alt="About Us" className="roundImage" />
+                 <img src={AdditionalImage1} alt="Couple playing parkour" className="roundImage additionalImage" />
+                <img src={AdditionalImage2} alt="Family playing parkour" className="roundImage additionalImage" />
             </div>
+            <Link to="/about">
+                    <button className="yellowButton">{t('AboutUs.buttonText')}</button>
+                </Link>
         </div>
     );
 }
