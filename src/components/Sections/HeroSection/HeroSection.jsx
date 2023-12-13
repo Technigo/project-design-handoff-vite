da32e716c261de54ad24db3c36eefedd5bd6bb14
BeckieMorton <beckam11@gmail.com>
2023-12-13 16:53:40 +1100

    all translation json files updated with styling

--- src/components/Sections/HeroSection/HeroSection.jsx
+++ src/components/Sections/HeroSection/HeroSection.jsx
@@ -1,6 +1,8 @@
 import styles from "./HeroSection.module.css";
 import { Button } from "../../ui/Button/Button";
 import { useTranslation } from "react-i18next";
+//Trans is used to bold certain words in the translated text with <strong> in the json file.
+import { Trans } from "react-i18next";
 
 export const HeroSection = () => {
   const { t, i18n } = useTranslation();
@@ -10,30 +12,32 @@ export const HeroSection = () => {
 
   return (
     <>
-      <div className={styles.heroContainer}>
-        <div className={styles.heroBox}>
-          <h1>{t("homePage.hero.h1")}</h1>
-          <p>{t("homePage.hero.p")}</p>
-        </div>
-        <div className={styles.heroBox}>
-          <p>
-            <button
-              className={styles.heroButton}
-              style={{ color: "white", backgroundColor: "black" }}
-            >
-              {t("homePage.hero.buttonJoin")}
-            </button>
-          </p>
-          <p>
-            <button className={styles.heroButton}>
-              {t("homePage.hero.buttonRead")}
-            </button>
-          </p>
+      <Trans>
+        <div className={styles.heroContainer}>
           <div className={styles.heroBox}>
-            <img src="/assets/hero-img.png" />
+            <h1>{t("homePage.hero.h1")}</h1>
+            <p>{t("homePage.hero.p")}</p>
+          </div>
+          <div className={styles.heroBox}>
+            <p>
+              <button
+                className={styles.heroButton}
+                style={{ color: "white", backgroundColor: "black" }}
+              >
+                {t("homePage.hero.buttonJoin")}
+              </button>
+            </p>
+            <p>
+              <button className={styles.heroButton}>
+                {t("homePage.hero.buttonRead")}
+              </button>
+            </p>
+            <div className={styles.heroBox}>
+              <img src="/assets/hero-img.png" />
+            </div>
           </div>
         </div>
-      </div>
+      </Trans>
     </>
   );
 };
