9ad4d3875938a1621e22d965b174ea47ccce9100
BeckieMorton <beckam11@gmail.com>
2023-12-13 15:36:47 +1100

    try again to make bold work

--- src/components/Sections/BodySection/BodySection.jsx
+++ src/components/Sections/BodySection/BodySection.jsx
@@ -1,6 +1,7 @@
 import styles from "./BodySection.module.css";
 import { Button } from "../../ui/Button/Button";
-import { useTranslation, Trans } from "react-i18next";
+import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
 
 //{t("homePage.body.")}
 // "body": {
@@ -16,82 +17,82 @@ export const BodySection = () => {
 
   return (
     <>
-      <Trans>
-        <div className={styles.bodyContainer}>
-          <div className={styles.bodyBox}>
+      <div className={styles.bodyContainer}>
+        <div className={styles.bodyBox}>
+          <Trans>
             <p> {t("homePage.body.bendP")}</p>
-          </div>
-          <div className={styles.bodyBox}>
-            <button
-              className={styles.bodyButton}
-              style={{ color: "white", backgroundColor: "black" }}
-            >
-              {t("homePage.body.bodyButtonJoin")}
-            </button>
-          </div>
-          <div className={styles.bodyBox}>
-            <img src="./assets/1st.png" />
-          </div>
-          <div className={styles.bodyBox}>
-            <p>{t("homePage.body.matPheader")}</p>
-            <p>{t("homePage.body.matP")}</p>
-            <button
-              className={styles.bodyButton}
-              style={{ color: "black", backgroundColor: "#ffecea" }}
-            >
-              {t("homePage.body.bodyButtonBook")}
-            </button>
-          </div>
-          <div className={styles.bodyBox}>
-            <img src="./assets/2nd.png" />
-          </div>
-          <div className={styles.bodyBox}>
-            <p>{t("homePage.body.schedulePheader")}</p>
-            <p>{t("homePage.body.scheduleP")}</p>
-            <button
-              className={styles.bodyButton}
-              style={{ color: "black", backgroundColor: "#ffecea" }}
-            >
-              Schedule{t("homePage.body.bodyButtonSchedule")}
-            </button>
-          </div>
-          <div className={styles.bodyBox}>
-            <img src="./assets/3rd.png" />
-          </div>
-          <div className={styles.bodyBox}>
-            <p>{t("homePage.body.tribePheader")}</p>
-            <p>{t("homePage.body.tribeP")}</p>
-            <button
-              className={styles.bodyButton}
-              style={{ color: "black", backgroundColor: "#ffecea" }}
-            >
-              {t("homePage.body.bodyButtonJoin")}
-            </button>
-          </div>
-          <div className={styles.bodyBox}>
-            <img src="./assets/3rd.png" />
-          </div>
-          <div className={styles.bodyBox}>
-            <p>{t("homePage.body.tourPHeader")}</p>
-            <p>{t("homePage.body.tourP")}</p>
-            <button
-              className={styles.bodyButton}
-              style={{ color: "black", backgroundColor: "#ffecea" }}
-            >
-              {t("homePage.body.bodyButtonExplore")}
-            </button>
-          </div>
-          <div className={styles.bodyBox}>
-            <p>{t("homePage.body.flexP")}</p>
-            <button
-              className={styles.bodyButton}
-              style={{ color: "white", backgroundColor: "black" }}
-            >
-              {t("homePage.body.bodyButtonJoin")}
-            </button>
-          </div>
+          </Trans>
         </div>
-      </Trans>
+        <div className={styles.bodyBox}>
+          <button
+            className={styles.bodyButton}
+            style={{ color: "white", backgroundColor: "black" }}
+          >
+            {t("homePage.body.bodyButtonJoin")}
+          </button>
+        </div>
+        <div className={styles.bodyBox}>
+          <img src="./assets/1st.png" />
+        </div>
+        <div className={styles.bodyBox}>
+          <p>{t("homePage.body.matPheader")}</p>
+          <p>{t("homePage.body.matP")}</p>
+          <button
+            className={styles.bodyButton}
+            style={{ color: "black", backgroundColor: "#ffecea" }}
+          >
+            {t("homePage.body.bodyButtonBook")}
+          </button>
+        </div>
+        <div className={styles.bodyBox}>
+          <img src="./assets/2nd.png" />
+        </div>
+        <div className={styles.bodyBox}>
+          <p>{t("homePage.body.schedulePheader")}</p>
+          <p>{t("homePage.body.scheduleP")}</p>
+          <button
+            className={styles.bodyButton}
+            style={{ color: "black", backgroundColor: "#ffecea" }}
+          >
+            Schedule{t("homePage.body.bodyButtonSchedule")}
+          </button>
+        </div>
+        <div className={styles.bodyBox}>
+          <img src="./assets/3rd.png" />
+        </div>
+        <div className={styles.bodyBox}>
+          <p>{t("homePage.body.tribePheader")}</p>
+          <p>{t("homePage.body.tribeP")}</p>
+          <button
+            className={styles.bodyButton}
+            style={{ color: "black", backgroundColor: "#ffecea" }}
+          >
+            {t("homePage.body.bodyButtonJoin")}
+          </button>
+        </div>
+        <div className={styles.bodyBox}>
+          <img src="./assets/3rd.png" />
+        </div>
+        <div className={styles.bodyBox}>
+          <p>{t("homePage.body.tourPHeader")}</p>
+          <p>{t("homePage.body.tourP")}</p>
+          <button
+            className={styles.bodyButton}
+            style={{ color: "black", backgroundColor: "#ffecea" }}
+          >
+            {t("homePage.body.bodyButtonExplore")}
+          </button>
+        </div>
+        <div className={styles.bodyBox}>
+          <p>{t("homePage.body.flexP")}</p>
+          <button
+            className={styles.bodyButton}
+            style={{ color: "white", backgroundColor: "black" }}
+          >
+            {t("homePage.body.bodyButtonJoin")}
+          </button>
+        </div>
+      </div>
     </>
   );
 };
