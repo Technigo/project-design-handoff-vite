667363f4d71cb26a29569942e7199a5ef8127708
BeckieMorton <beckam11@gmail.com>
2024-01-05 12:29:54 +1100

    event images zoom in effect

--- src/components/Sections/EventCards/EventCards.jsx
+++ src/components/Sections/EventCards/EventCards.jsx
@@ -11,63 +11,81 @@ export const EventCards = () => {
       <div className={styles.eventsContainer}>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event1.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event1.png" />
+            </div>
             <h2>{t("eventsPage.card1.header")}</h2>
             <p>{t("eventsPage.card1.text")}</p>
           </Trans>
         </div>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event2.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event2.png" />
+            </div>
             <h2>{t("eventsPage.card2.header")}</h2>
             <p>{t("eventsPage.card2.text")}</p>
           </Trans>
         </div>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event3.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event3.png" />
+            </div>
             <h2>{t("eventsPage.card3.header")}</h2>
             <p>{t("eventsPage.card3.text")}</p>
           </Trans>
         </div>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event4.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event4.png" />
+            </div>
             <h2>{t("eventsPage.card4.header")}</h2>
             <p>{t("eventsPage.card4.text")}</p>
           </Trans>
         </div>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event5.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event5.png" />
+            </div>
             <h2>{t("eventsPage.card5.header")}</h2>
             <p>{t("eventsPage.card5.text")}</p>
           </Trans>
         </div>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event6.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event6.png" />
+            </div>
             <h2>{t("eventsPage.card6.header")}</h2>
             <p>{t("eventsPage.card6.text")}</p>
           </Trans>
         </div>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event7.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event7.png" />
+            </div>
             <h2>{t("eventsPage.card7.header")}</h2>
             <p>{t("eventsPage.card7.text")}</p>
           </Trans>
         </div>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event8.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event8.png" />
+            </div>
             <h2>{t("eventsPage.card8.header")}</h2>
             <p>{t("eventsPage.card8.text")}</p>
           </Trans>
         </div>
         <div className={styles.eventsCard}>
           <Trans>
-            <img src="./assets/event9.png" />
+            <div className={styles.eventsImage}>
+              <img src="./assets/event9.png" />
+            </div>
             <h2>{t("eventsPage.card9.header")}</h2>
             <p>{t("eventsPage.card9.text")}</p>
           </Trans>
--- src/components/Sections/Footer/Footer.jsx
+++ src/components/Sections/Footer/Footer.jsx
@@ -21,7 +21,7 @@ export const Footer = () => {
   return (
     <>
       <Trans>
-        <div className={styles.footerContainer}>
+        <div className={styles.footerContainerMobile}>
           <div className={styles.footerBox}>
             <button
               className={styles.footerButton}
@@ -84,6 +84,74 @@ export const Footer = () => {
             <p>
               <img src="./assets/social.png" />
             </p>
+          </div>
+        </div>
+        <div className={styles.footerContainerDesktop}>
+          {/* column one */}
+          <div className={styles.footerBox}>
+            <button
+              className={styles.footerButton}
+              style={{ color: "white", backgroundColor: "black" }}
+            >
+              F
+            </button>
+          </div>
+          <div className={styles.footerBox}>
+            <p> {t("homePage.footer.ourClasses")}</p>
+          </div>{" "}
+          <div className={styles.footerBox}>
+            <p> {t("homePage.footer.terms")}</p>
+          </div>
+          <div className={styles.footerBox}>
+            <p> {t("homePage.footer.signUp")}</p>
+          </div>
+          <div className={styles.footerBox}>
+            <h2>{t("homePage.footer.header1")}</h2>
+            <h2>{t("homePage.footer.header2")}</h2>
+          </div>
+          <div className={styles.footerBox}>
+            <p> {t("homePage.footer.schedule")}</p>
+          </div>
+          <div className={styles.footerBox}>
+            <p> {t("homePage.footer.conditions")}</p>
+          </div>
+          <div className={styles.footerBox}>
+            <p> {t("homePage.footer.logIn")}</p>
+          </div>
+          <div>
+            <p></p>
+          </div>
+          <div className={styles.footerBox}>
+            <Link className={styles.linkStyle} to={`/events`}>
+              <p> {t("homePage.footer.events")}</p>
+            </Link>
+          </div>
+          <div className={styles.footerBox}>
+            <p> {t("homePage.footer.privacy")}</p>
+          </div>
+          <div className={styles.footerBox}>
+            <p>
+              <img src="./assets/social.png" />
+            </p>
+          </div>
+          <div>
+            <p></p>
+          </div>
+          <div className={styles.footerBox}>
+            <p> {t("homePage.footer.blog")}</p>
+          </div>
+          <div className={styles.footerBox}>
+            <div className={styles.internationalisationWwrapper}>
+              <button onClick={englishFunc}>
+                <img src="./assets/en-icon.png" />
+              </button>
+              <button onClick={deutschFunc}>
+                <img src="./assets/de-icon.png" />
+              </button>
+            </div>
+          </div>
+          <div>
+            <p></p>
           </div>
         </div>
       </Trans>
