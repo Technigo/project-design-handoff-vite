fa9685155274e4e7de0fb99813dc561ab47a61d7
Linnea Johansson <Linneajoh@gmail.com>
2024-01-13 16:22:11 +0100

    studio

--- src/SecondPage/Studio.jsx
+++ src/SecondPage/Studio.jsx
@@ -1,12 +1,25 @@
-// Mission.jsx
 import React from 'react';
+import Image3 from '../assets/Images/Image section 3.png';
+import './Studio.css';
 
 const Studio = () => {
   return (
-    <div>
-      {/* Content for the mission section */}
-      <h2>Our Studio</h2>
-      <p>Here you can add text that describes the mission of your yoga studio.</p>
+    <div className="studio-section">
+      <div className="studio-image">
+        <img src={Image3} alt="Studio" />
+      </div>
+      <div className="studio-content">
+        <h2>OUR STUDIOS FACILITATES</h2>
+        <ul>
+          <li>Spacious Practice Rooms: Our well-lit, tranquil spaces inspire a sense of serenity.</li>
+          <li>Quality Yoga Props: We provide mats, blocks, bolsters, and more for your practice.</li>
+          <li>Clean and Hygienic: We maintain high standards of cleanliness using eco-friendly products</li>
+          <li>Comfortable Lounge: Relax and connect with fellow yogis before and after class.</li>
+          <li>Changing and Shower Facilities: Freshen up with our convenient amenities.</li>
+          <li>Relaxing Soundscapes: Enjoy calming soundscapes for an oasis of peace.</li>
+        </ul>
+        <button className="book-now-button">Book Now</button>
+      </div>
     </div>
   );
 };
