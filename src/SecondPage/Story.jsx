cc597d9ff31b15a102ccdeee3296434ce5f4fe02
Linnea Johansson <Linneajoh@gmail.com>
2024-01-13 15:57:23 +0100

    story

--- src/SecondPage/Story.jsx
+++ src/SecondPage/Story.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import Image6 from '../assets/Images/Image section 6.png';
+import './Story.css'; 
 
 const Story = () => {
   return (
-    <div>
-      {/* Content for the mission section */}
-      <h2>Our Story</h2>
-      <p>Here you can add text that describes the mission of your yoga studio.</p>
+    <div className="story-section">
+      <div className="story-content">
+        <h2>OUR STORY</h2>
+        <p>In the heart of Stockholm, you'll find Serenity Yoga. Our journey began in 2022 in Mariatorget, Södermalm, and expanded to Hornstulls Strand in 2023.</p>
+        <p>We're all about yoga and holistic wellness. Our two studios are more than just places to practice; they're where you can discover inner peace, balance, and strength.</p>
+        <p>At Serenity Yoga, we're like a family. We're committed to making yoga a transformative experience for everyone, creating a safe and inclusive community. Join us as we continue this simple yet beautiful story.</p>
+      </div>
+      <div className="story-image">
+        <img src={Image6} alt="Yoga Class" />
+      </div>
     </div>
   );
 };
 
 export default Story;
+
