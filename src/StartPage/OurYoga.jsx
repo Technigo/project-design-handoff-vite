6bc24c2d5c9e7082498f13ebf5ba25b4d913f139
Linnea Johansson <Linneajoh@gmail.com>
2024-01-14 00:30:13 +0100

    translate our yoga

--- src/StartPage/OurYoga.jsx
+++ src/StartPage/OurYoga.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Image2 from '../assets/Images/Image section 2.png';
 import './OurYoga.css';
 
 const OurYoga = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="our-yoga-section">
       <div className="our-yoga-content">
-        <p>Welcome to our community of experienced teachers, where you'll find classes for all </p>
-        <p>levels. Join us for our heartwarming events, including workshops, teacher training, </p>
-        <p>and retreats.</p>
-        <button className="offerings-button">View Our Offerings</button>
+        <p>{t('Welcome to our community of experienced teachers, where you will find classes for all')}</p>
+        <p>{t('levels. Join us for our heartwarming events, including workshops, teacher training,')}</p>
+        <p>{t('and retreats.')}</p>
+        <button className="offerings-button">{t('View Our Offerings')}</button>
       </div>
       <div className="yoga-container">
         <img src={Image2} alt="Yoga practice" className="yoga-image"/>
         <div className="yoga-content">
-          <h1>OUR YOGA</h1>
-          <p>No matter which class you choose, we stress breath, muscle support, and focus to ensure a safe yoga practice. We tailor our classes to your individual needs, whether you require modifications due to injuries or want to challenge yourself. We love sharing our passion for yoga and are always available to answer your questions after the class. We are here for you!</p>
-          <button className="discover-classes-button">Discover Our Classes</button>
+          <h1>{t('OUR YOGA')}</h1>
+          <p>{t('No matter which class you choose, we stress breath, muscle support, and focus to ensure a safe yoga practice. We tailor our classes to your individual needs, whether you require modifications due to injuries or want to challenge yourself. We love sharing our passion for yoga and are always available to answer your questions after the class. We are here for you!')}</p>
+          <button className="discover-classes-button">{t('Discover Our Classes')}</button>
         </div>
       </div>
     </div>
--- src/i18n.jsx
+++ src/i18n.jsx
@@ -15,8 +15,13 @@ const resources = {
             'With us, yoga is for everyone, whether you are a beginner or an experienced yogini. Welcome to a place where you belong!': 'Enligt oss är yoga något för alla, oavsett om du är en nybörjare eller erfaren yogini. Välkommen till en plats där du kan känna dig som hemma!',      
             'DISCOVER OUR STUDIO': 'UTFORSKA VÅR STUDIO',
             'LOG IN': 'LOGGA IN',
-
-
+            'Welcome to our community of experienced teachers, where you will find classes for all': 'Välkommen till vårt community av erfarna lärare, här finner du klasser för alla',
+            'levels. Join us for our heartwarming events, including workshops, teacher training,': 'nivåer. Var med på våra hjärtvärmande event, workshops, lärarutbildning,', 
+            'and retreats.': 'och retreats.',
+            'View Our Offerings': 'Se våra erbjudanden',
+            'OUR YOGA': 'VÅR YOGA',
+            'No matter which class you choose, we stress breath, muscle support, and focus to ensure a safe yoga practice. We tailor our classes to your individual needs, whether you require modifications due to injuries or want to challenge yourself. We love sharing our passion for yoga and are always available to answer your questions after the class. We are here for you!': 'Oavsett vilken klass du väljer, lägger vi vikt vid andning, muskelstöd och fokus för att säkerställa en säker yogapraxis. Vi anpassar våra klasser till dina individuella behov, oavsett om du behöver modifieringar på grund av skador eller vill utmana dig själv. Vi älskar att dela vår passion för yoga och är alltid tillgängliga för att svara på dina frågor efter klassen. Vi finns här för dig!',
+            'Discover Our Classes': 'Se våra klasser',
 
 
 
