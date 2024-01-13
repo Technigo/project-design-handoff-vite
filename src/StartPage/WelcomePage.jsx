2520648003c07f03f6b87e1d43489c9cea2432fe
Linnea Johansson <Linneajoh@gmail.com>
2024-01-14 00:17:41 +0100

    translate welcomepage

--- src/StartPage/WelcomePage.jsx
+++ src/StartPage/WelcomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import HeroImage from '../assets/Images/Hero image.png';
 import Logo from '../assets/Images/Logotype.png';
 import './WelcomePage.css';
@@ -6,24 +7,26 @@ import { Link } from 'react-router-dom';
 
 
 const WelcomePage = () => {
+  const { t } = useTranslation();
+
   return (
       <div className="Header">
         <div className="navbar">
           <img src={Logo} alt="Logotype" className="logo"/>
           <ul className="nav-list">
-            <li><Link to="/about">ABOUT US</Link></li>
-            <li><a href="#">EXPLORE</a></li>
-            <li><a href="#">SCHEDULE</a></li>
-            <li><a href="#">PRICES</a></li>
+            <li><Link to="/about">{t('ABOUT US')}</Link></li>
+            <li><a href="/about">{t('EXPLORE')}</a></li>
+            <li><a href="/about">{t('SCHEDULE')}</a></li>
+            <li><a href="/about">{t('PRICES')}</a></li>
           </ul>
         </div>
       <div className="content">
         <div className="text">
-          <h1>Your journey to inner balance begins here.</h1>
-          <h2>With us, yoga is for everyone, whether you are a beginner or an experienced yogini. Welcome to a place where you belong!</h2>
+          <h1>{t('Your journey to inner balance begins here.')}</h1>
+          <h2>{t('With us, yoga is for everyone, whether you are a beginner or an experienced yogini. Welcome to a place where you belong!')}</h2>
           <div className="buttons">
-            <button className="discover-button">Discover our studio</button>
-            <button className="login-button">Log in</button>
+            <button className="discover-button">{t('DISCOVER OUR STUDIO')}</button>
+            <button className="login-button">{t('LOG IN')}</button>
           </div>
         </div>
         <div className="image">
--- src/i18n.jsx
+++ src/i18n.jsx
@@ -6,7 +6,19 @@ const resources = {
         translation: {
             'CONTACT': 'Kontakt',
             'TERMS': 'Villkor',
-            'FAQ': 'Vanliga frågor',          
+            'FAQ': 'Vanliga frågor',  
+            'ABOUT US': 'OM OSS',
+            'EXPLORE': 'UTFORSKA',
+            'SCHEDULE': 'SCHEMA',
+            'PRICES': 'PRISER',
+            'Your journey to inner balance begins here.': 'Din resa till inre balans börjar här',
+            'With us, yoga is for everyone, whether you are a beginner or an experienced yogini. Welcome to a place where you belong!': 'Enligt oss är yoga något för alla, oavsett om du är en nybörjare eller erfaren yogini. Välkommen till en plats där du kan känna dig som hemma!',      
+            'DISCOVER OUR STUDIO': 'UTFORSKA VÅR STUDIO',
+            'LOG IN': 'LOGGA IN',
+
+
+
+
 
         }
     },
