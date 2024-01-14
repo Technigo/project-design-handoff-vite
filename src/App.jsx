a4ece8e8122d803a22a1d515596eae9b1fe378d1
Linnea Johansson <Linneajoh@gmail.com>
2024-01-14 20:17:54 +0100

    reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartPage from './StartPage/StartPage';
 import SecondPage from './SecondPage/SecondPage';
-import Footer from './StartPage/Footer'; 
+import Footer from './ReusableComponent/Footer'; 
 import './i18n';
 
 export const App = () => {
