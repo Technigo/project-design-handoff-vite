9c547a319333b78e5331e0a5f905607f251412d0
Emmy Jansson <emmybengtsson@live.se>
2024-01-14 08:06:20 +0100

    changed the buttons

--- src/Components.jsx/Navbar.jsx
+++ src/Components.jsx/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
@@ -7,26 +6,22 @@ export const Navbar = () => {
     <header>
       <nav className="navbar">
         <img src="MAGNB.png" alt="Company Logo" />
-
-        <ul>
-          <div className="navbar-buttons">
-            <button onClick={() => (window.location.href = "/about")}>
-              About us
-            </button>
-            <button onClick={() => (window.location.href = "/membership")}>
-              Membership
-            </button>
-            <button onClick={() => (window.location.href = "/facilities")}>
-              Facilities
-            </button>
-            <button onClick={() => (window.location.href = "/opening-hours")}>
-              Opening Hours
-            </button>
-
-            <button className="sign-in-button">Sign in</button>
-            <button>Swedish/English</button>
-          </div>
-        </ul>
+        <div className="navbar-buttons">
+          <Link to="/about">
+            <button>About us</button>
+          </Link>
+          <Link to="/membership">
+            <button>Membership</button>
+          </Link>
+          <Link to="/facilities">
+            <button>Facilities</button>
+          </Link>
+          <Link to="/opening-hours">
+            <button>Opening Hours</button>
+          </Link>
+          <button className="sign-in-button">Sign in</button>
+          <button>Swedish/English</button>
+        </div>
       </nav>
     </header>
   );
