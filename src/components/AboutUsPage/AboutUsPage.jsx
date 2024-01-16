5cd38c313b2f2414c7d5464b057fb9e514595e5f
isaackamran <isaackamran@hotmail.com>
2024-01-16 01:47:03 +0100

    finished

--- src/components/AboutUsPage/AboutUsPage.jsx
+++ src/components/AboutUsPage/AboutUsPage.jsx
@@ -1,9 +1,9 @@
-import './AboutUsPage.css';
 import VisionImg from "../../../public/images/Image18.png";
 import StoryImg from "../../../public/images/Rectangle19.png";
 import FormImg from "../../../public/images/Image22.png";
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import './AboutUsPage.css';
 
 export const AboutUsPage = () => {
 
