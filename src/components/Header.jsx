085a6e0f618c1e412b726dfda2fd5fdae2fb9b9b
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-04-20 12:44:41 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,13 @@
+import { Nav } from './Nav'
+import { Logo } from './Logo'
+import { Button } from './Button'
+
 export const Header = () => {
-  return <div>Header</div>;
-};
+  return (
+    <header>
+      <Nav />
+      <Logo />
+      <Button />
+    </header>
+  )
+}
