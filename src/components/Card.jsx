de746e4660e16e82efeb5556c356a7a4a784190e
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-04-20 18:52:00 +0200

    Update - basic styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,36 @@
-export const Card = () => {
-  return <div>Card</div>;
+import styled from "styled-components";
+
+const StyledCard = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 254px !important;
+  border-radius: 10px;
+  background: var(--text-card-bg);
+  padding: 10px;
+  flex: 0 1 0;
+  // Shadow gets cut off - asked Catrine if we could change it. Also added 12px padding to carousel to show it...
+  /* box-shadow: 0px 68px 19px 0px rgba(0, 0, 0, 0),
+    0px 44px 17px 0px rgba(0, 0, 0, 0.01), 0px 24px 15px 0px rgba(0, 0, 0, 0.04),
+    0px 11px 11px 0px rgba(0, 0, 0, 0.06), 0px 3px 6px 0px rgba(0, 0, 0, 0.07); */
+  filter: drop-shadow(0px 4px 4px #1520193c);
+`;
+
+const Image = styled.img`
+  width: 234px;
+  height: 185px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+export const Card = ({ url, alt, heading, text }) => {
+  return (
+    <StyledCard>
+      <Image
+        src={url}
+        alt={alt}></Image>
+      <h3>{heading}</h3>
+      <p>{text}</p>
+    </StyledCard>
+  );
 };
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,3 +1,79 @@
+import styled from "styled-components";
+import { Card } from "./Card";
+
+const cardContent = [
+  {
+    image: "/unsplash_Brooke-Cagle.jpg",
+    alt: "Women stretching",
+    heading: "Membership",
+    text: "Sign up as a member to get full access to the modern and well equipped gym.",
+  },
+  {
+    image: "/unsplash_oORcW5Uc0dM_Brooke-Cagle.jpg",
+    alt: "Women lifting weights",
+    heading: "Face to face coaching",
+    text: "Book a one on one personal meeting to set up a workout plan suitable for your level and goals. ",
+  },
+  {
+    image: "/unsplash_urOgICfIldY_Gett-images.jpg",
+    alt: "Woman with computer on yoga mat",
+    heading: "Online coaching",
+    text: "Even if you’re not to get to our gym in person, I can still help you reach your goals through online coaching.",
+  },
+  {
+    image: "/unsplash_Gxr5vwk1V3k_Anna-Jakutajc-Wojtalik.jpg",
+    alt: "Fruits and vegetables",
+    heading: "Nutritional coching",
+    text: "What you eat is imortant. Book a meeting to set up a nutritional plan that will compliment your active lifestyle. ",
+  },
+];
+
+const StyledSection = styled.section`
+  padding: 0 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  @media all and (min-width: 1440px) {
+    align-items: center;
+    max-width: 1024px;
+  }
+`;
+
+const StyledCarousel = styled.div`
+  display: flex;
+  gap: 16px;
+  width: 90vw;
+  overflow-x: scroll;
+  padding-bottom: 12px;
+
+  @media all and (min-width: 1440px) {
+    justify-content: center;
+  }
+`;
+
+const H2 = styled.h2`
+  text-transform: uppercase;
+`;
+
 export const Carousel = () => {
-  return <div>Carousel</div>;
+  return (
+    <StyledSection>
+      <H2>Our services</H2>
+
+      <StyledCarousel>
+        {cardContent.map(card => {
+          return (
+            <Card
+              key={card.heading}
+              url={card.image}
+              alt={card.alt}
+              heading={card.heading}
+              text={card.text}
+            />
+          );
+        })}
+      </StyledCarousel>
+    </StyledSection>
+  );
 };
