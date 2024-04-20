f7080c346b250d69df32cbdf8d361302d75ef169
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-04-20 12:53:53 +0200

    Update - mounted components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { CTA } from "./components/CTA";
+import { Carousel } from "./components/Carousel";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { Quote } from "./components/Quote";
+import { Rating } from "./components/Rating";
+import { USP } from "./components/USP";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Quote />
+        <USP />
+        <Rating />
+        <USP />
+        <Carousel />
+        <CTA />
+      </main>
+      <Footer />
+    </>
+  );
 };
