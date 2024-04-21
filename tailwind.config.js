1a72bbafdec7a88fe5381bdcad534b417bc99deb
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-04-21 17:40:33 +0200

    adding all needed components and predetermend colors we need.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      yellow: "#F6E53C",
+      pink: "#DE20F9",
+      purple: "#4B189E",
+      darkPurple: "#F6E53C",
+      white: "#FFFFFF",
+      black: "#08031D",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
