56348341ab03c10f8ca40af73be639c0e858bbe6
fridaforser <fridaforser@gmail.com>
2024-04-22 15:29:36 +0200

    Formatting

--- src/components/1-TopMenu.jsx
+++ src/components/1-TopMenu.jsx
@@ -18,47 +18,47 @@ export const TopMenu = () => {
         />
       </div>
       <div className="menu-text-section">
-      <p className="menu-text">First Timers</p>
-      <p className="menu-text">Schedule</p>
-      <p className="buy">Buy Sessions</p>
+        <p className="menu-text">First Timers</p>
+        <p className="menu-text">Schedule</p>
+        <p className="buy">Buy Sessions</p>
       </div>
       <div className="user-menu-div">
-      <div className="user">
-      <img
-          className="user-phone"
-          src="./src/assets/icons/topmenu/UserPhone.png"
-        />
-        <img
-          className="user-phone-click"
-          src="./src/assets/icons/topmenu/UserPhoneClick.png"
-        />
-        <img
-          className="user-desktop"
-          src="./src/assets/icons/topmenu/UserDefault.png"
-        />
-         <img
-          className="user-desktop-hover"
-          src="./src/assets/icons/topmenu/UserTablClick.png"
-        />
-      </div>
-      <div className="menu">
-      <img
-          className="menu-phone"
-          src="./src/assets/icons/topmenu/MenuPhone.png"
-        />
-        <img
-          className="menu-phone-click"
-          src="./src/assets/icons/topmenu/MenuPhoneClick.png"
-        />
-        <img
-          className="menu-desktop"
-          src="./src/assets/icons/topmenu/MenuDefault.png"
-        />
-         <img
-          className="menu-desktop-hover"
-          src="./src/assets/icons/topmenu/MenuTablHoverClick.png"
-        />
-      </div>
+        <div className="user">
+          <img
+            className="user-phone"
+            src="./src/assets/icons/topmenu/UserPhone.png"
+          />
+          <img
+            className="user-phone-click"
+            src="./src/assets/icons/topmenu/UserPhoneClick.png"
+          />
+          <img
+            className="user-desktop"
+            src="./src/assets/icons/topmenu/UserDefault.png"
+          />
+          <img
+            className="user-desktop-hover"
+            src="./src/assets/icons/topmenu/UserTablClick.png"
+          />
+        </div>
+        <div className="menu">
+          <img
+            className="menu-phone"
+            src="./src/assets/icons/topmenu/MenuPhone.png"
+          />
+          <img
+            className="menu-phone-click"
+            src="./src/assets/icons/topmenu/MenuPhoneClick.png"
+          />
+          <img
+            className="menu-desktop"
+            src="./src/assets/icons/topmenu/MenuDefault.png"
+          />
+          <img
+            className="menu-desktop-hover"
+            src="./src/assets/icons/topmenu/MenuTablHoverClick.png"
+          />
+        </div>
       </div>
     </div>
   );
