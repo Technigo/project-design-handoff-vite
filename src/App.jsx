ea88e725daab97dede6ac93b1e2de18b4d30067b
Pernilla Sterner <hey@pernillasterner.se>
2024-04-22 09:31:56 +0200

    Set up basic structure for styling each component (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,46 @@
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <main>
+      <header className="top-navigation shared-padding">
+        <a href="#">GETTING STARTED</a>
+        <a href="#">CLASSES</a>
+      </header>
+
+      <section className="hero__section shared-padding">
+        <h1>HERO SECTION</h1>
+        <h2>This is H2</h2>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellat
+          iusto animi unde voluptatum adipisci! Obcaecati laboriosam error, vel
+          quos consectetur recusandae nesciunt tempore nostrum minus et, illo
+          eligendi accusamus!
+        </p>
+      </section>
+
+      <section className="article__section shared-padding">
+        <h1>ARTICLE SECTION</h1>
+        <h2>This is H2</h2>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellat
+          iusto animi unde voluptatum adipisci! Obcaecati laboriosam error, vel
+          quos consectetur recusandae nesciunt tempore nostrum minus et, illo
+          eligendi accusamus!
+        </p>
+      </section>
+
+      <section className="testimonial__section shared-padding">
+        <h1>TESTIMONIAL SECTION</h1>
+        <h2>This is H2</h2>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellat
+          iusto animi unde voluptatum adipisci! Obcaecati laboriosam error, vel
+          quos consectetur recusandae nesciunt tempore nostrum minus et, illo
+          eligendi accusamus!
+        </p>
+      </section>
+    </main>
+  );
 };
