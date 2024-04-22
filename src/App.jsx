8e91aa2eb21025976ed5e39ef409d84ed13a7d87
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-22 13:58:59 +0200

    Install tailwind packages and download two fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <div>
+      <h1 className="font-avenir text-4xl font-bold underline">Project handoff</h1>
+      <h2 className="font-futura">Find me in src/app.jsx!</h2>
+    </div>
+  );
 };
