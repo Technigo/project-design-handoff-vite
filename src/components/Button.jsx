4b739540c63faf771b28d0a7a29ebe55f15da3c7
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-04-22 13:49:45 +0200

    color overlay hero section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export const FindUsButton = () => {
   return (
-    <button className='w-[147px] h-10 text-center bg-yellow rounded-[10px] text-black font-bold hover:bg-dkpurple hover:text-pink'>
+    <button className='w-[147px] h-10 text-center bg-yellow rounded-[10px] text-[20px] text-black font-bold shadow-[rgba(222,32,249,0.5)_3px_4px_0px_0px] hover:bg-dkpurple hover:text-pink'>
       Find us!
     </button>
   )
@@ -8,7 +8,7 @@ export const FindUsButton = () => {
 
 export const JoinUsButton = () => {
   return (
-    <button className='w-[147px] h-10 text-center bg-yellow rounded-[10px] text-black font-bold hover:bg-dkpurple hover:text-pink'>
+    <button className='w-[147px] h-10 text-center bg-yellow rounded-[10px] text-[20px] text-black font-bold shadow-[rgba(222,32,249,0.5)_3px_4px_0px_0px] hover:bg-dkpurple hover:text-pink'>
       Join us!
     </button>
   )
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,18 +3,20 @@ import { FindUsButton } from "./Button"
 
 export const HeroSection = () => {
   return (
-    <div className='bg-hero-image bg-cover bg-center bg-dkpurple/50 h-[340px]'>
-      <Header />
-      <div className='flex flex-col text-center justify-center items-center '>
-        <h1 className='mt-6 text-2xl md:text-2xl lg:text-4xl'>
-          The Zumba Hall
-        </h1>
-        <p className='mt-1 mb-3 mx-[24px] md:mt-2 md:text-base lg:mt-5'>
-          Join our training centre and get fit through{" "}
-          <span className='text-[#F6E53C] italic md:text-base'>Dance</span>
-          <span className='italic md:text-base'>!</span>
-        </p>
-        <FindUsButton />
+    <div className='bg-hero-image bg-cover bg-center h-[340px] -z-10'>
+      <div className='bg-dkpurple/50 z-0 h-[340px]'>
+        <Header />
+        <div className='flex flex-col text-center justify-center items-center '>
+          <h1 className='mt-6 text-2xl md:text-2xl lg:text-4xl'>
+            The Zumba Hall
+          </h1>
+          <p className='mt-1 mb-3 mx-[24px] md:mt-2 md:text-base lg:mt-5'>
+            Join our training centre and get fit through{" "}
+            <span className='text-[#F6E53C] italic md:text-base'>Dance</span>
+            <span className='italic md:text-base'>!</span>
+          </p>
+          <FindUsButton />
+        </div>
       </div>
     </div>
   )
