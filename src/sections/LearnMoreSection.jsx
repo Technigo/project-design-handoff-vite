d7d00493438b4106c0245f7571dbeb37fcf35ed5
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-22 11:00:11 +0200

    update LearnMoreSection

--- src/sections/LearnMoreSection.jsx
+++ src/sections/LearnMoreSection.jsx
@@ -1,3 +1,12 @@
-export const LearnMoreSection = () => {
-  return null;
+import dataEn from "../data.en.json";
+import dataJp from "../data.jp.json";
+
+export const LearnMoreSection = ({ data }) => {
+  return (
+    <div>
+      <h2>{data.heading}</h2>
+      <p>{data.subheading}</p>
+      <button>{data.cta}</button>
+    </div>
+  );
 };
