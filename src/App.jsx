f2f7af1dc7a2586c0eb49f2d34e0efe9eaa5c831
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-22 15:03:08 +0200

    Fix the prettier extension for tailwind and add setting.json so as to make css file recongise as tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 export const App = () => {
   return (
     <div>
-      <h1 className="font-avenir text-4xl font-bold underline">Project handoff</h1>
+      <h1 className="font-avenir text-4xl font-bold underline">
+        Project handoff
+      </h1>
       <h2 className="font-futura">Find me in src/app.jsx!</h2>
     </div>
   );
