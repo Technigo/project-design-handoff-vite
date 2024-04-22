ee6aed723b60061404ab259b26ba94ac84d0ab60
Pernilla Sterner <hey@pernillasterner.se>
2024-04-22 13:04:14 +0200

    Feature install tailwind (#2)
    * Add images, icons and logo
    
    * Testing Tailwind and setting up basic theme colors, padding and screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 export const App = () => {
   return (
     <main>
-      <header className="top-navigation shared-padding">
+      <header className="top-navigation p-66 lg:bg-cyan-500">
         <a href="#">GETTING STARTED</a>
         <a href="#">CLASSES</a>
       </header>
 
-      <section className="hero__section shared-padding">
-        <h1>HERO SECTION</h1>
+      <section className="hero__section p-66 bg-royal-purple">
+        <h1 className="text-3xl font-bold underline">HERO SECTION</h1>
         <h2>This is H2</h2>
 
         <p>
@@ -18,7 +18,7 @@ export const App = () => {
         </p>
       </section>
 
-      <section className="article__section shared-padding">
+      <section className="article__section p-66">
         <h1>ARTICLE SECTION</h1>
         <h2>This is H2</h2>
 
@@ -30,7 +30,7 @@ export const App = () => {
         </p>
       </section>
 
-      <section className="testimonial__section shared-padding">
+      <section className="testimonial__section p-66 bg-plum-light">
         <h1>TESTIMONIAL SECTION</h1>
         <h2>This is H2</h2>
 
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      current: "currentColor",
+      white: "#fff",
+      black: "000",
+      "crimson-light": "#ec3642",
+      "royal-purple": "#762aa7",
+      "plum-light": "#9e3e82",
+      "deep-violet": "#542A8B",
+    },
+    padding: {
+      66: "66px",
+    },
+    screens: {
+      sm: "430px",
+      md: "834px",
+      lg: "1440px",
+    },
+  },
+  plugins: [],
+};
