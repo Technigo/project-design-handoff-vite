07191a5442ddd7e658fe7b0a0726f92b32588729
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-04-22 21:44:22 +0200

    statistic section done

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,22 +1,30 @@
 export const Statistics = () => {
   return (
-    <div className='flex px-1 mt-1 text-center'>
-      <h2 className='flex flex-col text-yellow text-xl'>
-        20
-        <span className='mt-1 mb-3 text-white text-sm'>
-          certified instructors
-        </span>
-      </h2>
-      <h2 className='flex flex-col text-yellow text-xl'>
-        20
-        <span className='mt-1 mb-3 text-white text-sm'>different classes</span>
-      </h2>
-      <h2 className='flex flex-col text-yellow text-xl'>
-        1
-        <span className='mt-1 mb-3 text-white text-sm'>
-          fee – join all classes
-        </span>
-      </h2>
+    <div className="mb-3 mt-1 flex justify-center gap-1 px-1 text-center md:mb-6 md:mt-2 lg:mb-7 lg:mt-5">
+      <div className="h-auto w-[82px] md:w-[158px] lg:w-[189px]">
+        <h2 className="flex flex-col text-xl leading-none text-yellow md:text-[45px] lg:text-[70px]">
+          20
+          <span className="md mb-3 mt-1 text-sm text-white md:text-[30px] md:leading-8">
+            Certified instructors
+          </span>
+        </h2>
+      </div>
+      <div className="h-auto w-[82px] md:w-[158px] lg:w-[189px]">
+        <h2 className="flex flex-col text-xl leading-none text-yellow md:text-[45px] lg:text-[70px]">
+          20
+          <span className="mb-3 mt-1 text-sm text-white md:text-[30px] md:leading-8">
+            Different classes
+          </span>
+        </h2>
+      </div>
+      <div className="h-auto w-[82px] md:w-[158px] lg:w-[189px]">
+        <h2 className="flex flex-col text-xl leading-none text-yellow md:text-[45px] lg:text-[70px]">
+          1
+          <span className="mb-3 mt-1 text-sm text-white md:text-[30px] md:leading-8">
+            Fee – join all classes
+          </span>
+        </h2>
+      </div>
     </div>
-  )
-}
+  );
+};
