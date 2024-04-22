3248c253aeffc34c94b6f43a15b5db464fd71f57
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-22 15:18:24 +0200

    Add color theme to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'primary-black': '#444446',
+        'secondary-ivory': '#FFF5EB',
+        'neutral-milk': '#F8F7F3',
+        'neutral-white': '#FFFFFF',
+        'accent-orange': '#D18B42',
+        'accent-blue': '#3C97D1',
+      },
       fontFamily: {
-        'avenir': ['Avenir'],
-        'futura': ['Futura']
-      }
+        avenir: ['Avenir'],
+        futura: ['Futura'],
+      },
     },
   },
   plugins: [],
