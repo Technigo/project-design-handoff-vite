a00e1fe1184c5894275a43859a60976eae8492b4
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-22 10:58:59 +0200

    Import useState for translation and render data in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './index.css';
-import { LearnMoreSection } from './sections/LearnMoreSection';
-import { FooterSection } from './sections/FooterSection';
+import { useState } from "react";
+import "./index.css";
+import { LearnMoreSection } from "./sections/LearnMoreSection";
+import { FooterSection } from "./sections/FooterSection";
+import dataEn from "./data.en.json";
+import dataJp from "./data.jp.json";
 
 export const App = () => {
+  const [locale, setLocale] = useState("en");
+
+  const data = locale === "en" ? dataEn : dataJp;
+
+  const handleLanguageChange = (e) => {
+    setLocale(e.target.value);
+  };
+
   return (
     <>
-      <LearnMoreSection />
-      <FooterSection />
+      <LearnMoreSection data={data["learn-more"]} />
+      <FooterSection
+        onLanguageChange={handleLanguageChange}
+        data={["footer"]}
+      />
     </>
   );
 };
