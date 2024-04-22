d5c1fd093823d275838cd322bfcd68715e96d9f4
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-22 11:42:25 +0200

    update LearnMoreSection component

--- src/sections/LearnMoreSection.jsx
+++ src/sections/LearnMoreSection.jsx
@@ -1,12 +1,19 @@
-import dataEn from "../data.en.json";
-import dataJp from "../data.jp.json";
+import PropTypes from "prop-types";
 
 export const LearnMoreSection = ({ data }) => {
   return (
-    <div>
-      <h2>{data.heading}</h2>
-      <p>{data.subheading}</p>
+    <div className="bg-background-lavender text-text-dark p-5 text-center">
+      <h2 className="text-2x1 md:text-4x1 font-bold mb-3">{data.heading}</h2>
+      <p className="mb-4">{data.subheading}</p>
       <button>{data.cta}</button>
     </div>
   );
+};
+
+LearnMoreSection.propTypes = {
+  data: PropTypes.shape({
+    heading: PropTypes.string.isRequired,
+    subheading: PropTypes.string.isRequired,
+    cta: PropTypes.string.isRequired,
+  }).isRequired,
 };
