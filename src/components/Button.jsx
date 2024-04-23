e124fed9379e76611296076f5465f9a126bb9936
unknown <linda.frischknecht@bluewin.ch>
2024-04-23 15:56:32 +0200

    adding hover on button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -19,9 +19,10 @@ export const StickyButton = () => {
 //Styled Components
 
 const StyledButton = styled.button`
-  background-color: ${({ $orange }) => ($orange ? "black" : "#FFFFFFD6")};
+  background-color: ${({ $orange }) => ($orange ? "#F4AB49" : "#FFFFFFD6")};
   width: ${({ $sticky }) => ($sticky ? "159px" : "88%")};
   border-radius: ${({ $sticky }) => ($sticky ? "100px" : "10px")};
+  color: ${({ $orange }) => ($orange ? "#FFFFFF" : "#000000")};
   //We will have to fix the width somehow because percentage doesn't work. I was just trying to come close but I don't know how to tell the button to fill the whole width except the margin...
   margin: 0 24px;
   height: 61px;
@@ -29,4 +30,9 @@ const StyledButton = styled.button`
   font-size: 21px;
   font-weight: bold;
   letter-spacing: 11px;
+
+  &:hover {
+    background-color: ${({ $orange }) => ($orange ? "#FFFFFF80" : "#827F7FB3")};
+    color: ${({ $orange }) => ($orange ? "#F4AB49" : "#FFFFFF")};
+  }
 `;
