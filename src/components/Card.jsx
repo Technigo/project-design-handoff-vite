2517b2cc3fdb463205586706bb3e2a5231051a4a
SofieFerrari <sofiestrahl@gmail.com>
2024-04-23 21:34:18 +0200

    styled cards, made title for card section and made discover-button for cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,35 @@
 import PropTypes from 'prop-types';
+import { Discover } from './Discover';
 
 import cardArray from '../CardArray';
 
 export const Card = () => {
-  // not sure how to pass the images
-  //need to talk about what div to put classname flex in for all to be in row and not content being on row in div
-
   return (
     <>
-      <div className="grid">
-        {cardArray.map((item) => (
-          <div key={item.id} className="flex flex-col md:flex-row">
-            <img src={item.image} alt={item.alt} />
-            <h3>{item.title}</h3>
-            <p>{item.paragraph}</p>
-          </div>
-        ))}
-      </div>
+      <section>
+        <div className="h-[195px] w-[360px] px-[11px] pb-[24px] pt-[64px] text-center font-futura text-xl font-bold">
+          <h2>OUR RETREATS</h2>
+          <p className="h-[70px] w-[338px] font-avenir text-base font-medium text-[#444446]">
+            For sun-seekers and dreamers who appreciate barefoot-luxury and slow
+            living.
+          </p>
+        </div>
+        <div className="grid">
+          {cardArray.map((item) => (
+            <div
+              key={item.id}
+              className="flex max-h-[538px] w-[360px] flex-col gap-6 px-[60px] py-6"
+            >
+              <div className="h-60 w-60">
+                <img src={item.image} alt={item.alt} />
+              </div>
+              <h3 className="font-futura text-lg font-medium">{item.title}</h3>
+              <p>{item.paragraph}</p>
+              <button>{item.button}</button>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
---
+++ src/components/Discover.jsx
@@ -0,0 +1,8 @@
+//non-clickable button for cards
+export const Discover = () => {
+  return (
+    <button className="font-avenir font-medium text-sm w-[158px] h-[42px] border-[1px] border-black px-[51,5px] py-[11,5px] gap-2.5">
+      <p>Discover</p>
+    </button>
+  )
+}
