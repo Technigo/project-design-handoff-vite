241699ecea512c2d56bc7b16e1d3355d8cd35ae2
SofieFerrari <sofiestrahl@gmail.com>
2024-04-23 09:59:18 +0200

    pics

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
 import cardArray from '../CardArray';
 import PropTypes from 'prop-types';
+import image from '../assets/cardImages/food.png';
+// import svgImage from "../assets/cardImages/food.svg"
 
 export const Card = ({ item: { id, title, image, paragraph, alt } }) => {
   console.log('is cardArray being passed?', cardArray);
+  console.log(image);
 
   // not sure how to pass the images
   //need to talk about what div to put classname flex in for all to be in row and not content being on row in div
@@ -11,7 +14,7 @@ export const Card = ({ item: { id, title, image, paragraph, alt } }) => {
     <>
       <div className="flex flex-col md:flex-row">
         <div className="flex flex-col md:flex-row" key={id}>
-          <img src={`${image}`} alt={`${alt}`} />
+          <img src={} alt={`${alt}`} />
           <h3>{title}</h3>
           <p>{paragraph}</p>
         </div>
