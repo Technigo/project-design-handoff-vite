2129da1f93b680243ecf24a81c5852bf0b3bebc6
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-04-24 00:17:41 +0200

    partially done with address

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -1,14 +1,55 @@
+import AtIconSmall from "../assets/icons/at-icon-small.svg";
+import AtIconLarge from "../assets/icons/at-icon-large.svg";
+import LetterIconSmall from "../assets/icons/letter-icon-small.svg";
+import LetterIconLarge from "../assets/icons/letter-icon-large.svg";
+import PhoneIconSmall from "../assets/icons/phone-icon-small.svg";
+import PhoneIconLarge from "../assets/icons/phone-icon-large.svg";
+
 export const Address = () => {
+  const atIconSource = {
+    small: AtIconSmall,
+    large: AtIconLarge,
+  };
+  const letterIconSource = {
+    small: LetterIconSmall,
+    large: LetterIconLarge,
+  };
+  const phoneIconSource = {
+    small: PhoneIconSmall,
+    large: PhoneIconLarge,
+  };
+
   return (
-    <section>
-      <div>
-        <h2>Postal address</h2>
-        <p>Streetname 10</p>
-        <p>1234 56 City</p>
-        <p>Country</p>
+    <section className="hidden flex-col text-base md:flex">
+      <div className="flex gap-[25px]">
+        <img
+          src={letterIconSource.small}
+          alt="letter icon"
+          srcSet={`${letterIconSource.small} 744w, ${letterIconSource.large} 1440w`}
+        />
+        <div className="flex flex-col">
+          <h2 className="text-lg">Postal address</h2>
+          <p>Streetname 10</p>
+          <p>1234 56 City</p>
+          <p>Country</p>
+        </div>
       </div>
-      <a href="tel:+01761234567">0176 - 12 345 67</a>
-      <a href="mailto:thezumbahall@outlook.com">thezumbahall@outlook.com</a>
+      <div className="flex gap-[25px]">
+        <img
+          src={phoneIconSource.small}
+          alt="phone icon"
+          srcSet={`${phoneIconSource.small} 744w, ${phoneIconSource.large} 1440w`}
+        />
+        <a href="tel:+01761234567">0176 - 12 345 67</a>
+      </div>
+      <div className="flex gap-[25px]">
+        <img
+          src={atIconSource.small}
+          alt="@ sign"
+          srcSet={`${atIconSource.small} 744w, ${atIconSource.large} 1440w`}
+        />
+        <a href="mailto:thezumbahall@outlook.com">thezumbahall@outlook.com</a>
+      </div>
     </section>
   );
 };
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,20 +1,23 @@
 import { LogoDark } from "../components/Logo";
 import { Social } from "../components/Social";
 import { Language } from "../components/Language";
+import { Address } from "../components/Address";
 
 export const Footer = () => {
   return (
     <>
-      <section className="h-[248px] w-auto bg-yellow text-black md:h-[331px] lg:h-[756px]">
-        <div className="grid px-1 pb-[30px] pt-[90px]">
+      <section className="h-[260px] w-auto bg-yellow text-black md:h-[331px] lg:h-[756px]">
+        <div className="grid px-1 pb-[23px] pt-[64px] md:px-2 md:pb-[30px] md:pt-[90px] lg:px-[112px] lg:pb-[107px] lg:pt-[188px] ">
           <LogoDark />
           <Social />
-          <div>Address</div>
-          <div className="flex gap-[20px]">
-            <p>Copyright notice</p>
-            <p>Privacy policy</p>
+          <Address />
+          <div className="flex justify-between">
+            <div className="flex gap-[20px] text-[14px]">
+              <p>Copyright notice</p>
+              <p>Privacy policy</p>
+            </div>
+            <Language />
           </div>
-          <Language />
         </div>
       </section>
     </>
