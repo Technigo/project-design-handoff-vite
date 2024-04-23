18e2e3fd68b468449c75b18285b69cce58b91fa1
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-23 15:09:28 +0200

    Add and style the Log in button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.svg';
 
 export const Header = () => {
   return (
-    <header className="h-[41rem] bg-cobra	bg-cover bg-[40%] bg-no-repeat">
+    <header className=" flex h-[41rem] flex-col items-center bg-cobra	bg-cover bg-[40%] bg-no-repeat">
       <div className="mx-5 flex justify-center py-8 pt-7">
         <img className="h-16" src={logo} alt="logo" />
       </div>
@@ -12,14 +12,15 @@ export const Header = () => {
         src={menuIcon}
         alt="hamburger menu"
       />
-      <div className="flex flex-col justify-center font-futura	">
-        <h1 className="text-center	text-3xl font-bold">
-          EMPOWERING VINYASA YOGA
-        </h1>
+      <div className="flex flex-col justify-center text-center	font-futura">
+        <h1 className="text-3xl font-bold">EMPOWERING VINYASA YOGA</h1>
         <h2 className="my-4 text-xl	font-bold">Online studio & Retreats</h2>
         <p className="pb-7	text-base font-medium">TO INSPIRE + UPLIFT YOU</p>
         <p className="text-base font-medium">Since 2010</p>
       </div>
+      <button className="mt-64 w-40 rounded-[100px] bg-accent-orange	px-12 py-3 font-extrabold text-neutral-white">
+        Log in
+      </button>
     </header>
   );
 };
