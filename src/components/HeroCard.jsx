e4d40736410a54bd673b9c19723ffc340d71a786
Citronskal <yiaswift@gmail.com>
2024-04-23 23:23:55 +0200

    trying to get the images out

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,13 +1,23 @@
-import "./HeroImages.json";
 import buttonData from "./Buttons.json";
 import { Buttons } from "./Buttons";
 
-export const HeroCard = ({ cardName, imageURL }) => {
-  const topThreeButtons = buttonData.slice(1, 5);
+
+// Image data mapping card names to image imports
+const heroImagesData = {
+  "Personal Training": "/hero01.png",
+  "Our Space": "/hero02.png",
+  "Over 30 Classes": "/hero03.png",
+};
+
+export const HeroCard = ({ cardName }) => {
+  const topThreeButtons = buttonData.slice(1, 4);
+
+  // Find the correct image for the given card name
+  const image = heroImagesData[cardName];
 
   return (
     <div>
-      <img src={imageURL} alt={cardName} />
+      <img src={image} alt={cardName} />
       <Buttons buttonList={topThreeButtons} />
     </div>
   );
