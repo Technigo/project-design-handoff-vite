374c5d16d28cdda450ab6c8783ed4202051c5812
unknown <linda.frischknecht@bluewin.ch>
2024-04-23 17:55:53 +0200

    adding slider area

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,3 +1,21 @@
+import { SliderCard } from "./SliderCard";
+import styled from "styled-components";
+import { activity } from "../../slidercardsdata.json";
+
 export const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <StyledSlider>
+      {activity.map((data, index) => (
+        <SliderCard key={index} data={data} />
+      ))}
+    </StyledSlider>
+  );
 };
+
+// Styled Components
+
+const StyledSlider = styled.div`
+  display: flex;
+  gap: 16px;
+  margin: 0 24px;
+`;
---
+++ src/components/SliderCard.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+export const SliderCard = ({ data }) => {
+  return (
+    <StyledSliderCard>
+      <img src={data.img} alt={data.title}></img>
+      <TitleBox>
+        <h3>{data.title}</h3>
+        <img src={data.icon}></img>
+      </TitleBox>
+      <p>{data.description}</p>
+    </StyledSliderCard>
+  );
+};
+
+//Styled Components
+
+const StyledSliderCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  letter-spacing: 11px;
+  width: 182px;
+  gap: 8px;
+`;
+
+const TitleBox = styled.div`
+  display: flex;
+`;
+
+SliderCard.propTypes = {
+  data: PropTypes.object,
+};
