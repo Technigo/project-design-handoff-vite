dda7cc6cae35df2d85c6c6058fcd5bef23c1349c
SofieFerrari <sofiestrahl@gmail.com>
2024-04-23 08:34:27 +0200

    moved cardImages to assetsfolder in src and changes pathway to cardArray

--- src/CardArray.jsx
+++ src/CardArray.jsx
@@ -1,7 +1,7 @@
 export const cardArray = [
   {
     id: 0,
-    image: './assets/global.svg',
+    image: 'src/assets/cardImages/global.svg',
     title: 'GLOBAL COMMUNITY',
     paragraph:
       'We believe in the power of community - of coming together with like-minded people from all around the world. At our retreats you’ll meet people from different background, age, careers, countries and cultures. We promise you’ll make new friends!',
@@ -9,7 +9,7 @@ export const cardArray = [
   },
   {
     id: 1,
-    image: './assets/food.svg',
+    image: 'src/assets/cardImages/food.svg',
     title: 'WHOLESOME FOOD',
     paragraph:
       'At our yoga retreats you can expect high-quality, lovingly prepared food with the option for vegan, gluten- or lactose-free. The food served is prepared with as much organic, local produce as possible based on what’s in season at the time of the retreat.',
@@ -17,7 +17,7 @@ export const cardArray = [
   },
   {
     id: 2,
-    image: './assets/destinations.svg',
+    image: 'src/assets/cardImages/destinations.svg',
     title: 'INSPIRING DESTINATIONS',
     paragraph:
       'Being avid travellers for more than 20 years, we LOVE to explore the world we’ve picked out our favourite destinations for our retreats. We offer retreats and teachers immersions in Portugal, Spain, Bali and Sweden and keep adding new destinations every year.',
@@ -25,7 +25,7 @@ export const cardArray = [
   },
   {
     id: 3,
-    image: './assets/venues.svg',
+    image: 'src/assets/cardImages/venues.svg',
     title: 'UNIQUE VENUES',
     paragraph:
       'Our retreat venues have been carefully handpicked and chosen with a lot of attention and care. We guarantee one-of-a-kind, lovingly run places surrounding by breath-taking nature, attention to detail and where you’ll get inspired to slow things down.',
