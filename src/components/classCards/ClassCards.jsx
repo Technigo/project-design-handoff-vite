0877a6b5fa66f368bb753cb324ee7b36ead5ebc9
Trista Shan <trista8703@hotmail.com>
2024-04-23 19:04:08 +0300

    hover

--- src/components/classCards/ClassCards.jsx
+++ src/components/classCards/ClassCards.jsx
@@ -20,5 +20,4 @@ export const ClassCards = () => {
     </div>
     </div>
   )
-  
 }
--- src/components/classCards/ClassPieces.jsx
+++ src/components/classCards/ClassPieces.jsx
@@ -1,6 +1,6 @@
 export const ClassPieces = ({image, alt, courseName, courseInfo}) => {
     return (
-        <div className='flex flex-col w-[422px] h-[552px] rounded-[10px] bg-purple px-[40px] pt-[40px] pb-[45px] gap-y-[25px] shadow-base shadow-yellow '>
+        <div className='flex flex-col w-[422px] h-[552px] rounded-[10px] bg-purple hover:bg-yellow px-[40px] pt-[40px] pb-[45px] gap-y-[25px] shadow-base shadow-yellow text-white hover:text-black hover:cursor-pointer'>
             <img src={image} alt={alt} className='w-[342px] h-[289px] rounded-[10px]'/>
             <div className="flex flex-col gap-y-2">
                 <h5 className="text-xl">{courseName}</h5>
