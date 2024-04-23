9cb04c3270425a0375b339eaa05d874dfcee933c
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-04-23 07:33:44 +0200

    build components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { NavBar } from "./components/NavBar";
+import { Hero } from "./components/Hero";
+import { OurClass } from "./components/OurClass";
+import { Event } from "./components/Event";
+import { Footer } from "./components/Footer";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+      <OurClass />
+      <Event />
+      <Footer />
+    </main>
+  );
 };
