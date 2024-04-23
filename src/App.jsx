ca76dc1c3c816964025e4d39b9ba9b4695b2f093
unknown <linda.frischknecht@bluewin.ch>
2024-04-23 13:55:27 +0200

    setting up styled components and making a structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+/* import styled from "styled-components"; */
+import { Body } from "./components/Body";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Hero />
+      <Body />
+      <Footer />
+    </>
+  );
 };
+
+const GlobalStyle = createGlobalStyle`
+*,
+*::before,
+*::after {
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif}
+
+:root {
+  margin: 0 24px;
+}
+`;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./Button";
+import { NavBar } from "./NavBar";
+
+export const Hero = () => {
+  return (
+    <>
+      <NavBar />
+      <Button />
+    </>
+  );
+};
