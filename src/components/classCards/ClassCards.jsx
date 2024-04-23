34dbdd32d9ef8794f84f8aaaa0de084e0741cb61
Trista Shan <trista8703@hotmail.com>
2024-04-24 01:06:57 +0300

    responsiveness

--- src/components/classCards/ClassCards.jsx
+++ src/components/classCards/ClassCards.jsx
@@ -5,8 +5,8 @@ import JustZumba from "../../assets/classImg/JustZumba.jpeg"
 
 export const ClassCards = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-y-5 px-[60px] pt-7 '>
-    <h4 className='ubuntu text-3xl text-white' >Some of our classes!</h4>
+    <div className='flex flex-col justify-center items-center md:gap-y-[30px] gap-y-5 px-[60px] pt-7 '>
+    <h4 className='ubuntu sm:text-xl md:text-xl text-3xl text-white' >Some of our classes!</h4>
     <div className='flex gap-[27px]'>
       <ClassPieces image={ZumbaEnergetic} alt={'People are having Zumba class'} 
     courseName={'Zumba Energetic'} courseInfo={'A class for you that want a fast paced and high cardio workout!'}
--- src/components/classCards/ClassPieces.jsx
+++ src/components/classCards/ClassPieces.jsx
@@ -1,12 +1,11 @@
 export const ClassPieces = ({image, alt, courseName, courseInfo}) => {
     return (
-        <div className='flex flex-col w-[422px] h-[552px] rounded-[10px] bg-purple hover:bg-yellow px-[40px] pt-[40px] pb-[45px] gap-y-[25px] shadow-base shadow-yellow text-white hover:text-black hover:cursor-pointer'>
-            <img src={image} alt={alt} className='w-[342px] h-[289px] rounded-[10px]'/>
-            <div className="flex flex-col gap-y-2">
-                <h5 className="text-xl">{courseName}</h5>
-            <p className="text-5xl">{courseInfo}</p>  
+        <div className='flex flex-col sm:w-[257px] sm:h-[369px] sm:pt-1 sm:pb-[24px] sm:gap-y-1 md:w-[334px] md:h-[382px] md:pt-[24px] md:px-2 md:pb-2w-[422px] h-[552px] rounded-[10px] bg-purple hover:bg-yellow px-[40px] pt-[40px] pb-[45px] gap-y-[25px] shadow-base shadow-yellow text-white hover:text-black hover:cursor-pointer '>
+            <img src={image} alt={alt} className='sm:w-[226px] sm:h-[132px] md:w-[270px] md:h-[196px] w-[342px] h-[289px] rounded-[10px]'/>
+            <div className="flex flex-col sm:px-[15px] sm:gap-y-[8px] md:gap-y-1 gap-y-2">
+                <h5 className="Ubuntu sm:text-lg md:text-lg  text-xl">{courseName}</h5>
+            <p className="Ubuntu md:text-base text-5xl">{courseInfo}</p>  
             </div>
-             
         </div>
     )
 }
