9e2724e6b0fe6d22f25766b1b3555b774408523e
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-24 14:58:19 +0200

    Add responsiveness on the menu bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,23 @@ import { HoverableButton } from './HoverableButton';
 export const Header = () => {
   return (
     <header className=" flex h-[41rem] flex-col items-center bg-cobra	bg-cover bg-[40%] bg-no-repeat">
-      <div className="mx-5 flex justify-center py-8 pt-7">
-        <img className="h-16" src={logo} alt="logo" />
+      <div className="mx-5 flex justify-center py-8 pt-7 lg:hidden">
+        <img className="h-16 md:h-20" src={logo} alt="logo" />
       </div>
+      <nav className="mb-28 mt-10 hidden w-4/5 self-center lg:block">
+        <ul className="flex flex-row items-end space-x-9">
+          <li>About us</li>
+          <li>Retreats</li>
+          <li>Free Classes</li>
+          <li className="relative top-2">
+            <img className="h-20" src={logo} alt="logo" />
+          </li>
+          <li>Online Yoga</li>
+          <li>Events</li>
+        </ul>
+      </nav>
       <img
-        className="absolute right-5 top-7 h-6 w-6"
+        className="absolute right-5 top-7 h-6 w-6 md:left-5 md:h-10 md:w-10 lg:hidden"
         src={menuIcon}
         alt="hamburger menu"
       />
@@ -19,7 +31,10 @@ export const Header = () => {
         <h3 className="pb-7	text-lg	 font-medium">TO INSPIRE + UPLIFT YOU</h3>
         <h3 className="text-lg font-medium">Since 2010</h3>
       </div>
-      <HoverableButton text="Log in" customStyle="mt-64" />
+      <HoverableButton
+        text="Log in"
+        customStyle="mt-64 md:absolute md:right-5 md:mt-0 md:top-7 lg:top-20 lg:right-12 "
+      />
     </header>
   );
 };
