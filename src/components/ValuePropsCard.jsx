cc86cc8433818b0268971ac3a8e2319ed1aa0280
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-24 17:34:27 +0200

    Fixed some issues with styling.

--- src/components/ValuePropsCard.jsx
+++ src/components/ValuePropsCard.jsx
@@ -6,24 +6,28 @@ export const ValuePropsCard = ({
   link,
 }) => {
   return (
-    <div className="bg-primary-violetblue border-2 border-black rounded flex flex-col items-center shadow-boxShadow border-cardBorder w-52 h-80">
+    <div className="bg-primary-violetblue border-2 border-black rounded flex flex-col items-center shadow-boxShadow w-52">
       <img
         src={image}
-        className="rounded-full w-40 h-40 object-cover border-solid border-2 border-black"
+        className="rounded-full object-cover border-solid border-2 border-black h-40 w-40 mt-6"
       ></img>
-      <h4 className="font-urbanistitalic font-medium text-primary-darkblue">
-        {subtitle.toUpperCase()}
-      </h4>
-      <h3 className="font-avengeance text-primary-darkblue text-lg">{title}</h3>
-      <p className="font-urbanist text-primary-darkblue text-sm text-center">
-        {description}
-      </p>
-      <a
-        href={link}
-        className="font-urbanist text-primary-darkblue text-sm font-semibold"
-      >
-        More
-      </a>
+      <div className="flex flex-col items-center mt-2 mr-2 ml-2 mb-6">
+        <h4 className="font-urbanistitalic font-medium text-primary-darkblue">
+          {subtitle.toUpperCase()}
+        </h4>
+        <h3 className="font-avengeance text-primary-darkblue text-lg">
+          {title}
+        </h3>
+        <p className="font-urbanist text-primary-darkblue text-sm text-center">
+          {description}
+        </p>
+        <a
+          href={link}
+          className="font-urbanist text-primary-darkblue text-sm font-semibold"
+        >
+          More
+        </a>
+      </div>
     </div>
   );
 };
