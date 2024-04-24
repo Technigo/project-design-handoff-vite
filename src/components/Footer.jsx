a0e36e745882a7a6e96a1614d46444b253c11b7d
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-24 15:59:48 +0200

    Create arrays to map over the content in footer and make it responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,35 +5,52 @@ import phone from '../assets/contactIcons/Phone.svg';
 import map from '../assets/contactIcons/Map.svg';
 import twitter from '../assets/contactIcons/Twitter.svg';
 
+const contact = [
+  { alt: 'phone', src: phone, text: 'Tel: +352 961 537 583' },
+  { alt: 'email', src: email, text: 'info@aspireyoga.com' },
+  { alt: 'location', src: map, text: 'Algarve, Portugal' },
+];
+
+const footerMenu = [
+  'Contact us',
+  'FAQ',
+  'Terms and conditions',
+  'Cancellation policy',
+  'Press',
+];
+
+const socialMeida = [
+  { alt: 'Facebook', src: facebook },
+  { alt: 'Instagram', src: instagram },
+  { alt: 'Twitter', src: twitter },
+];
+
 export const Footer = () => {
   return (
     <footer className="flex justify-center bg-primary-black py-12 text-neutral-white">
-      <div className="grid w-max grid-cols-1 gap-y-10">
+      <div className="grid w-max grid-cols-1 items-center gap-y-10 md:w-full md:grid-cols-2 md:justify-items-center">
         <ul className="flex  flex-col gap-8">
-          <li className="flex flex-row items-center gap-3">
-            <img src={phone} alt="phone number" />
-            <p>Tel: +352 961 537 583</p>
-          </li>
-          <li className="flex flex-row items-center gap-3">
-            <img src={email} alt="email" />
-            <p>info@aspireyoga.com</p>
-          </li>
-          <li className="flex flex-row items-center gap-3">
-            <img src={map} alt="location" />
-            <p>Algarve, Portugal</p>
+          {contact.map((item, index) => (
+            <li key={index} className="flex flex-row items-center gap-3">
+              <img src={item.src} alt={item.alt} />
+              <p>{item.text}</p>
+            </li>
+          ))}
+          <li className="hidden md:flex md:gap-10">
+            {socialMeida.map((item, index) => (
+              <img key={index} alt={item.alt} src={item.src} />
+            ))}
           </li>
         </ul>
-        <ul className="flex flex-col gap-8">
-          <li>Contact us</li>
-          <li>FAQ</li>
-          <li>Terms and conditions</li>
-          <li>Cancellation policy</li>
-          <li>Press</li>
+        <ul className="flex flex-col gap-8 md:gap-6">
+          {footerMenu.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
         </ul>
-        <div className="flex flex-row gap-10">
-          <img src={facebook} alt="Facebook" />
-          <img src={instagram} alt="Instagram" />
-          <img src={twitter} alt="Twitter" />
+        <div className="flex flex-row gap-10 md:hidden">
+          {socialMeida.map((item, index) => (
+            <img key={index} alt={item.alt} src={item.src} />
+          ))}
         </div>
       </div>
     </footer>
