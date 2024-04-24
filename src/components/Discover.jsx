64dd0168f4746d8bd121d8c406504bfafe4a6448
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-04-24 14:17:04 +0200

    Done with the footer on mobile version

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,8 +1,8 @@
 //non-clickable button for cards
 export const Discover = () => {
   return (
-    <button className="font-avenir font-medium text-sm w-[158px] h-[42px] border-[1px] border-black px-[51,5px] py-[11,5px] gap-2.5">
+    <button className="h-[42px] w-[158px] gap-2.5 border-[1px] border-black px-[51,5px]  py-[11,5px] font-avenir text-sm font-medium hover:bg-accent-orange">
       <p>Discover</p>
     </button>
-  )
-}
+  );
+};
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,32 +7,34 @@ import twitter from '../assets/contactIcons/Twitter.svg';
 
 export const Footer = () => {
   return (
-    <footer className="bg-primary-black text-neutral-white">
-      <ul>
-        <li>
-          <img src={phone} alt="phone number" />
-          <p>Tel: +352 961 537 583</p>
-        </li>
-        <li>
-          <img src={email} alt="email" />
-          <p>info@aspireyoga.com</p>
-        </li>
-        <li>
-          <img src={map} alt="location" />
-          <p>Algarve, Portugal</p>
-        </li>
-      </ul>
-      <ul>
-        <li>Contact us</li>
-        <li>FAQ</li>
-        <li>Terms and conditions</li>
-        <li>Cancellation policy</li>
-        <li>Press</li>
-      </ul>
-      <div>
-        <img src={facebook} alt="Facebook" />
-        <img src={instagram} alt="Instagram" />
-        <img src={twitter} alt="Twitter" />
+    <footer className="flex justify-center bg-primary-black py-12 text-neutral-white">
+      <div className="grid w-max grid-cols-1 gap-y-10">
+        <ul className="flex  flex-col gap-8">
+          <li className="flex flex-row items-center gap-3">
+            <img src={phone} alt="phone number" />
+            <p>Tel: +352 961 537 583</p>
+          </li>
+          <li className="flex flex-row items-center gap-3">
+            <img src={email} alt="email" />
+            <p>info@aspireyoga.com</p>
+          </li>
+          <li className="flex flex-row items-center gap-3">
+            <img src={map} alt="location" />
+            <p>Algarve, Portugal</p>
+          </li>
+        </ul>
+        <ul className="flex flex-col gap-8">
+          <li>Contact us</li>
+          <li>FAQ</li>
+          <li>Terms and conditions</li>
+          <li>Cancellation policy</li>
+          <li>Press</li>
+        </ul>
+        <div className="flex flex-row gap-10">
+          <img src={facebook} alt="Facebook" />
+          <img src={instagram} alt="Instagram" />
+          <img src={twitter} alt="Twitter" />
+        </div>
       </div>
     </footer>
   );
