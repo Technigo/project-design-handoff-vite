b69baab67fb7d25e4b720eef885ad6d59fabea1c
Your El1an3 <eliane_w@yahoo.com>
2024-04-24 11:49:54 +0200

    finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,158 @@
 //import styled
 import styled from "styled-components";
 
+//import icon
+import Globe from "../../public/icons/globe.png";
+
 //import reusables
 import { Logo } from "../reusables/Logo";
+import { Socials } from "../reusables/Socials";
 
 //styles
-const FooterAddress = styled.p`
+const FooterWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-areas:
+    "logo logo logo logo logo logo logo logo logo logo logo logo"
+    "adr adr adr adr adr adr adr adr adr adr adr adr "
+    "lk lk lk lk lk lk lk lk lk lk lk lk"
+    "lg lg lg lg lg lg lg lg lg lg lg lg"
+    " sl sl sl sl sl sl sl sl sl sl sl sl ";
+  row-gap: 31px;
+  padding: 68px 16px;
+
+  @media all and (min-width: 744px) {
+    grid-template-areas:
+      "logo logo logo logo lk lk lk lk adr adr adr adr"
+      " . . . . . . . lg lg sl sl sl";
+    padding: 26px 16px;
+    row-gap: 16px;
+    border-top: 1px solid var(--primary-blk);
+  }
+
+  @media all and (min-width: 1024px) {
+    grid-template-areas: "logo logo lk lk lk adr adr adr lg lg sl sl";
+    padding: 32px 16px;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  grid-area: logo;
+  display: flex;
+  justify-content: center;
+
+  @media all and (min-width: 744px) {
+    justify-content: flex-start;
+  }
+`;
+
+const AddressWrapper = styled.div`
+  grid-area: adr;
+
+  @media all and (min-width: 744px) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+
+  @media all and (min-width: 1024px) {
+    align-items: flex-start;
+  }
+`;
+
+const FooterText = styled.p`
   font-family: Poppins;
   font-size: 16px;
   font-weight: 400;
   text-align: center;
   color: var(--primary-blk);
+
+  @media all and (min-width: 744px) {
+    font-size: 14px;
+  }
+
+  @media all and (min-width: 1024px) {
+    font-size: 16px;
+  }
 `;
 
-const FooterLinks = styled.div`
+const LinksWrapper = styled.div`
+  grid-area: lk;
   display: flex;
   justify-content: center;
-  align-items: center;
-  gap: 12px;
+  column-gap: 12px;
+
+  @media all and (min-width: 744px) {
+    column-gap: 20px;
+  }
 `;
 
 const LanguageWrapper = styled.div`
+  grid-area: lg;
+  column-gap: 12px;
   display: flex;
-  align-items: flex-start;
-  gap: 12px;
+  justify-content: center;
+
+  @media all and (min-width: 744px) {
+    justify-content: flex-end;
+  }
 `;
 
 const IconWrapper = styled.div`
+  grid-area: sl;
+  column-gap: 21px;
   display: flex;
-  width: 339px;
   justify-content: center;
-  align-items: center;
-  gap: 21px;
+
+  @media all and (min-width: 744px) {
+    justify-content: flex-end;
+  }
+`;
+
+const FooterIcon = styled.img`
+  width: 24px;
+  height: 24px;
 `;
 
 //component
 export const Footer = () => {
   return (
     <>
-      <Logo type="secondary" />
-      <FooterAddress>
-        Home address 45B 120 71 Stockholm, Sweden +123 456 789
-      </FooterAddress>
-      <FooterLinks>About | Schedule | Contact</FooterLinks>
-      <LanguageWrapper> image English</LanguageWrapper>
-      <IconWrapper> Youtube Facebook Instagram</IconWrapper>
+      <FooterWrapper>
+        {/* renders logo based on viewport width */}
+        <LogoWrapper>
+          <Logo
+            type={
+              window.innerWidth < 744
+                ? "secondary"
+                : window.innerWidth >= 1024 //
+                ? "small"
+                : "big"
+            }
+          />
+        </LogoWrapper>
+        <AddressWrapper>
+          <FooterText>Home address 45B</FooterText>
+          <FooterText>120 71 Stockholm, Sweden</FooterText>
+          <FooterText>+123 456 789</FooterText>
+        </AddressWrapper>
+        <LinksWrapper>
+          <FooterText>About</FooterText>
+          {/* <FooterText>|</FooterText> */}
+          <FooterText>Schedule</FooterText>
+          {/* <FooterText>|</FooterText> */}
+          <FooterText>Contact</FooterText>
+        </LinksWrapper>
+        <LanguageWrapper>
+          <FooterIcon src={Globe} alt="Globe icon"></FooterIcon>
+          <FooterText>English</FooterText>
+        </LanguageWrapper>
+        <IconWrapper>
+          <Socials social="yt" url="https://www.youtube.com/" />
+          <Socials social="fb" url="https://www.facebook.com" />
+          <Socials social="ig" url="https://www.instagram.com/" />
+        </IconWrapper>
+      </FooterWrapper>
     </>
   );
 };
--- src/reusables/Logo.jsx
+++ src/reusables/Logo.jsx
@@ -9,19 +9,32 @@ import LogoSecondary from "../../public/logo/secondary-big.png";
 export const Logo = ({ type }) => {
   //determine which logo to use based on the 'type' prop
   let logoSrc;
+  let width;
+  let height;
+
   switch (type) {
     case "small":
       logoSrc = LogoPrimarySmall;
+      width = "133px";
+      height = "56px";
       break;
     case "big":
       logoSrc = LogoPrimaryBig;
+      width = "200px";
+      height = "84px";
       break;
     case "secondary":
       logoSrc = LogoSecondary;
+      width = "161px";
+      height = "200px";
       break;
     default:
-      logoSrc = LogoPrimarySmall; //default to small if no valid type is provided
+      logoSrc = LogoPrimarySmall;
+      width = "133px";
+      height = "56px"; //default to small if no valid type is provided
   }
 
-  return <img src={logoSrc} alt="Shanti Yoga Logo" />;
+  return (
+    <img src={logoSrc} alt="Shanti Yoga Logo" width={width} height={height} />
+  );
 };
