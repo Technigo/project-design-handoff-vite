bb639aea49c8a45d5f89d12ea161b1a8edf4b7e3
Pernilla Sterner <hey@pernillasterner.se>
2024-04-24 13:31:32 +0200

    Add footer with links (#8)

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { TopNavigation } from "./components/sections/TopNavigation";
 import { Hero } from "./components/sections/Hero/Hero";
 import { Article } from "./components/sections/Article";
 import { Banner } from "./components/sections/Banner";
+import { Footer } from "./components/sections/Footer";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <main className="bg-white">
         <TopNavigation />
-
         <Hero />
-
         <Article />
         <Banner />
       </main>
+      <Footer />
     </BrowserRouter>
   );
 };
