659ed78c470ed537c078a6d7526741f549aeb2b9
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-04-24 15:09:33 +0200

    added general style with createGlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 
 import { Home } from './components/Home'
 
 export const App = () => {
   return (
     <>
+      <GeneralStyle />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -13,3 +15,72 @@ export const App = () => {
     </>
   )
 }
+
+const GeneralStyle = createGlobalStyle`
+
+:root {
+  --text-card-bg: #fff;
+  --60: #fffff3;
+  --60-dark: #d4ba9f;
+  --text: #152019;
+  --30: #465d4c;
+  --hover-signup-book: #2b4033;
+  --onclick-signup-book: #819588;
+  --10: #d86e15;
+  --hover-login-links: #ef9d53;
+  --onclick-login: #f4b781;
+  --media-md: 744px;
+  --media-lg: 1440px;
+
+  margin: 0;
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+    'Helvetica Neue', sans-serif!important;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+@font-face {
+  font-family: Montserrat;
+  src: url(./assets/Montserrat-VariableFont_wght.ttf);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body,
+#root,
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+body {
+  background-color: var(--60);
+}
+
+main {
+  gap: 56px;
+  width: 100%;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+@media (min-width: 1440px) {
+  main {
+    gap: 80px;
+  }
+}
+@media (min-width: 744px) and (max-width: 1440px) {
+  main {
+    gap: 80px;
+  }
+}
+
+`
