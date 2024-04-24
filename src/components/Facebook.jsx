017961413975771579ef8f17606c58db44a6ea62
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-04-24 17:01:37 +0200

    correct style social icons

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const StyledSvg = styled.svg`
   width: 36px;
@@ -11,17 +11,15 @@ const StyledSvg = styled.svg`
     width: 56px;
     height: 56px;
   }
-`;
+`
 
-export const Facebook = ({ fill = "white" }) => {
+export const Facebook = ({ fill = 'white' }) => {
   return (
-    <StyledSvg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 448 512">
+    <StyledSvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <path
-        d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"
+        d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"
         fill={fill}
       />
     </StyledSvg>
-  );
-};
+  )
+}
