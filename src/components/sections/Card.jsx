846dd9c4f042fceda18450ea70b502c725bcb555
SofieFerrari <sofiestrahl@gmail.com>
2024-04-24 21:53:33 +0200

    added responsiveness to userInput and Card

--- src/components/sections/Card.jsx
+++ src/components/sections/Card.jsx
@@ -8,14 +8,14 @@ export const Card = () => {
   return (
     <>
       <section className="flex flex-col items-center">
-        <div className="h-[195px] w-60 pb-[24px] pt-[64px] text-center font-futura text-xl font-bold">
-          <h2>OUR RETREATS</h2>
-          <p className="h-[70px] text-center font-avenir text-base font-medium text-[#444446]">
+        <div className="pb-[24px] pt-[64px] text-center font-futura text-xl font-bold md:text-2xl xl:text-4xl">
+          <h2 className="pb-[20px] md:pb-[30px] xl:pb-[40px]">OUR RETREATS</h2>
+          <p className="md:px-30 px-10 text-center font-avenir text-lg font-medium text-[#444446] ">
             For sun-seekers and dreamers who appreciate barefoot-luxury and slow
             living.
           </p>
         </div>
-        <div className="grid grid-rows-4 gap-y-16 py-6">
+        <div className="grid grid-rows-4 gap-y-16 py-6 md:grid-rows-2 md:grid-cols-2 md:gap-x-16 xl:grid-cols-4 xl:grid-rows-1 xl:py-20">
           {cardArray.map((item) => (
             <div key={item.id} className=" mx-auto flex flex-col gap-6">
               <div>
@@ -31,7 +31,7 @@ export const Card = () => {
             </div>
           ))}
         </div>
-        <GeneralButton text="Book a retreat" customStyle="mt-20 mb-24" />
+        <GeneralButton text="Book a retreat" customStyle="mt-20 mb-24 md:" />
       </section>
     </>
   );
--- src/components/sections/UserInput.jsx
+++ src/components/sections/UserInput.jsx
@@ -3,19 +3,21 @@ import { GeneralButton } from '../GeneralButton';
 export const UserInput = () => {
   return (
     <>
-      <section className="gap[43px] grid h-[520px] w-full items-center justify-center bg-[#F8F7F3] px-[38px] py-6">
-        <div className="flex w-[284px] flex-col pb-[24px] text-center font-futura font-bold">
-          <h2 className="text-xl">JOIN OUR COMMUNITY</h2>
-          <p className="gap-[8px] justify-self-center px-10 text-center font-avenir text-base font-medium text-black">
+      <section className="grid w-full items-center justify-center bg-[#F8F7F3] px-[38px] py-20">
+        <div className="flex flex-col pb-[24px] text-center font-futura font-bold xl:mt-20">
+          <h2 className="font-futura text-xl font-bold md:text-2xl xl:text-4xl">
+            JOIN OUR COMMUNITY
+          </h2>
+          <p className=" justify-self-center px-10 pt-6 text-center font-avenir text-base font-medium text-black">
             Don’t miss out on free offerings, exclusive offers and new classes
             and retreats
           </p>
         </div>
-        <form className="grid h-[232px] w-[280px] gap-[24px]">
+        <form className="grid place-items-center gap-8 xl:flex xl:mt-8 xl:mb-12">
           <input
             type="text"
             placeholder="First Name"
-            className="h-[40px] w-[280px]  border-[1px] border-[#444446] bg-white px-[44,5px] py-[10,5px] text-center font-avenir font-medium text-black"
+            className="h-[40px] w-[280px] border-[1px] border-[#444446] bg-white  text-center font-avenir font-medium text-black"
           />
           <input
             type="text"
@@ -27,7 +29,7 @@ export const UserInput = () => {
             placeholder="Email Adress"
             className="h-[40px] w-[280px] border-[1px] border-[#444446] bg-white text-center font-avenir font-medium text-black"
           />
-          <GeneralButton text="Sign up" customStyle="justify-self-center" />
+          <GeneralButton text="Join us" customStyle="justify-self-center" />
         </form>
       </section>
     </>
