5ba8be7b4aa8c525e968b777ab7ecc987214134c
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-24 11:49:28 +0200

    Added styling for footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,30 +6,27 @@ import vimeoIcon from "../assets/icon-vimeo.svg";
 export const Footer = () => {
   return (
     <section className="flex flex-col items-center">
-      <img src={footerLogoMobile} alt="Logo" />
-      <ul className="flex flex-col items-center font-avengeance text-primary-darkblue">
+      <img src={footerLogoMobile} alt="Logo" className="mb-5" />
+      <ul className="flex flex-col items-center font-avengeance text-primary-darkblue gap-8 text-xs mb-10">
         <li>Contact</li>
         <li>Find your local club</li>
         <li>Insurances</li>
         <li>Our Partners</li>
         <li>FAQ</li>
       </ul>
-      <div className="flex gap-10">
+      <div className="flex gap-8 mb-10">
         <img src={instagramIcon} alt="Instagram Icon" />
         <img src={vimeoIcon} alt="Vimeo Icon" />
         <img src={facebookIcon} alt="Facebook Icon" />
       </div>
-      <div className="mt-4">
-        <label
-          htmlFor="search"
-          className="block text-sm font-medium text-gray-700" //Just random - should be changed
-        ></label>
+      <div>
+        <label htmlFor="search"></label>
         <input
           type="text"
           id="search"
           name="search"
           placeholder="Search"
-          className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring-blue-500 font-urbanist" //Just random - should be changed
+          className="px-2 py-2 w-40 h-10 border-2 border-primary-violetblue rounded-2xl focus:outline-none focus:border-primary-mediumpink font-urbanist shadow-inputShadow placeholder-primary-darkblue"
         />
       </div>
     </section>
