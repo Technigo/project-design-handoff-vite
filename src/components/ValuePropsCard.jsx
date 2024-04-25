c4cb3c4a4716059533f497d3e19e919936f55e8f
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-25 11:33:07 +0200

    Fixed issue with borders in ValueProps.

--- src/components/ValuePropsCard.jsx
+++ src/components/ValuePropsCard.jsx
@@ -6,10 +6,10 @@ export const ValuePropsCard = ({
   link,
 }) => {
   return (
-    <div className="bg-primary-valupropsviolet border-2 border-black rounded flex flex-col items-center shadow-boxShadow w-52 lg:w-64 lg:h-96">
+    <div className="bg-primary-valupropsviolet border border-black rounded flex flex-col items-center shadow-boxShadow w-52 lg:w-64 lg:h-96">
       <img
         src={image}
-        className="rounded-full object-cover border-solid border-2 border-black h-40 w-40 mt-6 lg:h-48 lg:w-48"
+        className="rounded-full object-cover border-solid border border-black h-40 w-40 mt-6 lg:h-48 lg:w-48"
       ></img>
       <div className="flex flex-col items-center mt-2 mr-2 ml-2 mb-6">
         <h4 className="font-urbanistitalic font-medium text-primary-darkblue lg:text-xl">
