2dfb8882845ee939b2e8cff1aed330e59b0f632b
Frida Svensson <fridamarisvensson@gmail.com>
2024-04-25 14:33:17 +0200

    Fix layout on mobile

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,7 +3,7 @@ import jsonData from "../benefits.json"
 
 export const Benefits = () => {
   return (
-    <div className="flex flex-col items-center gap-8 mx-4">
+    <div className="mx-4 flex flex-col items-center gap-8">
       {jsonData.map((item, index) => (
       <BenefitsCard key={index} {... item}/>
       ))}
--- src/components/BenefitsCard.jsx
+++ src/components/BenefitsCard.jsx
@@ -1,22 +1,26 @@
 export const BenefitsCard = ({ title, description, link, images, symbols }) => {
   return (
-    <div className="flex flex-row">
-      <picture>
+    <div className="grid grid-rows-1 grid-cols-[120px_auto] md:grid-cols[192px_auto_80px] lg:grid-cols[320px_auto_160px] relative">
+      <picture className="w-28 md:w-48 lg:w-80 border-2 border-red">
         <source
           media="(min-width: 668px)"
           srcSet={images.medium}
           alt="Karate girl"
         />
-        <img
-          src={images.small}
-          alt="Karate girl"
-          className="object-cover w-28 md:w-48 lg:w-80"
-        />
+        <img src={images.small} alt="Karate girl" className="object-contain" />
       </picture>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <a href="">{link}</a>
-      <picture>
+      <div>
+        <h3 className="font-avengeance text-primary-darkblue text-lg lg:text-2xl">
+          {title}
+        </h3>
+        <p className="font-urbanist text-primary-darkblue text-sm lg:text-lg">
+          {description}
+        </p>
+        <a href="" className="font-avengeance text-primary-mediumpink">
+          {link}
+        </a>
+      </div>
+      <picture className="absolute right-4 -bottom-9">
         <source
           media="(min-width: 668px)"
           srcSet={symbols.medium}
@@ -25,7 +29,7 @@ export const BenefitsCard = ({ title, description, link, images, symbols }) => {
         <img
           src={symbols.small}
           alt="Heart"
-          className="object-cover max-h-56 w-12 md:w-20 lg:w-40 "
+          className="object-contain object-top h-20 lg:max-h-56 w-12 md:w-20 lg:w-40"
         />
       </picture>
     </div>
