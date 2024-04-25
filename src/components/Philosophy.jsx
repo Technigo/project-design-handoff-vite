bb017297294fb7242a2c8e899f491bf6c94f8140
Your El1an3 <eliane_w@yahoo.com>
2024-04-25 23:46:23 +0200

    mobile styling for philo section

--- src/components/Philosophy.jsx
+++ src/components/Philosophy.jsx
@@ -2,7 +2,7 @@
 import styled from "styled-components";
 
 //import picture
-//import Lotus from "../../public/images/lotus.png";
+import Lotus from "../../public/images/lotus.png";
 
 //import hook
 import { useState } from "react";
@@ -12,18 +12,69 @@ import { HeadingTwo } from "../reusables/HeadingTwo";
 import { ParagraphTwo } from "../reusables/ParagraphTwo";
 
 //styles
-const PhiloSection = styled.section``;
+const PhiloSection = styled.section`
+  padding: 84px 16px;
+
+  @media all and (min-width: 744px) {
+    padding: 80px 16px;
+  }
+
+  @media all and (min-width: 1024px) {
+    padding: 174px 16px;
+  }
+`;
 
 // const ImageWrapper = styled.div`
-//   width: 100px;
-//   height: 100px;
-//   z-index: 2;
-//   object-fit: cover;
+//   padding-top: 33px;
+//   background-image: url(${Lotus});
+//   background-size: contain;
+//   background-repeat: no-repeat;
+//   background-position: center;
+//   opacity: 0.1;
 // `;
+
+// const PhiloWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+const ImageWrapper = styled.div`
+  padding-top: 33px;
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    /* height: 100%; */
+    background-image: url(${Lotus});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    opacity: 0.1;
+    /* pointer-events: none; */
+    z-index: -1; //set picture behind the context
+  }
+`;
+
+const PhiloWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px 0;
+  /* align-items: center; */
+  position: relative; //keeps content positioned correctly in the wrapper
+  z-index: 1;
+`;
 
 const PhiloTitle = styled.h3`
   font-weight: 500;
   font-size: 16px;
+  padding-bottom: 17px;
+  text-align: center;
 
   @media all and (min-width: 744px) {
     font-size: 24px;
@@ -32,11 +83,18 @@ const PhiloTitle = styled.h3`
   @media all and (min-width: 1024px) {
     font-size: 28px;
   }
+
+  &:hover {
+    cursor: pointer;
+    color: var(--primary-prpl);
+  }
 `;
 
 const PhiloText = styled.p`
   font-size: 12px;
   color: var(--primary-grey);
+  padding-bottom: 17px;
+  text-align: center;
 
   @media all and (min-width: 744px) {
     font-size: 14px;
@@ -100,13 +158,13 @@ const philoData = [
 //component
 
 export const Philosophy = () => {
-  const [selected, setSelected] = useState(null); // Track the selected item
+  const [selected, setSelected] = useState(null); //track the selected item
 
   const handleTitleClick = (index) => {
     if (selected === index) {
-      setSelected(null); // Deselect if clicked again
+      setSelected(null); //deselect if clicked again
     } else {
-      setSelected(index); // Select the clicked index
+      setSelected(index); //select the clicked index
     }
   };
 
@@ -118,14 +176,18 @@ export const Philosophy = () => {
         blossom uniquely in each of us. May you discover your center, your
         essence, and make room for it in your daily life.
       </ParagraphTwo>
-      {philoData.map((data, index) => (
-        <div key={index}>
-          <PhiloTitle onClick={() => handleTitleClick(index)}>
-            {data.title}
-          </PhiloTitle>
-          {selected === index && <PhiloText>{data.answer}</PhiloText>}
-        </div>
-      ))}
+      <ImageWrapper>
+        <PhiloWrapper>
+          {philoData.map((data, index) => (
+            <div key={index}>
+              <PhiloTitle onClick={() => handleTitleClick(index)}>
+                {data.title}
+              </PhiloTitle>
+              {selected === index && <PhiloText>{data.answer}</PhiloText>}
+            </div>
+          ))}
+        </PhiloWrapper>
+      </ImageWrapper>
     </PhiloSection>
   );
 };
