e1e89ef52cd0bbb7e13fa4fd79e9e62c8d1c1d17
Frida Svensson <fridamarisvensson@gmail.com>
2024-04-26 00:08:35 +0200

    Style hero and testimonials for tablet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,15 @@ import registerButton from "/assets/register-button01.png";
 
 export const Hero = () => {
   return (
-    <div className="relative mb-28">
+    <div className="relative mb-28 md:pb-52">
       <img
         src={heroImg}
-        className="object-cover h-full absolute"
+        className="object-cover size-full absolute"
         alt="Hero Image"
       />
-      <div className="flex flex-col w-72 top-28 pb-2 mx-4 relative">
+      <div className="flex flex-col w-72 top-28 md:top-80 pb-2 mx-4 md:mx-8 lg:mx-28 relative">
         <h1
-          className="font-avengeance text-primary-lightblue text-3xl mb-8 md:text-4xl lg:text-6xl lg:mt-20"
+          className="font-avengeance text-primary-lightblue text-3xl mb-8 md:text-4xl lg:text-6xl"
           style={{ WebkitTextStroke: "0.4px black" }}
         >
           YOUNG <br></br>HEROES,<br></br> BIG HEARTS
@@ -20,11 +20,11 @@ export const Hero = () => {
         <h2 className="font-urbanist bg-white text-primary-lightblue text-xl mb-4 lg:text-3xl ">
           We empower girls to find their inner dragons through karate.
         </h2>
-        <button>
+        <button className="md:absolute md:-top-28 md:right-0">
           <img src={registerButton} alt="Register Button" />
         </button>
       </div>
-      <div className="relative -bottom-28">
+      <div className="relative -bottom-28 md:-bottom-80">
         <Testimonials />
       </div>
     </div>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,18 +1,26 @@
 import waveMobile from "../assets/testimonials/wave-mobile.svg";
+import waveTablet from "../assets/testimonials/wave-tablet.svg";
 
 export const Testimonials = () => {
   return (
     <div className="h-40 relative overflow-x-scroll scroll-smooth">
+      <picture>
+      <source
+          media="(min-width: 668px)"
+          srcSet={waveTablet}
+          alt="Testimonials"
+        />
         <img
           src={waveMobile}
           className="h-40 object-cover object-left absolute overflow-visible"
           // style={{ width: "100%", height: "auto", maxWidth: "none" }}
         />
-      <div className="relative font-urbanistitalic text-primary-darkblue text-lg grid grid-cols-[190px_190px_190px_190px] text-center items-center px-4 gap-8 lg:text-2xl size-full">
-          <p>11 clubs in 7 cities within the Nordic region</p>
-          <p>Winner of 2021 Best Sport Intiative for Kids </p>
-          <p>Part or the JKA Sweden Group</p>
-          <p>First Kids sport offering an app solution </p>
+      </picture>
+      <div className="relative font-urbanistitalic text-primary-darkblue text-lg grid grid-cols-[190px_190px_190px_190px] text-center items-center mx-4 md:mx-8 lg:mx-28 gap-8 md:gap-14 lg:text-2xl size-full">
+        <p>11 clubs in 7 cities within the Nordic region</p>
+        <p>Winner of 2021 Best Sport Intiative for Kids </p>
+        <p>Part or the JKA Sweden Group</p>
+        <p>First Kids sport offering an app solution </p>
       </div>
     </div>
   );
