73715e8dfd70cf13e8a7206d0a7d62f840826ce7
Erica Mechler <erica.mechler1@gmail.com>
2024-04-25 16:33:22 +0200

    done with trust-indicators

--- src/components/Header/HeaderFooter.jsx
+++ src/components/Header/HeaderFooter.jsx
@@ -3,15 +3,15 @@ export const HeaderFooter = () => {
     <>
       <div className="py-8 flex flex-row justify-around md:m-w-6x1 md:py-10">
         <div className="flex flex-col gap-2 text-center ">
-          <h3 className="font-semibold text-4x1">5000+</h3>
+          <h3 className="font-medium text-2xl">5000+</h3>
           <p className="font-inter">Aktiva medlemskap</p>
         </div>
         <div className="flex flex-col gap-2 text-center">
-          <h3 className="font-semibold text-4x1">410</h3>
+          <h3 className="font-medium text-2xl">410</h3>
           <p className="font-inter">Fysioterapeuter och personliga tränare</p>
         </div>
-        <div className="hidden text-center md:block">
-          <h3 className="font-semibold text-4x1">36</h3>
+        <div className="hidden text-center md:flex flex-col gap-2">
+          <h3 className="font-medium text-2xl">36</h3>
           <p className="font-inter">Anläggningar runt om i Sverige</p>
         </div>
       </div>
