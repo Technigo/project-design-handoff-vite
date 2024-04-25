76720788c37fc55b37f7113d988dd60d47a577c5
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-25 15:11:13 +0200

    Added assets and text.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,15 @@
+import heroImg from "/assets/hero.png";
+import registerButton from "/assets/register-button01.png";
+
 export const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      <img src={heroImg}></img>
+      <h3>YOUNG HEROES, BIG HEARTS</h3>
+      <h2>We empower girls to find their inner dragons through karate.</h2>
+      <button>
+        <img src={registerButton} />
+      </button>
+    </div>
+  );
+};
