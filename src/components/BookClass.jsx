c99bcb9d1411f6c0d2849da153642212f927bda9
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-04-25 13:42:19 +0200

    worked on resuable component for CTA

--- src/components/BookClass.jsx
+++ src/components/BookClass.jsx
@@ -1,19 +1,12 @@
-import { Button } from "./reusable/Button"
+import { CTA } from "./reusable/CTA"
 
 export const BookClass = () => {
   return (
-    <div className="font-montserrat border-b border-t bg-off-black text-center">
-      <img src="/banner-tablet.png"></img>
-      <h2 className="text-white text-2xl font-medium mt-16">
-        Looking for a Challenge?
-      </h2>
-      <p className="text-white">Save your spot for the next class now!</p>
-      <div className="mt-6 mb-16">
-        <Button buttonText={"Book Now"} />
-      </div>
-      <div>
-        <img src="/banner-tablet.png" className="transform rotate-180" />
-      </div>
-    </div>
+    <CTA
+      title="Looking for a Challenge?"
+      description="Save your spot for the next class now!"
+      buttonText="Book Now"
+      showInput={false}
+    />
   )
 }
