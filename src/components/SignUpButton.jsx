0615375c04114a006bb37692bc71eae07f9eb107
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-25 11:00:05 +0200

    HeroSection mobile mocked up, Navbar still missing.

---
+++ src/components/SignUpButton.jsx
@@ -0,0 +1,8 @@
+export const SignUpButton = ({ buttonName }) => {
+
+  return (
+    <>
+      <button className="bg-main-blue hover:bg-hover-blue text-white font-montserrat w-36 h-12 rounded-lg drop-shadow-button-shadow active:drop-shadow-none active:translate-y-1">{buttonName}</button>
+    </>
+  );
+};
