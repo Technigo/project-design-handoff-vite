62ce41eb1c2e6cc25225a5c47361ff311578194d
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-25 17:23:27 +0200

    Added styling.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,32 +3,30 @@ import registerButton from "/assets/register-button01.png";
 
 export const Hero = () => {
   return (
-    <div className="container relative" style={{ height: "598px" }}>
+    <div className="container" style={{ height: "598px" }}>
       <div className="w-screen">
         <img
           src={heroImg}
-          className="object-cover w-full"
+          className="object-cover w-full relative"
           style={{ height: "598px" }}
           alt="Hero Image"
         />
       </div>
       <div
-        className="absolute w-screen pt-12 pl-5"
+        className="absolute flex flex-col w-80"
         style={{
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          top: "10%",
+          left: "5%",
           height: "598px",
-          width: "100%",
         }}
       >
         <h1
-          className="font-avengeance text-primary-lightblue text-3xl mb-8"
+          className="font-avengeance text-primary-lightblue text-3xl mb-8 md:text-4xl lg:text-6xl lg:mt-20"
           style={{ WebkitTextStroke: "0.4px black" }}
         >
           YOUNG <br></br>HEROES,<br></br> BIG HEARTS
         </h1>
-        <h2 className="font-urbanist bg-white text-primary-lightblue text-xl mb-4 ">
+        <h2 className="font-urbanist bg-white text-primary-lightblue text-xl mb-4 lg:text-3xl">
           We empower girls to find their inner dragons through karate.
         </h2>
         <button>
