f96f021e1ed106fe5077274210b8d1a260ea5f4a
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-25 15:38:35 +0200

    Review cards for mobile

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,5 +1,8 @@
-export const ReviewCard = () => {
+export const ReviewCard = ({ review }) => {
+  console.log(review);
   return (
-    <div>ReviewCard</div>
-  )
-}
+    <div className="border-2 border-dashed border-black m-4 px-2 py-6">
+      <p className="italic text-center text-sm">{review.review}</p>
+    </div>
+  );
+};
