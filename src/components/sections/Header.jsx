90f46e19b45811c783b2d67b9260f4af1f521bef
SofieFerrari <sofiestrahl@gmail.com>
2024-04-25 09:24:14 +0200

    styled hero

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -6,9 +6,9 @@ import { GeneralButton } from '../GeneralButton';
 
 export const Header = () => {
   return (
-    <header className="grid h-dvh grid-cols-1 grid-rows-[auto_auto_1fr] bg-cobra bg-cover bg-[40%] bg-no-repeat md:max-h-[1133px] xl:max-h-[1024px]">
+    <header className="grid h-dvh grid-cols-1 grid-rows-[auto_auto_1fr] bg-cobra bg-cover bg-[35%] bg-no-repeat md:max-h-[1133px] xl:max-h-[1024px]">
       <div className="mx-5 flex justify-center py-8 pt-7 xl:hidden">
-        <img className="h-16 md:h-20" src={logo} alt="logo" />
+        <img className="md:h-35 h-25 mt-7" src={logo} alt="logo" />
       </div>
       <nav className="mb-28 mt-10 hidden w-max justify-self-center xl:block">
         <ul className="flex flex-row items-end space-x-9 text-lg font-medium">
@@ -27,17 +27,17 @@ export const Header = () => {
         src={menuIcon}
         alt="hamburger menu"
       />
-      <div className="flex flex-col justify-center text-center	font-futura">
-        <h1 className="text-3xl font-bold xl:text-5xl">
+      <div className="mt-10 flex flex-col justify-center gap-3 text-center font-futura xl:mt-0">
+        <h1 className="text-4xl font-bold xl:text-6xl">
           EMPOWERING VINYASA YOGA
         </h1>
-        <h2 className="my-4 text-xl	font-bold xl:text-4xl">
+        <h2 className="my-4 text-3xl font-bold xl:text-5xl">
           Online studio & Retreats
         </h2>
-        <h3 className="pb-7	text-lg	font-medium xl:text-xl">
+        <h3 className="pb-7	text-2xl	font-medium xl:text-3xl">
           TO INSPIRE + UPLIFT YOU
         </h3>
-        <h3 className="text-lg font-medium xl:text-xl">Since 2010</h3>
+        <h3 className="mt-5 text-2xl font-medium xl:text-3xl">Since 2010</h3>
       </div>
       <GeneralButton
         text="Log in"
