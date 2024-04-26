8987269a8b3e341be0a2c2066cbfe46b9be427bb
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-04-26 14:53:58 +0200

    Added logo for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,33 @@
+import mobileLogo from "../assets/footerImages/mobile-logo.png";
 import styled from "styled-components";
 export const Footer = () => {
   return (
     <FooterContent>
-      <p>Learn, Laugh, and
-         Connect on the Slopes!</p>
+      <MobileLogo></MobileLogo>
+      <FooterText>
+        <h1>Learn, Laugh, and Connect on the Slopes!</h1>
+      </FooterText>
     </FooterContent>
   );
 };
 
 const FooterContent = styled.div`
-background-color: #234059;
-height: 600px;
-color: #FFC8DD;
+  background-color: #234059;
+  height: 600px;
+  color: #ffc8dd;
+  padding-top: 40px;
+`;
+
+const MobileLogo = styled.div`
+  background-image: url(${mobileLogo});
+  background-repeat: no-repeat;
+  height: 30px;
+  width: 80px;
+  margin-left: 60px;
+`;
+
+const FooterText = styled.div`
+  h1 {
     font-family: Philosopher;
     font-size: 30px;
     font-style: normal;
@@ -20,5 +36,5 @@ color: #FFC8DD;
     letter-spacing: 1.08px;
     padding-left: 100px;
     margin-bottom: 24px;
-
+  }
 `;
