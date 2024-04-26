126f1a136a59580dbe4a8d5b3ac5456dc0ca561f
Trista Shan <trista8703@hotmail.com>
2024-04-26 22:32:15 +0300

    class card done

--- src/components/classCards/ClassCards.jsx
+++ src/components/classCards/ClassCards.jsx
@@ -8,9 +8,9 @@ export const ClassCards = () => {
   // const {bgColor,fontColor, clickCard } = useGym()
 
   return (
-    <div className='flex flex-col justify-center items-center md:gap-y-[30px] gap-y-5 px-[60px] pt-7 '>
-    <h4 className='ubuntu sm:text-xl md:text-xl text-3xl text-white' >Some of our classes!</h4>
-    <div className='flex gap-[27px]'>
+    <div className='flex flex-col justify-center items-center sm:pt-[40px] md:pt-[80px] md:gap-y-1 lg:pt-[144px] lg:gap-y-5 lg:px-[60px]'>
+    <h4 className='ubuntu sm:text-xl  lg:text-3xl text-white' >Some of our classes!</h4>
+    <div className='flex sm:gap-1 md:gap-[30px] lg:gap-[27px]'>
       <ClassPieces image={ZumbaEnergetic} alt={'People are having Zumba class'} 
     courseName={'Zumba Energetic'} courseInfo={'A class for you that want a fast paced and high cardio workout!'}
     />
--- src/components/classCards/ClassPieces.jsx
+++ src/components/classCards/ClassPieces.jsx
@@ -9,16 +9,17 @@ export const ClassPieces = ({image, alt, courseName, courseInfo}) => {
       //     <p className="Ubuntu md:text-base text-5xl">{courseInfo}</p>
       //     </div>
       // </div>
-      <div className="flex h-[552px] w-[422px] flex-col gap-y-[25px] rounded-[10px] bg-purple px-[40px] pb-[45px] pt-[40px] text-white shadow-base shadow-yellow hover:cursor-pointer hover:bg-yellow hover:text-black sm:w-[257px] sm:h-[369px] sm:gap-y-1 sm:pb-[24px] sm:pt-1 md:h-auto md:w-full md:px-2 md:pb-2 md:pt-4">
+      <div className="flex flex-col sm:w-[257px] sm:h-[369px] sm:pt-1 sm:px-[15px] sm:pb-[24px] sm:gap-y-1 md:w-[334px] md:h-[382px] md:pt-[24px] md:px-[32px] md:pb-[32px] lg:w-[422px] lg:h-[552px] lg:pt-[40px] lg:px-[40px] lg:pb-[45px] lg:gap-y-[25px] bg-purple shadow-base shadow-yellow hover:cursor-pointer hover:bg-yellow hover:text-black rounded-[10px]">
         <img
           src={image}
           alt={alt}
-          className="h-[289px] w-[342px] rounded-[10px] sm:h-auto sm:w-full md:h-auto md:w-full"
+          className="sm:w-[226px] sm:h-[132px] rounded-[10px] md:w-[270px] md:h-[196px] lg:w-[342px] lg:h-[289px]"
         />
-        <div className="flex flex-col gap-y-2 sm:gap-y-[8px] sm:px-[15px] md:gap-y-1">
-          <h5 className="Ubuntu text-xl sm:text-lg md:text-lg">{courseName}</h5>
-          <p className="Ubuntu text-5xl md:text-base">{courseInfo}</p>
+        <div className="flex flex-col sm:gap-y-[8px] sm:px-[15px] md:gap-y-1 lg:gap-y-2">
+          <h5 className="ubuntu sm:text-lg lg:text-xl">{courseName}</h5>
+          <p className="ubuntu sm:text-5xl md:text-base lg:text-5xl">{courseInfo}</p>
         </div>
       </div>
     );
 }
+//h-[552px] w-[422px] flex-col gap-y-[25px] rounded-[10px] bg-purple px-[40px] pb-[45px] pt-[40px] text-white shadow-base shadow-yellow hover:cursor-pointer hover:bg-yellow hover:text-black sm:w-[257px] sm:h-[369px] sm:gap-y-1 sm:pb-[24px] sm:pt-1 md:h-auto md:w-full md:px-2 md:pb-2 md:pt-4
