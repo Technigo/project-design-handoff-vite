07082e66b4d652666ab78c92e29b11210cfa630f
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-26 09:46:13 +0200

    Added missing alt-tags on images for accessibility.

--- src/components/ValuePropsCard.jsx
+++ src/components/ValuePropsCard.jsx
@@ -9,6 +9,7 @@ export const ValuePropsCard = ({
     <div className="bg-primary-valupropsviolet border-half border-black rounded-md p-6 lg:p-6 flex flex-col items-center shadow-boxShadow min-w-52 w-52 lg:min-w-64 lg:w-64 ">
       <img
         src={image}
+        alt="Image of girls doing karate"
         className="rounded-full object-cover border-solid border border-black h-40 w-40 lg:h-48 lg:w-48"
       ></img>
       <div className="flex flex-col items-center mt-2 ">
@@ -19,13 +20,14 @@ export const ValuePropsCard = ({
           {title}
         </h3>
         <p className="font-urbanist text-primary-darkblue text-sm text-center lg:text-lg">
-          {description}<br></br>
-        <a
-          href={link}
-          className="font-urbanist text-primary-darkblue text-sm font-semibold lg:text-lg"
-        >
-          More
-        </a>
+          {description}
+          <br></br>
+          <a
+            href={link}
+            className="font-urbanist text-primary-darkblue text-sm font-semibold lg:text-lg"
+          >
+            More
+          </a>
         </p>
       </div>
     </div>
