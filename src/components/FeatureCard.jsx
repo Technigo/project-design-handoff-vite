cf184b823e09973e814fff14c0310ccb78d8fbcd
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-26 07:21:41 +0200

    smaller text for mobile

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,17 +1,11 @@
-import arrow from "../assets/icons/Forward.svg"
+import arrow from "../assets/icons/Forward.svg";
 import meditation from "../assets/images/meditation.png";
 import distanceWorking from "../assets/images/distance-working.png";
 import drinkingCoffee from "../assets/images/drinking-coffee.png";
 
 export const FeatureCard = ({ feature }) => {
   console.log(feature);
-    let img = "";
-    
-    let reducedContentCheck = false;
-
-    if ( feature.smallTitle === true ){
-        reducedContentCheck = true;
-    }
+  let img = "";
 
   switch (feature.title) {
     case "Work-Life Balance":
@@ -19,9 +13,11 @@ export const FeatureCard = ({ feature }) => {
       img = meditation;
       break;
     case "Get the job done":
+    case "目標達成の場":
       img = distanceWorking;
       break;
     case "Supportive Community":
+    case "充実のコミュニティ":
       img = drinkingCoffee;
       break;
   }
@@ -29,13 +25,13 @@ export const FeatureCard = ({ feature }) => {
   return (
     <div className="border border-black bg-background-neon-green rounded-lg drop-shadow-card m-4 flex">
       <img className="w-20 h-20 self-center" src={img} />
-          <div className="p-4">
-              {reducedContentCheck ? <h3 className="tablet:hidden">{feature.smallTitle} </h3> : <h3 className="hidden tablet:inline">{feature.title}</h3>}
-
-
-        <p>{feature.description}</p>
-          </div>
-          <img className="h-6 self-center" src={arrow} />
+      <div className="p-4">
+        <h3 className="block tablet:hidden">{feature.smallTitle} </h3>
+        <h3 className="hidden tablet:block">{feature.title}</h3>
+        <p className="block tablet:hidden">{feature.smallDescription}</p>
+        <p className="hidden tablet:block">{feature.description}</p>
+      </div>
+      <img className="h-6 self-center" src={arrow} />
     </div>
   );
 };
--- src/sections/FeatureReviewSection.jsx
+++ src/sections/FeatureReviewSection.jsx
@@ -1,9 +1,26 @@
+import {useState, useEffect} from "react"
+
 import { FeatureCard } from "../components/FeatureCard";
 import { ReviewCard } from "../components/ReviewCard";
 
 export const FeatureReviewSection = ({ featureData, reviewData }) => {
   console.log("Feature data: ", featureData);
   console.log("Review data: ", reviewData);
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+      const checkIfMobile = () => setIsMobile(window.innerWidth < 360);
+      checkIfMobile();
+
+      function handleResize() {
+        checkIfMobile();
+      }
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
   return (
     <div className="bg-background-neon-green py-6">
       {featureData.map((feature, index) => (
