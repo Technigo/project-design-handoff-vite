823c6dc90aad82bdd88018e322d0c321f3315f13
Katarina Sjölin <kat.sjolin@gmail.com>
2024-04-26 12:00:17 +0200

    changed mistake in importing header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
 import { Strength } from "./components/Strength";
 import { Reviews } from "./components/Reviews";
 
