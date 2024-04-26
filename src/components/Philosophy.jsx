db14938c2125b3aa50b64c540c67675a501244d2
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-04-26 19:41:05 +0200

    Update styles in tailwind.config.js, Reviews.css, index.css, and Philosophy.jsx

--- src/components/Philosophy.jsx
+++ src/components/Philosophy.jsx
@@ -5,20 +5,20 @@ export const Philosophy = () => {
   return (
     <>
 
-        <div className=" wrapper relative text-darkblue bg-yogawhite flex lpy-7 mx-auto align-middle">
-        <div id='slider' className='w-full h-full overflow-x-scroll whitespace-nowrap scroll smooth'>
+      <div className="font-inter wrapper relative text-darkblue bg-yogawhite flex py-[65px] mx-auto lg:py-[109px]">
+        <div id='slider' className='w-full h-full overflow-x-scroll whitespace-nowrap scroll smooth lg:overflow-x-hidden'>
           {articles.map((article, index) => (
             <div className="relative align-top py-7 h-full inline-block mx-3 max-w-[194px] md:flex-col  md:px-5 md:max-w-[234px] lg:max-w-[417px]" key={index}>
-              <img  className=" max-w-[194px] max-h-[137px] md:max-w-[234px] md:max-h-[167px] lg:max-w-[417px] lg:max-h-[310px]"
-               src={article.image}
-               alt={article.title}
-               />
-              <h3 className='font-semibold text-16 lg:text-30'>{article.title}</h3>
-              <p className="font-inter mt- mt-6 text-wrap mb-[]">{article.copy}</p>
-              <p className='text-16 md:text-16 lg:text-18'>Read more &gt;</p>
+              <img className=" max-w-[194px] max-h-[137px] md:max-w-[234px] md:max-h-[167px] lg:max-w-[417px] lg:max-h-[310px]"
+                src={article.image}
+                alt={article.title}
+              />
+              <h3 className='font-semibold text-25 md:text-25 lg:text-36 lg:mb-s40'>{article.title}</h3>
+              <p className=" mt-[25px] text-wrap text-16 md:text-20 lg:text-30">{article.copy}</p>
+              <p className='text-16 md:text-20 lg:text-30'>Read more &gt;</p>
             </div>
           ))}
-</div>
+        </div>
       </div>
 
     </>
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,30 +3,28 @@ import leftblob from "/images/leftblob.svg";
 import rightblob from "/images/rightblob.svg";
 import "./Reviews.css";
 
-
 export const Reviews = () => {
-
 
   return (
     <div className="review-wrap">
-    <div className="max-w-[1370px] mx-auto align-middle  bg-yogawhite flex-1  py-7 ">
-     <div className="reviews  bg-yogawhite  bg-no-repeat bg-contain md:mx-8 "   style={{
-        backgroundImage: `url(${leftblob}), url(${rightblob})`,
-        backgroundPosition: 'left bottom, right top',
-        backgroundSize: 'contain, contain',
-        minHeight:'111px',
-        maxHeight:'300px',
-      }}>
-        <div className="flex justify-evenly" >
-          {customerReviews.map((review, index) => (
-            <div className="reviews flex-col justify-items-start sm:w-24 m-2 text-16 md:w-32 md:text-20 lg:w-56 lg:text-25"  key={index} >
-              <p className="font-inter text-darkblue text-center ">{review}</p>
-            </div>
-          ))}
-        </div>
+      <div className="max-w-[1370px] mx-auto align-middle  bg-yogawhite flex-1  mt-[65px] pt-[20px]  ">
+        <div className="reviews  bg-yogawhite  bg-no-repeat bg-contain md:mx-8" style={{
+          backgroundImage: `url(${leftblob}), url(${rightblob})`,
+          backgroundPosition: 'left 15px, right -15px',
+          backgroundSize: 'contain, contain',
+          minHeight: '111px',
+          maxHeight: '300px',
+        }}>
+          <div className="flex justify-evenly" >
+            {customerReviews.map((review, index) => (
+              <div className="reviews flex-col justify-items-start sm:w-24 m-2 text-16 md:w-32 md:text-20 lg:w-56 lg:text-25" key={index} >
+                <p className="font-inter text-darkblue text-center ">{review}</p>
+              </div>
+            ))}
+          </div>
         </div>
 
-        </div>
+      </div>
     </div>
 
   );
