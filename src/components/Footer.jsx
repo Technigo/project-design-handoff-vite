9aa63f5f968bfc99f74d395d3f1480b213644a53
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-04-26 21:21:21 +0200

    Finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import tabletLogo from "../assets/footerImages/tablet-logo.png";
+import facebook from "../assets/followUsSection/facebook.png";
+import instagram from "../assets/followUsSection/instagram.png";
+import chevron from "../assets/followUsSection/chevron.png";
 import styled from "styled-components";
+
 export const Footer = () => {
   return (
     <FooterContent>
@@ -13,6 +17,17 @@ export const Footer = () => {
       <FooterList>FAQ</FooterList>
       <FooterList>Privacy Policy</FooterList>
       <FooterList>Terms and conditions</FooterList>
+      <FollowUs>
+        <Icons>
+          <FooterList>Follow us</FooterList>
+          <IconImage src={facebook} />
+          <IconImage src={instagram} />
+        </Icons>
+        <Language>
+          <FooterList>EN</FooterList>
+          <ChevronImage src={chevron} />
+        </Language>
+      </FollowUs>
     </FooterContent>
   );
 };
@@ -55,10 +70,39 @@ const FooterList = styled.li`
   display: flex;
   padding-left: 24px;
   padding-bottom: 10px;
+  padding-top: 6px;
   color: #fff;
   font-family: Mulish;
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
   line-height: 130%;
+`;
+
+const FollowUs = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 50px;
+`;
+
+const IconImage = styled.img`
+  height: 24px;
+  padding: 5px;
+`;
+
+const ChevronImage = styled.img`
+  height: 20px;
+  height: 13px;
+  padding-bottom: 10px;
+  padding-top: 8px;
+  padding-left: 10px;
+  padding-right: 24px;
+`;
+
+const Language = styled.div`
+  display: flex;
+`;
+
+const Icons = styled.div`
+  display: flex;
 `;
