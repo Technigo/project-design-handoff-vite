735c64dc02e2ef949d73b95727b973b4cba04cc9
maikanetaka <maikanetaka@gmail.com>
2024-04-26 18:43:44 +0900

    Add language toggle

--- src/sections/FooterSection.jsx
+++ src/sections/FooterSection.jsx
@@ -1,11 +1,20 @@
 import PropTypes from "prop-types";
 
-export const FooterSection = ({ data }) => {
+export const FooterSection = ({ data, onLanguageChange }) => {
   const { links, info } = data;
+
+    const toggleLanguage = () => {
+    onLanguageChange({ target: { value: data.locale === 'en' ? 'jp' : 'en' }});
+  };
 
   return (
   <footer className="bg-main-blue text-text-light font-montserrat text-sm font-extralight text-left">
-    <div className="p-6">
+      <div className="p-6">
+        <div className="language-toggle">
+        <button onClick={toggleLanguage}>
+          {data.locale === 'en' ? '日本語' : 'English'}
+        </button>
+      </div>
       <div className="mb-4 w-full md:w-auto">
         <a href="/" className="flex items-center justify-center md:justify-start ">
         </a>
@@ -41,7 +50,7 @@ export const FooterSection = ({ data }) => {
 );
 };
 
-
 FooterSection.propTypes = {
-  data: PropTypes.object.isRequired
+  data: PropTypes.object.isRequired,
+  onLanguageChange: PropTypes.func.isRequired
 };
