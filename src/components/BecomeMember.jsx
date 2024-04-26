e848a25a793c453bbe3d9a396da35091fba069a9
Your Name <you@example.com>
2024-04-26 09:25:00 +0200

    some exttra changes

--- src/components/BecomeMember.jsx
+++ src/components/BecomeMember.jsx
@@ -5,8 +5,8 @@ export const BecomeMember = () => {
     <section className="my-12  bg-peach" >
       <div className=" flex flex-col align-center justify-center mx-auto md:flex-row  gap-8 items-center  px-14 py-20 ">
           <img className="rounded-full rounded-b-none w-64 md:w-350 2xl:w-500" src={membership} alt="membership-image" />
-        <div className="flex flex-col gap-8 final-cta-content text-center md:w-350 2xl:w-500">
-          <h3 className="text-3xl font-bold">Det handlar om dig</h3>
+        <div className="flex flex-col gap-8 justify-center items-center text-center md:w-350 2xl:w-500">
+          <h3 className="text-3xl font-bold">Det handlar om dig.</h3>
           <p className="max-w-xs">
             Ditt välmående är våran prioritet. Gå med i Studio Mama&apos;s
             community och stärk din kropp och ditt sinne!
