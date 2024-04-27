484c64c7e94c03b1cb496ddcc17224151428cff7
Caracal23 <154092424+Caracal23@users.noreply.github.com>
2024-04-27 08:28:46 +0200

    adding some function to arrow in hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,19 @@ import { Button } from "./reusable/Button";
 import ArrowDown from "/arrow-down.svg";
 
 export const HeroSection = () => {
+  const onArrowClick = () => {
+    const trustIndicatorsSection = document.getElementById(
+      "trust-indicators-section"
+    );
+    window.scrollTo({
+      top:
+        trustIndicatorsSection.getBoundingClientRect().top +
+        window.scrollY -
+        (160 + 80),
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="relative bg-hero-pattern bg-no-repeat bg-cover bg-bottom object-none h-136 w-auto py-20 tablet:h-220 tablet:bg-hero-tablet desktop:bg-hero-desktop desktop:h-244">
       <div className="flex flex-col justify-center mt-24 tablet:mx-8">
@@ -16,7 +29,8 @@ export const HeroSection = () => {
         <Button buttonText={"Book Now"} />
       </div>
       <img
-        className="hidden desktop:block desktop:w-16 desktop:absolute desktop:inset-x-0 bottom-0"
+        onClick={onArrowClick}
+        className="hidden desktop:block desktop:w-16 desktop:absolute desktop:inset-x-1/2 desktop:bottom-0 cursor-pointer"
         src={ArrowDown}
       />
     </div>
--- src/components/TrustIndicators.jsx
+++ src/components/TrustIndicators.jsx
@@ -1,6 +1,9 @@
 export const TrustIndicators = () => {
   return (
-    <div className="my-20 mx-4 font-montserrat flex justify-between gap-5 sm:gap-10 tablet:gap-20 tablet:my-32 desktop:my-40 desktop:mx-32 desktop:gap-24">
+    <div
+      id="trust-indicators-section"
+      className="my-20 mx-4 font-montserrat flex justify-between gap-5 sm:gap-10 tablet:gap-20 tablet:my-32 desktop:my-40 desktop:mx-32 desktop:gap-24"
+    >
       <div className="flex flex-col text-center flex-1 desktop:flex-row">
         <p className="font-semibold text-2xl mb-2 tablet:text-4xl desktop:mr-4 desktop:text-6xl desktop:w-60">
           4.9
@@ -26,5 +29,5 @@ export const TrustIndicators = () => {
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
