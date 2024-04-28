7021dac8cb9b843ef37afa2ac0c1cbde5b9a84d7
Katarina Sjölin <kat.sjolin@gmail.com>
2024-04-28 14:32:35 +0200

    added some more styling for Footer and Strength

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ export const Footer = () => {
     <footer className="bottom-0 mt-2 w-full font-Inter text-darkblue">
         <section>
             <div className="bg-lightblue p-6 text-center">
-                <h2 className="text-lg">{t("footer.join")}</h2>
-                <input type="text" placeholder={t("footer.placeholder")} className="rounded-md text-center"/>
-                <button className=" bg-steelblue px-8 py-1 rounded-3xl transition hover:text-white ml-2">{t("footer.sign")}</button>
+                <h2 className="text-lg pb-1">{t("footer.join")}</h2>
+                <input type="text" placeholder={t("footer.placeholder")} className="bg-lightblue border border-darkblue py-0.5 rounded-2xl text-center text-darkblue max-sm:w-3/5"/>
+                <button className=" bg-steelblue px-4 py-0.5 rounded-3xl font-semibold transition hover:text-beige ml-2">{t("footer.sign")}</button>
             </div>
 
-            <div className="flex flex-col gap-2 text-center bg-steelblue p-6 desktop:hidden">
+            <div className="flex flex-col gap-2 text-center bg-steelblue p-4 desktop:hidden">
               <label className="text-lg font-semibold">{t("footer.studio")}</label>
               <ul className="flex flex-col">
                 <li>{t("footer.office")}</li>
@@ -23,25 +23,25 @@ export const Footer = () => {
               </ul>
             </div>
 
-            <div className="text-center bg-steelblue p-6 desktop:hidden">
+            <div className="text-center bg-steelblue p-4 desktop:hidden">
               <p className="text-lg font-semibold">{t("footer.shop")}</p>
 
-              <nav className="mt-2">
+              <nav className="mt-1">
                 <ul className="gap-2">
                   <li>
-                    <a className="transition hover:text-white" href="/">
+                    <a className="transition hover:text-beige" href="/">
                     {t("footer.props")}
                     </a>
                  </li>
 
                  <li>
-                   <a className="transition hover:text-white" href="/">
+                   <a className="transition hover:text-beige" href="/">
                    {t("footer.clothing")}
                   </a>
                  </li>
 
                  <li>
-                   <a className="transition hover:text-white" href="/">
+                   <a className="transition hover:text-beige" href="/">
                    {t("footer.reading")}
                    </a>
                  </li>
@@ -49,13 +49,13 @@ export const Footer = () => {
               </nav>
             </div>
 
-            <div className="text-center bg-steelblue p-6 desktop:hidden">
+            <div className="text-center bg-steelblue p-4 desktop:hidden">
               <p className="text-lg font-semibold">{t("footer.privacy")}</p>
 
-              <nav className="mt-2">
+              <nav className="mt-1">
                 <ul className="gap-2">
                   <li>
-                    <a className="transition hover:text-white" href="/">
+                    <a className="transition hover:text-beige" href="/">
                       {t("footer.conditions")}
                     </a>
                  </li>
@@ -63,32 +63,31 @@ export const Footer = () => {
               </nav>
             </div>
           
-            <div className="bg-steelblue w-full p-2 desktop:flex desktop:items-center desktop:justify-between">
-                <ul className="hidden desktop:flex flex-col justify-center mb-2 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row p-6">
+            <div className="bg-steelblue w-full desktop:flex desktop:items-center desktop:justify-around p-2">
+                <ul className="hidden desktop:flex flex-row justify-center mb-2 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5">
                 <li>
-                    <a href="/" className="hover:text-white transition-colors duration-300">{t("footer.about")}</a>
+                    <a href="/" className="transition hover:text-beige">{t("footer.about")}</a>
                 </li>
                 <li>
-                    <a href="/" className="hover:text-white transition-colors duration-300">{t("footer.contact")}</a>
+                    <a href="/" className="transition hover:text-beige">{t("footer.contact")}</a>
                 </li>
                 <li>
-                    <a href="/" className="hover:text-white transition-colors duration-300">{t("footer.shop")}</a>
+                    <a href="/" className="transition hover:text-beige">{t("footer.shop")}</a>
                 </li>
                 <li>
-                    <a href="/" className="hover:text-white transition-colors duration-300">{t("footer.privacy")}</a>
+                    <a href="/" className="transition hover:text-beige">{t("footer.privacy")}</a>
                 </li>
                 </ul>
 
-                <div className="p-6 text-center desktop:mb-2">
-                <select name="" id="" value={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)} className="bg-steelblue border-darkblue rounded-md">
-                    <option value="" selected disabled>{t("footer.select")}</option>
+                <div className="text-center desktop:mb-2">
+                <select name="" id="" value={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)} className="bg-steelblue border-4 border-darkblue rounded-2xl p-1">
                     <option value="en">{t("footer.en")}</option>
                     <option value="sv">{t("footer.sw")}</option>
                 </select>
                 </div>
             </div>
 
-            <div className="bg-steelblue p-6 text-center desktop:hidden">
+            <div className="bg-steelblue text-center desktop:hidden py-2">
               <p className="text-xs text-darkblue md:text-sm">Copyright &copy; 2024 Y Studio Stockholm</p>
             </div>
         </section>
@@ -112,3 +111,6 @@ export const Footer = () => {
 /*const changeLanguage = (language) => {
     i18n.changeLanguage(language)
   }*/
+
+
+/* <option value="" selected disabled>{t("footer.select")}</option>*/
--- src/components/Strength.jsx
+++ src/components/Strength.jsx
@@ -4,21 +4,21 @@ import strength3 from "/images/strength3.svg"
 
 export const Strength = () => {
   return (
-    <section className="flex flex-col justify-center p-2 gap-2 mx-3 tablet:mx-8 desktop:*:mx-36 font-Inter">
+    <section className="flex flex-col justify-center p-2 gap-6 mx-2 tablet:mx-8 desktop:mx-36 font-Inter mb-8">
       <div className="">
-        <img className="float-left mr-4 max-w-[150px] md:max-w-[350px] lg:max-w-[600px]" src={strength1} />
+        <img className="float-left mr-3 max-w-[120px] md:max-w-[350px] lg:max-w-[600px]" src={strength1} />
         <h2 className="text-lg tablet:text-2xl desktop:text-3xl font-semibold">Build strength - loose stress</h2>
-        <p className="text-base tablet:text-xl desktop:text2xl">Physically demanding while soothing your soul, Ashtanga yoga is the premium choice.</p>
+        <p className="text-base tablet:text-xl desktop:text2xl overflow-auto">Physically demanding while soothing your soul, Ashtanga yoga is the premium choice.</p>
       </div>
       <div className="">
-        <img className="float-left mr-4 max-w-[150px] md:max-w-[350px] lg:max-w-[600px]" src={strength2} />
+        <img className="float-left mr-3 max-w-[120px] md:max-w-[350px] lg:max-w-[600px]" src={strength2} />
         <h2 className="text-lg tablet:text-2xl desktop:text-3xl font-semibold">Close to where you are</h2>
-        <p className="text-base tablet:text-xl desktop:text2xl">With over 50 locations all over the city, we made it easy for you to exercise.</p>
+        <p className="text-base tablet:text-xl desktop:text2xl overflow-auto">With over 50 locations all over the city, we made it easy for you to exercise.</p>
       </div>
       <div>
-        <img className="float-left mr-4 max-w-[150px] md:max-w-[350px] lg:max-w-[600px]" src={strength3} />
+        <img className="float-left mr-3 max-w-[120px] md:max-w-[350px] lg:max-w-[600px]" src={strength3} />
         <h2 className="text-lg tablet:text-2xl desktop:text-3xl font-semibold">Flexible schedules</h2>
-        <p className="text-base tablet:text-xl desktop:text2xl">We believe in easy access yoga classes. Drop-in or schedule ahead. Sign up or on the go.</p>
+        <p className="text-base tablet:text-xl desktop:text2xl overflow-auto">We believe in easy access yoga classes. Drop-in or schedule ahead. Sign up or on the go.</p>
       </div>
     </section>
   )
