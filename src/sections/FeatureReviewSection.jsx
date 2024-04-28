120bed269888169defc091e3d3e4d1bc9e3c148b
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-28 19:08:55 +0200

    Last minute changes

--- src/sections/FeatureReviewSection.jsx
+++ src/sections/FeatureReviewSection.jsx
@@ -6,13 +6,13 @@ import { ReviewCard } from "../components/ReviewCard";
 export const FeatureReviewSection = ({ featureData, reviewData }) => {
   return (
     <div className="bg-background-neon-green">
-      <div className="max-w-screen-xl m-auto flex">
+      <div className="max-w-screen-xl m-auto flex flex-col desktop:flex-row">
         <div className="desktop:basis-7/12">
           {featureData.map((feature, index) => (
             <FeatureCard key={index} feature={feature} />
           ))}
         </div>
-        <div className="flex flex-col tablet:flex-row desktop:flex-col desktop:basis-5/12 justify-center">
+        <div className="flex flex-col tablet:flex-row desktop:flex-col tablet:basis-auto desktop:basis-5/12 justify-center">
           {reviewData.map((review, index) => (
             <ReviewCard key={index} review={review} />
           ))}
