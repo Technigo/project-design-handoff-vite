1c85984fb4dae643902c196965ba05b0624e81cb
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-04-29 11:50:50 +0200

    Refactor App.jsx and Philosophy.jsx to improve code readability and fix styling issues

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,7 @@ export const App = () => {
     <Strength />
     <Footer />
   </div>
-=========
-  return (
-    <div className="flex flex-col">
-      <Header />
-      <Reviews />
-      <Philosophy />
-      <Strength />
-      <Footer />
-    </div>
+
   )
->>>>>>>>> Temporary merge branch 2
+
 }
