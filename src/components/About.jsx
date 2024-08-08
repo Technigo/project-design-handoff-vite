7d9fc86fd0e4782f67f443e141f28839748f1b1a
Cornelia Dahlin <cornelia.dahlin@proton.me>
2024-08-08 02:01:38 +0200

    Responsiveness + added arrow to smaller screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const AboutContainer = styled.section`
@@ -8,9 +9,11 @@ const AboutContainer = styled.section`
   align-items: center;
   justify-content: center;
   padding: 20px 16px;
+
   @media (min-width: 744px) {
     padding: 20px 24px;
   }
+
   @media (min-width: 1024px) {
     padding: 20px 80px;
     flex-direction: row;
@@ -23,6 +26,7 @@ const AboutTitleWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 5px;
+
   @media (min-width: 1024px) {
     align-items: flex-start;
     gap: 0px;
@@ -37,7 +41,7 @@ const AboutSubtitle = styled.h1`
   text-align: center;
   margin: 0;
 
-  @media (min-width: 744px) {
+  @media (min-width: 745px) {
     font-size: 64px;
   }
 
@@ -54,7 +58,7 @@ const AboutTitle = styled.h1`
   text-align: center;
   margin: 0;
 
-  @media (min-width: 744px) {
+  @media (min-width: 745px) {
     font-size: 124px;
   }
 
@@ -67,38 +71,90 @@ const AboutText = styled.p`
   font-family: "Poppins", sans-serif;
   font-size: 16px;
   text-align: left;
+  line-height: 24px;
+  margin-top: 15px;
+  padding-bottom: 20px;
+  position: relative;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  column-gap: 33px;
-  align-items: center;
-  max-width: 100%;
-  padding-bottom: 20px;
-  margin-top: 15px;
-  line-height: 24px;
+
   @media (min-width: 1024px) {
     max-width: 50%;
   }
+
+  @media (max-width: 745px) {
+    font-size: 14px;
+    line-height: 21px;
+    text-align: center;
+    margin-top: 25px;
+    grid-template-columns: 1fr;
+  }
+
+  @media (max-width: 430px) {
+    display: -webkit-box;
+    -webkit-line-clamp: ${(props) =>
+      props.isExpanded
+        ? "unset"
+        : "4"}; /* Number of lines to show when collapsed */
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    max-height: ${(props) =>
+      props.isExpanded
+        ? "none"
+        : "63px"}; /* Adjust height for collapsed state */
+  }
+`;
+
+const ReadMoreLink = styled.a`
+  font-family: "Poppins", sans-serif;
+  font-size: 14px;
+  color: black;
+  cursor: pointer;
+  text-decoration: none;
+  margin-top: 25px;
+  display: none;
+
+  @media (max-width: 430px) {
+    display: flex;
+  }
+`;
+
+const ArrowIcon = styled.img`
+  width: 47.5px;
+  height: 14px;
+  margin-left: 15px;
+  align-self: center;
 `;
 
 export const About = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <AboutContainer>
       <AboutTitleWrapper>
         <AboutSubtitle>About</AboutSubtitle>
         <AboutTitle>SHANTI</AboutTitle>
       </AboutTitleWrapper>
-      <AboutText>
+      <AboutText isExpanded={isExpanded}>
         <span>
           Shanti believes in fostering inner peace and personal growth. Our
           mission is to provide urban sanctuaries where individuals can connect,
           explore, and find resilience.
-        </span>{" "}
+        </span>
         <span>
           Named after the Sanskrit word for &#34;peace&#34;, Shanti encapsulates
           our commitment to fostering inner tranquility and empowerment among
           our students.
         </span>
       </AboutText>
+      <ReadMoreLink onClick={toggleExpanded} href="#read-more">
+        {isExpanded ? "Read less" : "Read more"}
+        <ArrowIcon src="/icons/arrow.svg" alt="Arrow icon" />
+      </ReadMoreLink>
     </AboutContainer>
   );
 };
