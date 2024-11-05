91e5cd7e2988a5e317b39d398f7b27f8fcb0744d
Zoe <zoe.lindqvist@gmail.com>
2024-11-05 20:35:21 +0100

    typography compo

---
+++ src/ui/Typography.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledH1 = styled.h1`
+  color: #331709;
+  text-align: center;
+  font-family: Italiana;
+  font-size: 2.25rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 3.375rem */
+  letter-spacing: -0.02475rem;
+`;
+
+export const StyledH2 = styled.h2`
+  color: var(--Labels-Primary, #000);
+  text-align: center;
+  font-family: Poppins;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+`;
+
+export const StyledBody = styled.p`
+  color: var(--Labels-Primary, #000);
+  text-align: center;
+  font-family: Poppins;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
