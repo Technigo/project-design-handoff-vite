be41221fc5789e98755085b5517acf083a57b5ff
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-11-05 14:37:19 +0100

    started with project design handoff

---
+++ src/re-components/Typography.jsx
@@ -0,0 +1,19 @@
+
+import styled from "styledcomponents";
+
+export const H1 = styled.h1`
+color: var(--font-color-primary);
+text-shadow: 0px 0px 2px rgba(255, 68, 17, 0.30);
+font-family: "Bebas Neue", sans-serif;
+font-size: 92px;
+font-weight: 400;
+line-height: 100%; 
+letter-spacing: 1px;
+text-transform: uppercase;
+`
+
+export const H2 = 
+
+export const H3 = 
+
+export const BodyText = 
