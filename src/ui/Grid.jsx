2c9c7f7531cbab60c6d656d869934cd66f46ea08
Zoe <zoe.lindqvist@gmail.com>
2024-11-06 12:18:34 +0100

    grid

---
+++ src/ui/Grid.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Grid = styled.section`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+
+  width: 24.375rem;
+  height: 36.625rem;
+  flex-shrink: 0;
+`;
