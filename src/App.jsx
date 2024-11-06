348c83099e11f212954df826bb8b563255e4d3de
Anna <annah80@gmail.com>
2024-11-06 22:42:48 +0100

    created a globalstyles.jsx file and moved all css there, created a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { Header } from "./components/Header/Header"
 // import { PersonalizedSpells } from "./components/PersonalizedSpells/PersonalizedSpells"
 // import { Footer } from "./components/Footer/Footer"
 import spellsData from "./spells.json";
-import "./App.css"
+import { GlobalStyle } from "./styles/GlobalStyle"
 import { Library } from "./components/Library/Library";
-
-
-
 
 export const App = () => {
 
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Library spellsData={spellsData} />
       {/* <PersonalizedSpells />
---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  /* CSS Reset */
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+:root {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+
+
+body {
+  background: linear-gradient(180deg, #360C25 4.67%, #E1C9D8 18%, #E1C9D8 75%, #360C25 100%);
+  background: -webkit-linear-gradient(180deg, #360C25 0%, #E1C9D8 50%, #360C25 100%);
+  background: -moz-linear-gradient(180deg, #360C25 0%, #E1C9D8 50%, #360C25 100%);
+  background: linear-gradient(180deg, #360C25 0%, #E1C9D8 50%, #360C25 100%);
+  width: 100%;
+  height: 1500px;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+`
