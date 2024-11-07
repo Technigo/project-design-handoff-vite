cd37ef435f2e6c69045b7292aed5900c8e43d582
gabster94 <iofe.gabriella@gmail.com>
2024-11-07 16:40:37 +0100

    went back to the original code in vite.config.js due to error 500

---
+++ src/sections/socialproof/Socialproof.jsx
@@ -0,0 +1,7 @@
+import { ReviewCard } from "../../re-components/ReviewCard";
+
+export const SocialProof = () => {
+    return (
+        <p>I am the SocialProof section {ReviewCard} </p>
+    )
+}
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [
-        (
-          "babel-plugin-styled-components",
-          {
-            displayName: true,
-            fileName: false
-          }
-        )
-      ]
-    }
-  })],
+  plugins: [react()],
 })
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react({
+//     babel: {
+//       plugins: [
+//         (
+//           "babel-plugin-styled-components",
+//           {
+//             displayName: true,
+//             fileName: false
+//           }
+//         )
+//       ]
+//     }
+//   })],
+// })
