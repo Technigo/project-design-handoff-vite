af9d0a9bc57ba8d50f616d00658cb2fa719e85b0
Anna <annah80@gmail.com>
2024-11-07 22:05:17 +0100

    added h2 styling and export in typography

---
+++ src/components/PersonalizedSpells/CarouselCard.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import styled from 'styled-components';
+import { Button } from '../../ui/Button';
+import { Typography } from '../../ui/Typography';
+
+
+
+const Card = styled.article`
+  display: flex;
+  width: 330px;
+	height: 110px;
+	padding: 16px;
+	justify-content: center;
+	align-items: center;
+  scroll-snap-align: start;
+  cursor: pointer; 
+  transition: transform 0.2s;
+	border-radius: 16px;
+	background: #EEE7EB;
+	z-index: 1;
+
+  /* &:hover {
+    transform: scale(1.05);   
+  } */
+`;
+
+const P = styled.p`
+	color: #000;
+	text-align: center;
+	font-family: "Josefin Sans";
+	font-size: 16px;
+	font-style: italic;
+	font-weight: 400;
+	line-height: 150%; /* 24px */
+`;
+
+
+
+export const CarouselCard = ({ text }) => {
+
+  return (
+    	<Card>
+        <P>{text}</P>
+      	{/* <Typography text={text} level={4} /> */}
+    	</Card>
+  );
+};
--- src/ui/Typography.jsx
+++ src/ui/Typography.jsx
@@ -2,7 +2,7 @@
 import styled from 'styled-components';
 
 
-const Level1 = styled.h1`
+const Level1 = styled.h1` 
 color: #000;
 text-align: center;
 font-family: "Yeseva One";
@@ -11,12 +11,33 @@ font-style: normal;
 font-weight: 400;
 margin: 0;
 
-
    //Desktop styling
    @media (min-width: 1200px) {
     font-size: 6rem;
   
   }
+`
+
+const Level2 = styled.h2`
+color: #FFF;
+text-align: center;
+font-family: "Yeseva One";
+font-size: 2.25rem;
+font-style: normal;
+font-weight: 400;
+margin: 0;
+
+`
+
+const Level3 = styled.h3`
+color: #000;
+text-align: center;
+font-family: "Yeseva One";
+font-size: 1.5rem;
+font-style: light;
+font-weight: 400;
+margin: 0;
+
 `
 
 const Typography4 = styled.p`
@@ -64,10 +85,12 @@ line-height: 1.2375rem;
 export const Typography = ({ typography, level = 2, className }) => {
   // Use `Typography4`, 5 and 6 for level 4,5 and 6 otherwise dynamically select `h1`, `h2`, etc.
   const Tag = level === 1 ? Level1
-    : level === 4 ? Typography4
-      : level === 5 ? Typography5
-        : level === 6 ? Typography6
-          : `h${level}`; // Use `h2`, `h3` directly for levels 2 and 3
+    : level === 2 ? Level2
+      : level === 3 ? Level3
+        : level === 4 ? Typography4
+          : level === 5 ? Typography5
+            : level === 6 ? Typography6
+              : `h${level}`; // Use `h2`, `h3` directly for levels 2 and 3
   return <Tag className={`typography typography${level} ${className}`}>{typography}</Tag>
 };
 
