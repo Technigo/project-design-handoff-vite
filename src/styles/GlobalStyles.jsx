6698f6acf03b1ee197f9d67514d2146e93e6a009
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-11-07 17:24:35 +0100

    added content in themes.js and removed reduntant code from globalstyles.jsx

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -1,18 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle `
-:root {
-  margin: 0;
-  
-  --color-primary: #FFF6F4;
-  --color-secondary: #F93701;
-  --color-tertiary: #FFE675; 
-
-  --font-color-primary: #FFFCFB;
-  --font-color-body: #1E1E1E;
-
-
-}
+ * {
+    margin: 0;
+    padding: 0;
+  }
 
 code {
 
