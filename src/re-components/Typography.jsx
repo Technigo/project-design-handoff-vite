a415c2054e95c26380cd6ba57ceab00cde77df48
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-11-07 19:11:06 +0100

    added content in introcontent

--- src/re-components/Typography.jsx
+++ src/re-components/Typography.jsx
@@ -4,14 +4,12 @@ export const H1 = styled.h1 `
   font-family: ${({ theme }) => theme.fonts.heading};
   font-size: ${({ theme }) => theme.fontSizes.h1};
   line-height: ${({ theme }) => theme.lineHeights.h1};
-  font-weight: ${({ theme }) => theme.fontWeights.bold};
   color: ${({ theme }) => theme.text.secondary};
 `;
 export const SubH1 = styled.h1 `
   font-family: ${({ theme }) => theme.fonts.heading};
   font-size: ${({ theme }) => theme.fontSizes.subH1};
   line-height: ${({ theme }) => theme.lineHeights.subH1};
-  font-weight: ${({ theme }) => theme.fontWeights.bold};
   color: ${({ theme }) => theme.text.secondary};
 `;
 
@@ -25,12 +23,11 @@ export const BodyText = styled.p`
   font-family: ${({ theme }) => theme.fonts.body};
   font-size: ${({ theme }) => theme.fontSizes.bodyLarge};
   line-height: ${({ theme }) => theme.lineHeights.bodyLarge};
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
 `;
 
 export const FinePrint = styled.p`
   font-family: ${({ theme }) => theme.fonts.body};
   font-size: ${({ theme }) => theme.fontSizes.finePrint};
   line-height: ${({ theme }) => theme.lineHeights.finePrint};
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  color: ${({ theme }) => theme.text.secondary};
 `;
