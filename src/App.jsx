e3848956043af47d65ba5f4dc5e6c0cf8a47f821
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-11-07 14:20:14 +0100

    Added content to ParagraghBody component and footer icons to assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navigation } from "./components/Navigation";
 import { Header } from "./components/Header";
+import { BodyParagraph } from "./components/BodyParagraph";
 
 export const App = () => {
   return (
     <div>
       <Navigation />
       <Header />
+      <BodyParagraph />
     </div>
   );    
 };
---
+++ src/components/BodyParagraph.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTextContainer = styled.div`
+	width: 371px;
+	height: 143px;
+	flex-shrink: 0;
+	padding-left: 16px;
+	padding-right: 16px;
+	padding-bottom: 32px;
+	padding-top: 32px;
+`;
+const StyledParagraph = styled.p`
+	color: #545F71;
+	font-feature-settings: 'calt' off;
+	font-family: "Open Sans";
+	font-size: 16px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: 30px; /* 187.5% */
+	letter-spacing: -0.16px;
+	margin: 0;
+`;
+
+export const BodyParagraph = () => {
+  return (
+		<StyledTextContainer>
+			<StyledParagraph>Många barnfamiljer kämpar ekonomiskt under vintern – stötta dem och bidra till en glädjefylld jul. Din gåva kan vara skillnaden mellan en tuff tid och en magisk högtid. </StyledParagraph>
+		</StyledTextContainer>
+  );
+};
