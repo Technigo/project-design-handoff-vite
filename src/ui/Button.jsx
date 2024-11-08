7d287342f138ac7eb9eef571548f9a24635225d9
JoyceKuode <joyce.kuo@me.com>
2024-11-08 19:35:05 +0100

    Fix button color and sizing

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  background-color: #5d6d50; /* Dark green color */
-  color: white;
-  border: none;
-  border-radius: 20px;
-  padding: 8px 16px;
-  font-size: 1rem;
-  font-weight: bold;
-  cursor: pointer;
-  display: flex;
+
+  display: inline-flex;
   align-items: center;
   justify-content: center;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Optional shadow for depth */
+  background-color:  ${({ theme }) => theme.colors.secondary};
+  color: ${({theme}) => theme.colors.primary};
+  border: none;
+  height: 2.5rem;
+  border-radius: 1.5625rem;
+  padding: 0.5rem 4rem;
+  font-size: 1.25rem;
+  font-weight: 400;
+  cursor: pointer;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   transition: background-color 0.3s ease;
 
   &:hover {
