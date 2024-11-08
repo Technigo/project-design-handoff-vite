83447a693b29c86b21f9f57bf184fb6b823c1c44
esstefanny <estefannymorana@gmail.com>
2024-11-08 19:50:01 +0100

    added Heading3 component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigation } from "./components/Navigation";
 import { Header } from "./components/Header";
 import { BodyParagraph } from "./components/BodyParagraph";
+import Heading3 from "./components/Heading3";
 import { CardInfo } from "./components/CardInfo";
 
 import { Footer } from "./components/Footer";
@@ -12,6 +13,7 @@ export const App = () => {
       <Navigation />
       <Header />
       <BodyParagraph />
+      <Heading3>Hjälp till och gör skillnad!</Heading3>
       <CardInfo /> 
       <Footer />
     </div>
---
+++ src/components/Heading3.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Heading3 = ({ children }) => {
+  const style = {
+    color: '#1C7D79',
+    textAlign: 'center',
+    fontFamily: 'Schoolbell, sans-serif',
+    fontSize: '36px',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: '32px', // 88.889%
+    letterSpacing: '-0.72px',
+  };
+
+  return <h3 style={style}>{children}</h3>;
+};
+
+export default Heading3;
