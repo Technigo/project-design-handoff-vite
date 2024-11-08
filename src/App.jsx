8d00ffe53269e13d14a04830a8eae41133782e08
esstefanny <estefannymorana@gmail.com>
2024-11-08 20:25:46 +0100

    added animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header";
 import { BodyParagraph } from "./components/BodyParagraph";
 import Heading3 from "./components/Heading3";
 import { CardInfo } from "./components/CardInfo";
-
+import Animation from "./components/Animation";
 import { Footer } from "./components/Footer";
 
 export const App = () => {
@@ -15,6 +15,7 @@ export const App = () => {
       <BodyParagraph />
       <Heading3>Hjälp till och gör skillnad!</Heading3>
       <CardInfo /> 
+      <Animation />
       <Footer />
     </div>
   );    
