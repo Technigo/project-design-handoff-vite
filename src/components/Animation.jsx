5582d1e232719016eb2539020e3021332001a348
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-11-08 15:06:49 +0100

    Animation component done

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 
+const AnimationBox = styled.div`
+  padding-top: 58px;
+`;
+
 const AnimationContainer = styled.div`
   width: 380px;
   height: 134px;
   display: flex;
+  flex-direction: column;
   flex-shrink: 0;
   margin: 0 auto;
   align-items: center;
@@ -14,7 +19,15 @@ const AnimationContainer = styled.div`
   background: #FDF8F5;
 `;
 
-const StaticText = styled.span`
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Centers StaticText */
+  width: 351px;
+  height: 76px;
+`;
+
+const StaticText = styled.div`
   color: #545F71;
   font-family: "Open Sans";
   font-size: 16px;
@@ -22,9 +35,10 @@ const StaticText = styled.span`
   font-weight: 400;
   line-height: 36px; /* 225% */
   letter-spacing: -0.16px;
+  align-self: flex-start;
 `;
 
-const DynamicText = styled.span`
+const DynamicText = styled.div`
   color: #1C7D79;
   font-family: Schoolbell;
   font-size: 24px;
@@ -32,13 +46,15 @@ const DynamicText = styled.span`
   font-weight: 400;
   line-height: 36px; /* 150% */
   letter-spacing: -0.24px;
+  align-self: flex-start;
+  // margin-left: 30px;
 `;
 
 export const Animation = () => {
   const phrases = [
+    "kan hålla händerna varma",
     "får äta sig mätta",
-    "kan gå i skolan",
-    "får uppleva en trygg jul"
+    "får en julklapp att öppna"
   ];
 
   const [index, setIndex] = useState(0);
@@ -52,11 +68,14 @@ export const Animation = () => {
     return () => clearInterval(interval); // Clean up the interval on unmount
   }, [phrases.length]);
   
-  
   return (
-    <AnimationContainer>
-      <StaticText>Tack för att du är med och bidrar till att barn</StaticText>
-      <DynamicText>{phrases[index]}</DynamicText>
-    </AnimationContainer>
+    <AnimationBox>
+      <AnimationContainer>
+        <TextContainer>
+          <StaticText>Tack för att du är med och bidrar till att barn</StaticText>
+          <DynamicText>{phrases[index]}</DynamicText>
+        </TextContainer>
+      </AnimationContainer>
+    </AnimationBox>
   )
 }
