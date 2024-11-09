1942c7a480d769b2b5ffd57259c7a50982a381e9
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-11-09 12:43:09 +0100

    Tried updating responsiveness on Navigation but not working correct yet

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,21 +3,16 @@ import styled from "styled-components";
 import HeaderLogo from "../assets/HeaderLogo.png";
 import MenuIcon from "../assets/MenuIcon.svg";
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 // Container for navigation bar
 const NavContainer = styled.div`
   display: flex;
-  width: 412px;
+  width: 100vw; // Full viewport width for NavContainer
   height: 53px;
   justify-content: space-between;
   align-items: center;
-  flex-shrink: 0;
-  background: #FDF8F5;
+  background: #fdf8f5;
+  padding: 0 16px; // Padding for consistent spacing on both sides
+  box-sizing: border-box; // Ensures padding doesn’t add to width
   position: relative;
 `;
 
@@ -26,17 +21,17 @@ const StyledHeaderLogo = styled.img`
   width: 171px;
   height: 23px;
   flex-shrink: 0;
-  padding-left: 16px;
 `;
 
+// Menu icon styling
 const StyledMenuIcon = styled.img`
   width: 45px;
   height: 45px;
   flex-shrink: 0;
-  padding-right: 16px;
   cursor: pointer;
 `;
 
+// Menu styling for dropdown
 const StyledMenu = styled.div`
   display: ${({ $isOpen }) => ($isOpen ? "flex" : "none")};
   flex-direction: column;
@@ -44,8 +39,8 @@ const StyledMenu = styled.div`
   width: 60px;
   height: auto;
   top: 53px;
-  right: 0;
-  background-color: #FDF8F5;
+  right: 16px; // Align menu to the right edge with padding
+  background-color: #fdf8f5;
   padding: 16px;
   z-index: 1;
   border-radius: 0 0 4px 4px;
@@ -69,17 +64,21 @@ export const Navigation = () => {
   };
 
   return (
-    <Wrapper>
-      <NavContainer>
-        <StyledHeaderLogo src={HeaderLogo} alt="Räddningsmissionen logo in orange and grey colors" />
-        <StyledMenuIcon src={MenuIcon} alt="Hamburger menu" onClick={handleMenuClick} />
-        {/* Pass $isOpen instead of isOpen */}
-        <StyledMenu $isOpen={isMenuOpen}>
-          <StyledMenuItem>Home</StyledMenuItem>
-          <StyledMenuItem>About</StyledMenuItem>
-          <StyledMenuItem>Contact</StyledMenuItem>
-        </StyledMenu>
-      </NavContainer>
-    </Wrapper>
+    <NavContainer>
+      <StyledHeaderLogo
+        src={HeaderLogo}
+        alt="Räddningsmissionen logo in orange and grey colors"
+      />
+      <StyledMenuIcon
+        src={MenuIcon}
+        alt="Hamburger menu"
+        onClick={handleMenuClick}
+      />
+      <StyledMenu $isOpen={isMenuOpen}>
+        <StyledMenuItem>Home</StyledMenuItem>
+        <StyledMenuItem>About</StyledMenuItem>
+        <StyledMenuItem>Contact</StyledMenuItem>
+      </StyledMenu>
+    </NavContainer>
   );
 };
