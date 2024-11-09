8f6d629cc1dbc556b1c0deccac99b18036ae4d38
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-11-09 13:50:49 +0100

    tried fixing the slideshow

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -1,12 +1,14 @@
 
 import { Introcontent } from "./Introcontent";
 import { Slideshow } from "./Slideshow" 
+import { StartTodayButton1 } from "../main/StartTodayButton1"; 
 
 export const Header = () => {
     return (
         <section>
             <Introcontent/>
             <Slideshow/>
+            <StartTodayButton1/>
         </section>
     )
 }
