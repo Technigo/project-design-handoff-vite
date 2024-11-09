fa0be92902a8de12522fad78d99fa05d99061626
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-11-09 01:01:15 +0100

    added animated logo in the footer

---
+++ src/re-components/LogoAnimation.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import heartslogo from "../../public/logo/heartslogo.svg"
+
+
+// Container for the logo
+const LogoContainer = styled.div`
+  width: 252px;
+height: 252px;
+flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeartLogo = styled.img`
+width: 100%;
+height: 100%;
+`;
+
+
+export const LogoAnimation = () => {
+  return (
+    <LogoContainer>
+      <HeartLogo src={heartslogo} alt="logo with blinking hearts" />
+    </LogoContainer>
+  );
+};
+
+
+
+
+
--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { FinePrint } from "../../re-components/Typography"
+import { LogoSection } from "./LogoSection"
 
 // import { FooterButtons } from ".//InfoLinks"
 import { SocialLinks } from "./SocialLinks"
@@ -7,6 +8,7 @@ export const Footer = () => {
   return (
     <>
     <SocialLinks/>
+    <LogoSection/>
     <p>I am the Footer</p>
     </>
   )
