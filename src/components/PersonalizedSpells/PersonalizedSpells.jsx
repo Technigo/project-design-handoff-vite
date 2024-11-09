3b8cf171165f3064ab00ad0961246b5406495e60
Anna <annah80@gmail.com>
2024-11-09 21:54:36 +0100

    added an animation for the quotes instead of a button to click on

--- src/components/PersonalizedSpells/PersonalizedSpells.jsx
+++ src/components/PersonalizedSpells/PersonalizedSpells.jsx
@@ -1,71 +1,69 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Typography } from "../../ui/Typography";
-import { CarouselCard } from "./CarouselCard"
-import { Button } from "../../ui/Button"
-import { useMediaQuery } from "./MediaQuery"
+import { CarouselCard } from "./CarouselCard";
+import { Button } from "../../ui/Button";
+import { useMediaQuery } from "./MediaQuery";
 import { SignUpForm } from "./SignUpForm";
 import styled from "styled-components";
 import flower from "../../assets/Flower.png";
 
-
-
 const CarouselBackground = styled.section`
-    position: relative;
-    margin: 50px 0 0;
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  position: relative;
+  margin: 50px 0 0;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const PersBackgroundImage = styled.img`
-    width: 100%;
-    height: 70vh;
-    position: absolute;
-    z-index: -1;
-    bottom: -100px;
-    right: -140px;
+  width: 100%;
+  height: 70vh;
+  position: absolute;
+  z-index: -1;
+  bottom: -100px;
+  right: -140px;
 
-    @media (min-width: 768px) {
-        height: 80vh;
-        width: 110%;
-    }
+  @media (min-width: 768px) {
+    height: 80vh;
+    width: 110%;
+  }
 
-    @media (min-width: 1200px) {
-        height: 85vh;
-        width: 80%;
-        right: -390px;
-    }
+  @media (min-width: 1200px) {
+    height: 85vh;
+    width: 80%;
+    right: -390px;
+  }
 `;
 
 const PersTitle = styled.section`
-    display: flex;
-    width: 100%;
-    height: auto;
-    padding: 10px;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-    background: #111010;
-    color: #fff;
-    z-index: 1;
+  display: flex;
+  width: 100%;
+  height: auto;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  background: #111010;
+  color: #fff;
+  z-index: 1;
 `;
 
 const PersInfoSection = styled.section`
-    width: 295px;
-    margin: 3rem auto 0;
-    z-index: 1;
+  width: 295px;
+  margin: 3rem auto 0;
+  z-index: 1;
 
-    @media screen and (min-width: 1200px) {
-        width: 40rem;
-        margin: 3rem auto;
-    }
+  @media screen and (min-width: 1200px) {
+    width: 40rem;
+    margin: 3rem auto;
+  }
 `;
 
 const PersGrid = styled.section`
-    display: flex;
+  display: flex;
   flex-direction: ${({ isMobile }) => (isMobile ? 'column' : 'row')};
   align-items: center;
   justify-content: ${({ isMobile }) => (isMobile ? 'center' : 'flex-start')};
@@ -76,28 +74,26 @@ const PersGrid = styled.section`
   max-width: 100%;
 `;
 
-
-
 export const PersonalizedSpells = ({ quotesData }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0); // Added state for current quote index
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   const togglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
   };
 
+  // Automatisk uppdatering av citat var 3 sekund
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === quotesData.quotes.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Interval for automatic quote rotation
 
-  /* Next quote function on mobil, media query to shoe all quotes in on row on ipad, desktops */
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const isMobile = useMediaQuery("(max-width: 768px)");
-
-
-  // Handle click to see next quote
-  const handleNextQuote = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === quotesData.quotes.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
+    // Rensa intervallet när komponenten avmonteras
+    return () => clearInterval(interval);
+  }, [quotesData.quotes.length]); // Dependence on quotes data length for update
 
   return (
     <CarouselBackground>
@@ -116,16 +112,12 @@ export const PersonalizedSpells = ({ quotesData }) => {
       {/* Render the SignUpPopup if isPopupOpen is true */}
       {isPopupOpen && <SignUpForm onClose={togglePopup} />}
 
-
       <PersGrid isMobile={isMobile}>
         {isMobile ? (
-          <>
-            <CarouselCard
-              key={currentIndex}
-              text={quotesData.quotes[currentIndex].text}
-            />
-            <Button text="Next" isMobile={isMobile} onClick={handleNextQuote} />
-          </>
+          <CarouselCard
+            key={currentIndex}
+            text={quotesData.quotes[currentIndex].text}
+          />
         ) : (
           quotesData.quotes.map((quote, index) => (
             <CarouselCard key={index} text={quote.text} />
