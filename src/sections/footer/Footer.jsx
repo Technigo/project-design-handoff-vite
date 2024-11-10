103aa03b743419eee488e0e8177e5a05529adb7e
gabster94 <iofe.gabriella@gmail.com>
2024-11-10 13:58:08 +0100

    some more styling for footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -3,6 +3,14 @@ import { LogoSection } from "./LogoSection"
 import { FinePrintFooter } from "../../re-components/Typography"
 import { SocialLinks, StartTodayButton } from "./SocialLinks"
 
+const FooterContainer = styled.div `
+  background-color: ${({ theme }) => theme.backgrounds.highlight};
+    @media (min-width: ${({ theme }) => theme.breakpoints.large}) {
+      padding-left: 2.625rem;
+      padding-right: 2.625rem;
+    }
+`;
+
 const FooterWrapper = styled.div`
 background-color: ${({ theme }) => theme.backgrounds.highlight};
 padding-top: ${({ theme }) => theme.spacing.small};
@@ -13,17 +21,15 @@ gap: 1rem;
 align-self: stretch;
 `;
 
-
-
 export const Footer = () => {
   return (
-    <>
-    <StartTodayButton/>
-    <SocialLinks/>
-    <LogoSection/>
-    <FooterWrapper>
-      <FinePrintFooter>** Acts of kindness are highly contagious  © 2024</FinePrintFooter>
-    </FooterWrapper>
-    </>
+    <FooterContainer>
+      <StartTodayButton/>
+      <SocialLinks/>
+      <LogoSection/>
+      <FooterWrapper>
+        <FinePrintFooter>** Acts of kindness are highly contagious  © 2024</FinePrintFooter>
+      </FooterWrapper>
+    </FooterContainer>
   )
 }
